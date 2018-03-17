a0aad59002413624e70c11ae1cd9d1da8e341736
Overtorment <overtorment@gmail.com>
2018-03-17 22:58:30 +0000

    codestyle

--- SecondaryBottomTabs.js
+++ SecondaryBottomTabs.js
@@ -5,6 +5,7 @@
 import React from 'react';
 import { Button, ScrollView } from 'react-native';
 import { SafeAreaView, StackNavigator, TabNavigator } from 'react-navigation';
+import PropTypes from 'prop-types';
 
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import SampleText from './SampleText';
@@ -30,20 +31,71 @@ const MyNavScreen = ({ navigation, banner }) => (
   </ScrollView>
 );
 
+MyNavScreen.propTypes = {
+  banner: PropTypes.string,
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }),
+};
+
 const MyHomeScreen = ({ navigation }) => (
   <MyNavScreen banner="Home Screen" navigation={navigation} />
 );
+
+MyHomeScreen.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }),
+};
 
 const MyNotificationsSettingsScreen = ({ navigation }) => (
   <MyNavScreen banner="Notifications Screen" navigation={navigation} />
 );
 
+MyNotificationsSettingsScreen.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }),
+};
+
 const MySettingsScreen = ({ navigation }) => (
   <MyNavScreen banner="Settings Screen" navigation={navigation} />
 );
 
+MySettingsScreen.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }),
+};
+
 var bitcoin = require('bitcoinjs-lib');
 var myString = bitcoin.ECPair.makeRandom().toWIF();
+
+const tabBarIconMainTab = ({ tintColor, focused }) => (
+  <Ionicons
+    name={focused ? 'ios-home' : 'ios-home-outline'}
+    size={26}
+    style={{ color: tintColor }}
+  />
+);
+
+tabBarIconMainTab.propTypes = {
+  tintColor: PropTypes.string,
+  focused: PropTypes.boolean,
+};
+
+const tabBarIconSettingsTab = ({ tintColor, focused }) => (
+  <Ionicons
+    name={focused ? 'ios-settings' : 'ios-settings-outline'}
+    size={26}
+    style={{ color: tintColor }}
+  />
+);
+
+tabBarIconSettingsTab.propTypes = {
+  tintColor: PropTypes.string,
+  focused: PropTypes.boolean,
+};
 
 const TabNav = TabNavigator(
   {
@@ -53,13 +105,7 @@ const TabNav = TabNavigator(
       navigationOptions: {
         title: 'Welcome1 ' + myString,
         tabBarLabel: 'Transactions',
-        tabBarIcon: ({ tintColor, focused }) => (
-          <Ionicons
-            name={focused ? 'ios-home' : 'ios-home-outline'}
-            size={26}
-            style={{ color: tintColor }}
-          />
-        ),
+        tabBarIcon: tabBarIconMainTab,
       },
     },
     SettingsTab: {
@@ -67,13 +113,7 @@ const TabNav = TabNavigator(
       path: '/settings',
       navigationOptions: {
         title: 'Settings',
-        tabBarIcon: ({ tintColor, focused }) => (
-          <Ionicons
-            name={focused ? 'ios-settings' : 'ios-settings-outline'}
-            size={26}
-            style={{ color: tintColor }}
-          />
-        ),
+        tabBarIcon: tabBarIconSettingsTab,
       },
     },
   },
