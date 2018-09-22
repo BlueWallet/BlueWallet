d88cb06ee772530fc3468f1638ff2d8ad076f417
Overtorment <overtorment@gmail.com>
2018-09-22 14:43:44 +0100

    REL

--- patches/fix_mangle.sh
+++ patches/fix_mangle.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-grep -rl "mangle: { toplevel: true }" ./node_modules/  | xargs sed -i "s/mangle: { toplevel: true }/mangle: false/g"
+grep -rl "mangle: { toplevel: true }" ./node_modules/  | xargs sed -i '' -e "s/mangle: { toplevel: true }/mangle: false/g"
