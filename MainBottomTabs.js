7debdf912bb3a75d7d5e37a8fe938b0fb6269f25
Overtorment <overtorment@gmail.com>
2018-09-27 20:29:13 +0100

    REF: cleanup dead code

--- MainBottomTabs.js
+++ MainBottomTabs.js
@@ -1,28 +1,13 @@
 import { createStackNavigator } from 'react-navigation';
 
-import transactions from './screen/transactions';
 import wallets from './screen/wallets';
-import send from './screen/send';
 import settings from './screen/settings';
-import receive from './screen/receive';
 
 const Tabs = createStackNavigator(
   {
     Wallets: {
       screen: wallets,
       path: 'wallets',
-    },
-    Transactions: {
-      screen: transactions,
-      path: 'trans',
-    },
-    Send: {
-      screen: send,
-      path: 'cart',
-    },
-    Receive: {
-      screen: receive,
-      path: 'receive',
     },
     Settings: {
       screen: settings,
