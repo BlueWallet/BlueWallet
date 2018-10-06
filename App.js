818e35a3eb3856b8535283a91415e1a4399ac32e
Overtorment <overtorment@gmail.com>
2018-10-06 03:19:16 +0100

    OPS

--- App.js
+++ App.js
@@ -8,8 +8,6 @@ import Selftest from './screen/selftest';
 import About from './screen/settings/about';
 import PlausibleDeniability from './screen/plausibledeniability';
 import Sentry from 'sentry-expo';
-
-Sentry.enableInExpoDevelopment = true; // TODO: Remove this once Sentry is correctly setup.
 Sentry.config('https://23377936131848ca8003448a893cb622@sentry.io/1295736').install();
 
 /** @type {AppStorage} */
