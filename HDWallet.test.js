7d85c868795ca8fa07992338ac445327d2c6878d
Overtorment <overtorment@gmail.com>
2018-10-31 15:09:12 +0000

    TST

--- HDWallet.test.js
+++ HDWallet.test.js
@@ -71,7 +71,7 @@ it('can generate Segwit HD (BIP49)', async () => {
 
 it('HD (BIP49) can create TX', async () => {
   if (!process.env.HD_MNEMONIC) {
-    console.warn('process.env.HD_MNEMONIC not set, skipped');
+    console.error('process.env.HD_MNEMONIC not set, skipped');
     return;
   }
   jasmine.DEFAULT_TIMEOUT_INTERVAL = 90 * 1000;
