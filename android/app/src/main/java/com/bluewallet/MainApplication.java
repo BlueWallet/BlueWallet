ca662388a8533ed026cdade92450a81f0e25b0a0
Overtorment <overtorment@gmail.com>
2018-11-18 21:16:22 +0000

    OPS: adnroid/

--- android/app/src/main/java/com/bluewallet/MainApplication.java
+++ android/app/src/main/java/com/bluewallet/MainApplication.java
@@ -3,17 +3,17 @@ package com.bluewallet;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import im.shimo.react.prompt.RNPromptPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.horcrux.svg.SvgPackage;
 import io.sentry.RNSentryPackage;
 import com.sensors.RNSensorsPackage;
+import com.bitgo.randombytes.RandomBytesPackage;
+import im.shimo.react.prompt.RNPromptPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.mkuczera.RNReactNativeHapticFeedbackPackage;
 import com.idehub.GoogleAnalyticsBridge.GoogleAnalyticsBridgePackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
-import com.bitgo.randombytes.RandomBytesPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -34,17 +34,17 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNPromptPackage(),
             new VectorIconsPackage(),
             new SvgPackage(),
             new RNSentryPackage(),
             new RNSensorsPackage(),
+            new RandomBytesPackage(),
+            new RNPromptPackage(),
             new LinearGradientPackage(),
             new RNReactNativeHapticFeedbackPackage(),
             new GoogleAnalyticsBridgePackage(),
             new RNDeviceInfo(),
-            new RCTCameraPackage(),
-            new RandomBytesPackage()
+            new RCTCameraPackage()
       );
     }
 
