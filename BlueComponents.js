05d63ff3f6a02c4ef91f3d42ace2c66d9574e334
Marcos Rodriguez Vélez <marcospr@pm.me>
2019-01-06 17:48:06 -0500

    FIX: Only allow one dot on BTC

--- BlueComponents.js
+++ BlueComponents.js
@@ -1136,6 +1136,7 @@ export class BlueBitcoinAmount extends Component {
               onChangeText={text => {
                 text = text.replace(',', '.');
                 text = this.props.unit === BitcoinUnit.BTC ? text.replace(/[^0-9.]/g, '') : text.replace(/[^0-9]/g, '');
+                text = text.replace(/(\..*)\./g, '$1')
                 this.props.onChangeText(text);
               }}
               placeholder="0"
