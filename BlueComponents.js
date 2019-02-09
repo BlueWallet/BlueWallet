8d48489e894bd9882b2ff8ecf1005587d2fa6811
Marcos Rodriguez Vélez <marcospr@pm.me>
2019-02-09 15:46:43 -0500

    FIX: Dismiss keyboard when pressing return

--- BlueComponents.js
+++ BlueComponents.js
@@ -15,6 +15,7 @@ import {
   StyleSheet,
   Dimensions,
   Image,
+  Keyboard,
   SafeAreaView,
   InputAccessoryView,
   Clipboard,
@@ -1572,6 +1573,7 @@ export class BlueAddressInput extends Component {
           value={this.props.address}
           style={{ flex: 1, marginHorizontal: 8, minHeight: 33 }}
           editable={!this.props.isLoading}
+          onSubmitEditing={Keyboard.dismiss}
         />
         <TouchableOpacity
           disabled={this.props.isLoading}
--- screen/lnd/lndCreateInvoice.js
+++ screen/lnd/lndCreateInvoice.js
@@ -103,6 +103,7 @@ export default class LNDCreateInvoice extends Component {
                   numberOfLines={1}
                   style={{ flex: 1, marginHorizontal: 8, minHeight: 33 }}
                   editable={!this.state.isLoading}
+                  onSubmitEditing={Keyboard.dismiss}
                 />
               </View>
               {this.renderCreateButton()}
