d62e02939092276a61ba56709c78af82b8a30a73
Marcos Rodriguez Vélez <marcospr@pm.me>
2019-02-13 04:00:38 -0500

    Balance fix

--- class/abstract-hd-wallet.js
+++ class/abstract-hd-wallet.js
@@ -395,8 +395,8 @@ export class AbstractHDWallet extends LegacyWallet {
 
       this.next_free_address_index = await binarySearchIterationForExternalAddress(100);
 
-      this.balance = 0;
-      this.unconfirmed_balance = 0;
+      // this.balance = 0;
+      // this.unconfirmed_balance = 0;
       this.usedAddresses = [];
 
       // generating all involved addresses:
