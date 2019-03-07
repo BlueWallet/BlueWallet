4c19b4b78c2442b91cb21b67eb10da17ab6f7c2b
Marcos Rodriguez Vélez <marcospr@pm.me>
2019-03-06 22:06:53 -0500

    ADD: WatchOS scheme and files

---
+++ ios/BlueWalletWatch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  BlueWalletWatch Extension
+//
+//  Created by Marcos Rodriguez on 3/6/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
