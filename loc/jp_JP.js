20854c21fd143917b26ee26e60e69d2caed18c4d
Overtorment <overtorment@gmail.com>
2019-05-03 23:58:23 +0100

    REF: added localization strings

--- loc/jp_JP.js
+++ loc/jp_JP.js
@@ -228,4 +228,13 @@ module.exports = {
       "Please take a moment to write down this mnemonic phrase on a piece of paper. It's your backup you can use to restore the wallet on other device. You can use Electrum wallet on desktop (https://electrum.org/) to restore the same wallet.",
     ok: 'OK, I wrote this down!',
   },
+  lndViewInvoice: {
+    wasnt_paid_and_expired: 'This invoice was not paid for and has expired',
+    has_been_paid: 'This invoice has been paid for',
+    please_pay: 'Please pay',
+    sats: 'sats',
+    for: 'For:',
+    additional_info: 'Additional Information',
+    open_direct_channel: 'Open direct channel with this node:',
+  },
 };
