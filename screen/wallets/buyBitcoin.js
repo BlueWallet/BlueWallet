298aba26f4e0b9bf1e5a131a145afd31d81e15e6
Tristan Edwards <tristan.edwards@me.com>
2019-05-09 12:17:01 +0200

    Support Safello redirect URLs
    When the user buys bitcoin through Safello, they will be redirected to
    the Yoti app to verify their identity.
    
    Previously, when the process was done, they
    would be redirected to a browser window to continue their purchase, but
    since bluewallet has a `bluewallet://` redirect URI associated to its
    app ID, we can redirect them back to the app with a
    `safello-state-token` query parameter appended to the URI instead.
    
    Bluewallet then simply needs to check if that query parameter is
    present, and if so, open a new WebView with the same Safello URL +
    `stateToken=xxx` in order to resume the user's session. This way, the
    whole buying process can take place inside Bluewallet's app, which
    is a clear improvement in terms of user experience.

--- screen/wallets/buyBitcoin.js
+++ screen/wallets/buyBitcoin.js
@@ -63,7 +63,29 @@ export default class BuyBitcoin extends Component {
       return <BlueLoading />;
     }
 
-    return <WebView source={{ uri: 'https://bluewallet.io/buy-bitcoin-redirect.html?address=' + this.state.address }} />;
+    const { safelloStateToken } = this.props.navigation.state.params
+
+     if (safelloStateToken) {
+      return (
+        <WebView
+          source={{
+            uri:
+              "https://app.safello.com/sdk/quickbuy.html?appId=1234-5678&stateToken=" +
+              safelloStateToken,
+          }}
+        />
+      )
+    }
+
+     return (
+      <WebView
+        source={{
+          uri:
+            "https://bluewallet.io/buy-bitcoin-redirect.html?address=" +
+            this.state.address,
+        }}
+      />
+    )
   }
 }
 
