6ed96d94deb668acae11dabb76cef089d77a7f0a
ncoelho <nunovc@gmail.com>
2019-06-05 22:27:38 +0200

    add missing strings

--- loc/ZAR_Xho.js
+++ loc/ZAR_Xho.js
@@ -216,4 +216,19 @@
    placeholder: 'Invoyisi',
    sameWalletAsInvoiceError: ': Awukwazi ukuhlawula i-invoyisi kunye ngengxowa oyisebenzisile ukudala  leyo invoyisi.',
  },
+ pleasebackup: {
+    title: 'Your wallet is created...',
+    text:
+      "Please take a moment to write down this mnemonic phrase on a piece of paper. It's your backup you can use to restore the wallet on other device. You can use Electrum wallet on desktop (https://electrum.org/) to restore the same wallet.",
+    ok: 'OK, I wrote this down!',
+  },
+  lndViewInvoice: {
+    wasnt_paid_and_expired: 'This invoice was not paid for and has expired',
+    has_been_paid: 'This invoice has been paid for',
+    please_pay: 'Please pay',
+    sats: 'sats',
+    for: 'For:',
+    additional_info: 'Additional Information',
+    open_direct_channel: 'Open direct channel with this node:',
+  },
 };
