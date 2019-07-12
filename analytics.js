47e8853dccb639368b4c177aedf34a33402a3d7b
Overtorment <overtorment@gmail.com>
2019-07-12 23:34:06 +0100

    OPS: android RN 0.60

--- analytics.js
+++ analytics.js
@@ -1,12 +1,9 @@
 //  import Amplitude from 'react-native-amplitude-analytics';
-import { GoogleAnalyticsTracker } from 'react-native-google-analytics-bridge';
 
 // Amplitude.initialize('8b7cf19e8eea3cdcf16340f5fbf16330');
-const analytics = new GoogleAnalyticsTracker('UA-121673546-1');
 
 let A = function(event) {
   // Amplitude.logEvent(event);
-  analytics.trackEvent(event, event);
 };
 
 A.ENUM = {
