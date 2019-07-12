cd16396028b3c402679e8d0060c3c40950d2c1aa
Overtorment <overtorment@gmail.com>
2019-07-12 23:34:06 +0100

    OPS: android RN 0.60

--- android/app/src/main/java/io/bluewallet/bluewallet/MainApplication.java
+++ android/app/src/main/java/io/bluewallet/bluewallet/MainApplication.java
@@ -2,49 +2,12 @@ package io.bluewallet.bluewallet;
 
 import android.app.Application;
 
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
-import com.reactnativecommunity.slider.ReactSliderPackage;
-import com.diegofhg.obscure.ObscurePackage;
-import com.peel.react.TcpSocketsModule;
-import com.remobile.qrcodeLocalImage.RCTQRCodeLocalImagePackage;
-import com.imagepicker.ImagePickerPackage;
-import com.reactnativecommunity.webview.RNCWebViewPackage;
-import io.sentry.RNSentryPackage;
-import com.bitgo.randombytes.RandomBytesPackage;
-import im.shimo.react.prompt.RNPromptPackage;
-import com.BV.LinearGradient.LinearGradientPackage;
-import com.mkuczera.RNReactNativeHapticFeedbackPackage;
-import com.idehub.GoogleAnalyticsBridge.GoogleAnalyticsBridgePackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.rnfs.RNFSPackage;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import org.reactnative.camera.RNCameraPackage;
-import io.sentry.RNSentryPackage;
-import com.bitgo.randombytes.RandomBytesPackage;
-import im.shimo.react.prompt.RNPromptPackage;
-import com.BV.LinearGradient.LinearGradientPackage;
-import com.mkuczera.RNReactNativeHapticFeedbackPackage;
-import com.idehub.GoogleAnalyticsBridge.GoogleAnalyticsBridgePackage;
-import com.reactnativecommunity.webview.RNCWebViewPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.horcrux.svg.SvgPackage;
-import io.sentry.RNSentryPackage;
-import com.bitgo.randombytes.RandomBytesPackage;
-import im.shimo.react.prompt.RNPromptPackage;
-import com.BV.LinearGradient.LinearGradientPackage;
-import com.mkuczera.RNReactNativeHapticFeedbackPackage;
-import com.idehub.GoogleAnalyticsBridge.GoogleAnalyticsBridgePackage;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import org.reactnative.camera.RNCameraPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.rnfs.RNFSPackage;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -57,28 +20,11 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new AsyncStoragePackage(),
-            new ReactSliderPackage(),
-            new ObscurePackage(),
-            new TcpSocketsModule(),
-            new RCTQRCodeLocalImagePackage(),
-            new ImagePickerPackage(),
-            new RNCWebViewPackage(),
-            new RNSentryPackage(),
-            new RandomBytesPackage(),
-            new RNPromptPackage(),
-            new RNReactNativeHapticFeedbackPackage(),
-            new GoogleAnalyticsBridgePackage(),
-            new RNDeviceInfo(),
-            new LinearGradientPackage(),
-            new RNFSPackage() ,
-            new VectorIconsPackage(),
-            new SvgPackage(),
-            new RNCameraPackage(),
-            new RNGestureHandlerPackage()
-      );
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new MyReactNativePackage());
+      return packages;
     }
 
     @Override
