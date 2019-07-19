f29e8a2fa8bf1fc69715931a5b6084056c036efc
Marcos Rodriguez <marcospr@pm.me>
2019-07-19 10:25:57 -0400

    TST

--- podinstall.sh
+++ podinstall.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+echo "AppCenter XCode Project: "
+echo $APPCENTER_XCODE_PROJECT
+echo
 if [[ "$APPCENTER_XCODE_PROJECT" == "BlueWallet" && "$OSTYPE" == "darwin"* ]]; then
         cd ios
         pod install
