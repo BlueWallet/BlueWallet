d1305ffce54be83836faf27637bc036c4179f4bb
Marcos Rodriguez <marcospr@pm.me>
2019-07-20 22:03:44 -0400

    ADD: AppCenter analytics and crash sdk

--- ios/BlueWallet/AppDelegate.m
+++ ios/BlueWallet/AppDelegate.m
@@ -16,6 +16,10 @@
 #import "RNSentry.h" // This is used for versions of react < 0.40
 #endif
 #import "WatchBridge.h"
+#import <AppCenterReactNative.h>
+#import <AppCenterReactNativeAnalytics.h>
+#import <AppCenterReactNativeCrashes.h>
+#import <AppCenterReactNativePush.h>
 
 @implementation AppDelegate
 
@@ -40,7 +44,9 @@
   self.session = self.watchBridge.session;
   [self.session activateSession];
   self.session.delegate = self;
-  
+  [AppCenterReactNative register];
+  [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
+  [AppCenterReactNativeCrashes registerWithAutomaticProcessing];
   return YES;
 }
 
