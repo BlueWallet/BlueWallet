2ae474288d58643b239f8b9f14e2c494fd4301b8
Marcos Rodriguez <marcospr@pm.me>
2019-08-06 21:42:41 -0400

    FIX: Fix Send max toolbar calculation

--- BlueComponents.js
+++ BlueComponents.js
@@ -778,15 +778,34 @@ export class BlueUseAllFundsButton extends Component {
         }}
       >
         <View style={{ flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'flex-start' }}>
-          <Text style={{ color: BlueApp.settings.alternativeTextColor, fontSize: 16, marginLeft: 8, marginRight: 0, paddingRight: 0, paddingLeft: 0, paddingTop: 12, paddingBottom: 12 }}>
-            {this.props.wallet.getBalance()} {BitcoinUnit.BTC}
+          <Text
+            style={{
+              color: BlueApp.settings.alternativeTextColor,
+              fontSize: 16,
+              marginLeft: 8,
+              marginRight: 0,
+              paddingRight: 0,
+              paddingLeft: 0,
+              paddingTop: 12,
+              paddingBottom: 12,
+            }}
+          >
+            Total: {loc.formatBalanceWithoutSuffix(this.props.wallet.getBalance(), BitcoinUnit.BTC, true).toString()} {BitcoinUnit.BTC}
           </Text>
-          {this.props.wallet.allowSendMax() && this.props.wallet.balance > 0 && (
-            <BlueButtonLink style={{ marginLeft: 8, paddingRight: 0, paddingLeft: 0, paddingTop: 12, paddingBottom: 12 }} title="Use All" onPress={this.props.onUseAllPressed} />
+          {this.props.wallet.allowSendMax() && this.props.wallet.getBalance() > 0 && (
+            <BlueButtonLink
+              style={{ marginLeft: 8, paddingRight: 0, paddingLeft: 0, paddingTop: 12, paddingBottom: 12 }}
+              title="Use All"
+              onPress={this.props.onUseAllPressed}
+            />
           )}
         </View>
         <View style={{ flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'flex-end' }}>
-          <BlueButtonLink style={{ paddingRight: 8, paddingLeft: 0, paddingTop: 12, paddingBottom: 12 }} title="Done" onPress={Keyboard.dismiss} />
+          <BlueButtonLink
+            style={{ paddingRight: 8, paddingLeft: 0, paddingTop: 12, paddingBottom: 12 }}
+            title="Done"
+            onPress={Keyboard.dismiss}
+          />
         </View>
       </View>
     );
