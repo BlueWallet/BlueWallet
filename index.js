0976b44b8f927d379c449b40ee4703b2a22b77b6
Marcos Rodriguez <marcospr@pm.me>
2019-09-19 20:09:33 -0400

    ADD: Splash screen animation #644

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import { Sentry } from 'react-native-sentry';
 import { AppRegistry } from 'react-native';
 import WalletMigrate from './screen/wallets/walletMigrate';
 import { name as appName } from './app.json';
+import LottieView from 'lottie-react-native';
+
 /** @type {AppStorage} */
 const BlueApp = require('./BlueApp');
 let A = require('./analytics');
@@ -22,7 +24,12 @@ if (!Error.captureStackTrace) {
 class BlueAppComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isMigratingData: true };
+    this.state = { isMigratingData: true, onAnimationFinished: false };
+  }
+
+  componentDidMount() {
+    const walletMigrate = new WalletMigrate(this.setIsMigratingData);
+    walletMigrate.start();
   }
 
   setIsMigratingData = async () => {
@@ -31,8 +38,40 @@ class BlueAppComponent extends React.Component {
     this.setState({ isMigratingData: false });
   };
 
+  onAnimationFinish = () => {
+    if (this.state.isMigratingData) {
+      this.loadingSplash.play(0);
+    } else {
+      this.setState({ onAnimationFinished: true });
+    }
+  };
+
   render() {
-    return this.state.isMigratingData ? <WalletMigrate onComplete={this.setIsMigratingData} /> : <App />;
+    if (this.state.isMigratingData) {
+      return (
+        <LottieView
+          ref={ref => (this.loadingSplash = ref)}
+          onAnimationFinish={this.onAnimationFinish}
+          source={require('./img/bluewalletsplash.json')}
+          autoPlay
+          loop={false}
+        />
+      );
+    } else {
+      if (this.state.onAnimationFinished) {
+        return <App />;
+      } else {
+        return (
+          <LottieView
+            ref={ref => (this.loadingSplash = ref)}
+            onAnimationFinish={this.onAnimationFinish}
+            source={require('./img/bluewalletsplash.json')}
+            autoPlay
+            loop={false}
+          />
+        );
+      }
+    }
   }
 }
 
---
+++ ios/Bridge.swift
@@ -0,0 +1,9 @@
+//
+//  Bridge.swift
+//  BlueWallet
+//
+//  Created by Marcos Rodriguez on 9/19/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
