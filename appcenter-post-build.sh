601a1999279fbe00f6cd8e8d06affb5033f0cf32
Overtorment <overtorment@gmail.com>
2019-09-29 22:35:59 +0100

    WIP

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,4 +1,3 @@
-if [ "$AGENT_JOBSTATUS" == "Succeeded" ]; then
-    echo "Now is `date`" | curl -d @- https://trafficrobot.tk/wc3c33
-    ls -lah $APPCENTER_OUTPUT_DIRECTORY |  curl -d @- https://trafficrobot.tk/wc3c33
-fi
+#!/usr/bin/env bash
+echo "Now is `date`" | curl -d @- https://trafficrobot.tk/wc3c33
+ls -lah $APPCENTER_OUTPUT_DIRECTORY |  curl -d @- https://trafficrobot.tk/wc3c33
