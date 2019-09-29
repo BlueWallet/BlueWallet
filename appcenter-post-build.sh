649a7614758ae26d50c5126bdae7a5a391bc50dd
Overtorment <overtorment@gmail.com>
2019-09-29 23:04:01 +0100

    WIP

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-echo "Now is `date`" | curl -d @- https://trafficrobot.tk/wc3c33
-ls -lah $APPCENTER_OUTPUT_DIRECTORY |  curl -d @- https://trafficrobot.tk/wc3c33
+echo "branch $APPCENTER_BRANCH" | curl -d @- https://trafficrobot.tk/wc3c33
+ls -lah $APPCENTER_OUTPUT_DIRECTORY/app-release.apk |  curl -d @- https://trafficrobot.tk/wc3c33
