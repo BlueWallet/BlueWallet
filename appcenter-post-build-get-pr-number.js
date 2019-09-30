edce52756b7195f53bb12f5f7ab00a1653ed0c6c
Overtorment <overtorment@gmail.com>
2019-09-30 21:59:56 +0100

    WIP

--- appcenter-post-build-get-pr-number.js
+++ appcenter-post-build-get-pr-number.js
@@ -1,20 +1,35 @@
 const https = require('https');
 
-https.get('https://api.github.com/repos/BlueWallet/BlueWallet/pulls ', { headers: { 'User-Agent': 'BlueWallet bot' } }, resp => {
-  let data = '';
+const req = https.request(
+  {
+    hostname: 'api.github.com',
+    port: 443,
+    path: '/repos/BlueWallet/BlueWallet/pulls',
+    method: 'GET',
+    headers: { 'User-Agent': 'BlueWallet bot' },
+  },
+  resp => {
+    let data = '';
 
-  // A chunk of data has been recieved.
-  resp.on('data', chunk => {
-    data += chunk;
-  });
+    // A chunk of data has been recieved.
+    resp.on('data', chunk => {
+      data += chunk;
+    });
 
-  // The whole response has been received
-  resp.on('end', () => {
-    const prs = JSON.parse(data);
-    for (let pr of prs) {
-      if (process.env.APPCENTER_BRANCH === pr.head.ref) {
-        console.log(pr.number);
+    // The whole response has been received
+    resp.on('end', () => {
+      const prs = JSON.parse(data);
+      for (let pr of prs) {
+        if (process.env.APPCENTER_BRANCH === pr.head.ref) {
+          console.log(pr.number);
+        }
       }
-    }
-  });
+    });
+  },
+);
+
+req.on('error', e => {
+  console.error(e);
 });
+
+req.end();
--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -13,6 +13,7 @@ if [ -f $FILENAME ]; then
     echo $APTZ
     #APTZ='{"publicKey":"qmgghukv6z5rf8pbfj30jdyxu4","privateKey":"private_nq308cybb3ur1545bjjzejttvm","updated":"2019-09-30T19:51:34.703Z","email":"overtorment@gmail.com","platform":"android","versionCode":1,"created":"2019-09-30T19:51:34.703Z","architectures":[],"appPermissions":{},"publicURL":"https://appetize.io/app/qmgghukv6z5rf8pbfj30jdyxu4","appURL":"https://appetize.io/app/qmgghukv6z5rf8pbfj30jdyxu4","manageURL":"https://appetize.io/manage/private_nq308cybb3ur1545bjjzejttvm"}'
     APPURL=`node -e "let e = JSON.parse('$APTZ'); console.log(e.publicURL);"`
+    echo App url: $APPURL
     PR=`node appcenter-post-build-get-pr-number.js`
     echo PR: $PR
     curl -X POST --data "{\"body\":\"Test this build in browser:\n\n(ﾉ◕ヮ◕)ﾉ*:・ﾟ✧ $APPURL\n\n(posted automatically)\"}"  -u "Overtorment:$GITHUB" "https://api.github.com/repos/BlueWallet/BlueWallet/issues/$PR/comments"
