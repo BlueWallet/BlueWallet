00bb9fe28bbfa738eb28ab1b585164db17fc0c29
Marcos Rodriguez <marcospr@pm.me>
2019-11-02 20:25:55 -0400

    ADD: Today Extension and Quick Actions.
    Update MainInterface.storyboard
    
    ADD: Cache fetched data
    
    ADD: Add "Up from/Down from" data
    
    OPS: Add Provision Profile
    
    Update project.pbxproj
    
    ADD: Synchronize user selected currency with Today extension
    
    Update Info.plist
    
    ADD: quick actions
    
    Update App.js
    
    Update App.js
    
    Update App.js
    
    Update App.js
    
    Update App.js
    
    FIX: Fix currency bugs
    
    FIX: Hide balance from quick actions
    
    ADD: cqc
    
    Update currency.js
    
    Update TodayViewController.swift
    
    Update App.js
    
    REF: headercolor
    
    Update App.js
    
    FIX: carousel would not show
    
    FIX: Fix
    
    FIX
    
    FIX
    
    Update API.swift
    
    FIX: Fixed alerts
    
    TST
    
    Revert "TST"
    
    This reverts commit bcdf62e9b238cc5da200cc4bce45e4e55f480d6d.
    
    ADD: Tests mocks
    
    REF: Device Quick Actions ref
    
    FIX: Fixed crash
    
    Update quickActions.ios.js
    
    ADD: QuickActions for Android
    
    D
    
    FIX: Tests
    
    OP

--- edit-version-number.sh
+++ edit-version-number.sh
@@ -1,6 +1,7 @@
 vim ios/BlueWallet/Info.plist
 vim ios/BlueWalletWatch/Info.plist
 vim "ios/BlueWalletWatch Extension/Info.plist"
+vim "ios/TodayExtension/Info.plist"
 vim android/app/build.gradle
 vim package.json
 vim package-lock.json
--- tests/setup.js
+++ tests/setup.js
@@ -5,3 +5,18 @@ jest.mock('react-native-watch-connectivity', () => {
     updateApplicationContext: jest.fn(),
   }
 })
+
+jest.mock('react-native-quick-actions', () => {
+  return {
+    clearShortcutItems: jest.fn(),
+    setQuickActions: jest.fn(),
+    isSupported: jest.fn(),
+  }
+})
+
+jest.mock('react-native-default-preference', () => {
+  return {
+    setName: jest.fn(),
+    set: jest.fn(),
+  }
+})
