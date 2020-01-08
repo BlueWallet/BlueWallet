eb580b29419716a4e071f0990b5fd91264a6b711
Overtorment <overtorment@gmail.com>
2020-01-08 21:41:24 +0000

    WIP

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -7,6 +7,7 @@ echo -n "Branch 2 "
 git log -n 1 --pretty=%d HEAD | awk '{print $2}' | sed 's/origin\///' | sed 's/)//'
 
 FILENAME="$APPCENTER_OUTPUT_DIRECTORY/app-release.apk"
+FILENAME_IOS="$APPCENTER_OUTPUT_DIRECTORY/BlueWallet.ipa"
 
 if [ -f $FILENAME ]; then
     APTZ=`curl "https://$APPETIZE@api.appetize.io/v1/apps" -F "file=@$FILENAME" -F "platform=android"`
@@ -26,4 +27,17 @@ if [ -f $FILENAME ]; then
     DLOAD_APK="http://filestorage.bluewallet.io:1488/$HASH.apk"
 
     curl -X POST --data "{\"body\":\"♫ This was a triumph. I'm making a note here: HUGE SUCCESS ♫\n\n [android in browser] $APPURL\n\n[download apk]($DLOAD_APK) \"}"  -u "$GITHUB" "https://api.github.com/repos/BlueWallet/BlueWallet/issues/$PR/comments"
+fi
+
+
+if [ -f $FILENAME_IOS ]; then
+  # means we are building production IOS build. lets try to build sim build as well and upload it to appetize
+  mv "$FILENAME_IOS" "$FILENAME_IOS.bak"
+  xcodebuild -arch i386 -configuration Release -sdk iphonesimulator13.0 -workspace 'ios/BlueWallet.xcworkspace' -scheme 'BlueWallet'
+  echo 'ios/build/Debug-iphonesimulator/    :'
+  ls -lah ios/build/Debug-iphonesimulator/
+  echo
+  find | grep "\.app"
+
+  mv "$FILENAME_IOS.bak" "$FILENAME_IOS"
 fi
