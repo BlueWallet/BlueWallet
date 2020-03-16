22cf386f505b5c741156dcccea2d4b4075d338b0
Overtorment <overtorment@gmail.com>
2020-03-16 13:51:08 +0000

    TST: wip detox on circleci

---
+++ tests/e2e/detox-prepare-android-emu.sh
@@ -0,0 +1,2 @@
+$ANDROID_HOME/tools/bin/sdkmanager "system-images;android-28;default;x86_64"
+$ANDROID_HOME/tools/bin/avdmanager create avd -n Pixel_API_28_AOSP -d pixel --package "system-images;android-28;default;x86_64"
