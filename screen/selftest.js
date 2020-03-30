25f29fee279b693b2a7414f32426265fe813ca01
Overtorment <overtorment@gmail.com>
2020-03-30 22:52:01 +0100

    TST: selftest improvements REF: lint REL: ver bump

--- screen/selftest.js
+++ screen/selftest.js
@@ -32,14 +32,12 @@ export default class Selftest extends Component {
         for (let c = 0; c < 1000; c++) {
           await w.generate();
           if (uniqs[w.getSecret()]) {
-            errorMessage += 'failed to generate unique private key; ';
-            isOk = false;
-            break;
+            throw new Error('failed to generate unique private key');
           }
           uniqs[w.getSecret()] = 1;
         }
       } else {
-        console.warn('skipping RN-specific test');
+        // skipping RN-specific test
       }
 
       //
@@ -55,7 +53,7 @@ export default class Selftest extends Component {
         let electrumTxs = await BlueElectrum.getTransactionsByAddress(addr4elect);
         if (electrumTxs.length !== 1) throw new Error('BlueElectrum getTransactionsByAddress failure, got ' + JSON.stringify(electrumTxs));
       } else {
-        console.warn('skipping RN-specific test');
+        // skipping RN-specific test'
       }
 
       //
@@ -63,8 +61,7 @@ export default class Selftest extends Component {
       let l = new LegacyWallet();
       l.setSecret('Kxr9tQED9H44gCmp6HAdmemAzU3n84H3dGkuWTKvE23JgHMW8gct');
       if (l.getAddress() !== '19AAjaTUbRjQCMuVczepkoPswiZRhjtg31') {
-        errorMessage += 'failed to generate legacy address from WIF; ';
-        isOk = false;
+        throw new Error('failed to generate legacy address from WIF');
       }
 
       //
@@ -92,8 +89,7 @@ export default class Selftest extends Component {
         txHex !==
         '01000000017f514cf57cd451e30c981c8e14e6b455535197ff3b477ddb7227fa16f05c442f010000006b483045022100b9a6545847bd30418c133437c7660a6676afafe6e7e837a37ef2ead931ebd586022056bc43cbf71855d0719f54151c8fcaaaa03367ecafdd7296dbe39f042e432f4f012103aea0dfd576151cb399347aa6732f8fdf027b9ea3ea2e65fb754803f776e0a509ffffffff01905f0100000000001976a914aa381cd428a4e91327fd4434aa0a08ff131f1a5a88ac00000000'
       ) {
-        errorMessage += 'failed to create TX from legacy address; ';
-        isOk = false;
+        throw new Error('failed to create TX from legacy address');
       }
 
       // now, several utxos
@@ -134,18 +130,12 @@ export default class Selftest extends Component {
       toAddr = '1GX36PGBUrF8XahZEGQqHqnJGW2vCZteoB';
       amount = 0.0009;
       fee = 0.0001;
-      try {
-        txHex = l.createTx(utxos, amount, fee, toAddr);
-      } catch (e) {
-        errorMessage += e.message + '; ';
-        isOk = false;
-      }
+      txHex = l.createTx(utxos, amount, fee, toAddr);
       if (
         txHex !==
         '0100000002ef23d5fd18900bb205ca08071b2ad3f559a522bc43ed84c54b72030a040536dc000000006a47304402206b4f03e471d60dff19f4df1a8203ca97f6282658160034cea0f2b7d748c33d9802206058d23861dabdfb252c8df14249d1a2b00345dd90d32ab451cc3c6cfcb3b402012103aea0dfd576151cb399347aa6732f8fdf027b9ea3ea2e65fb754803f776e0a509ffffffff17b09e5d51a18b0af3359e33d098118c46a182106d97041862e6bffe04c173c4000000006b4830450221009785a61358a1ee7ab5885a98b111275226e0046a48b69980c4f53ecf99cdce0a02200503249e0b23d633ec1fbae5d41a0dcf9758dce3560066d1aee9ecfbfeefcfb7012103aea0dfd576151cb399347aa6732f8fdf027b9ea3ea2e65fb754803f776e0a509ffffffff02905f0100000000001976a914aa381cd428a4e91327fd4434aa0a08ff131f1a5a88ac400d0300000000001976a914597ce022baa887799951e0496c769d9cc0c759dc88ac00000000'
       ) {
-        errorMessage += 'failed to create TX from legacy address; ';
-        isOk = false;
+        throw new Error('failed to create TX from legacy address');
       }
 
       //
@@ -153,8 +143,7 @@ export default class Selftest extends Component {
       l = new SegwitP2SHWallet();
       l.setSecret('Kxr9tQED9H44gCmp6HAdmemAzU3n84H3dGkuWTKvE23JgHMW8gct');
       if (l.getAddress() !== '34AgLJhwXrvmkZS1o5TrcdeevMt22Nar53') {
-        errorMessage += 'failed to generate segwit P2SH address from WIF; ';
-        isOk = false;
+        throw new Error('failed to generate segwit P2SH address from WIF');
       }
 
       //
@@ -180,15 +169,13 @@ export default class Selftest extends Component {
       let txid = txDecoded.getId();
 
       if (txid !== '110f51d28d585e922adbf701cba802e549b8fe3a53fa5d62426ab42549c9b6de') {
-        errorMessage += 'created txid doesnt match; ';
-        isOk = false;
+        throw new Error('created txid doesnt match');
       }
       if (
         tx !==
         '0100000000010123c3e0950eabb2b5c1a956e9d003c516fc29ff529211bd552be719fb78ea5e0f0200000017160014597ce022baa887799951e0496c769d9cc0c759dc0000000001a0860100000000001976a914ff715fb722cb10646d80709aeac7f2f4ee00278f88ac02473044022075670317a0e5b5d4eef154b03db97396a64cbc6ef3b576d98367e1a83c1c488002206d6df1e8085fd711d6ea264de3803340f80fa2c6e30683879d9ad40f3228c56c012103aea0dfd576151cb399347aa6732f8fdf027b9ea3ea2e65fb754803f776e0a50900000000'
       ) {
-        errorMessage += 'created tx hex doesnt match; ';
-        isOk = false;
+        throw new Error('created tx hex doesnt match');
       }
 
       let feeSatoshi = new BigNumber(0.0001);
@@ -197,16 +184,16 @@ export default class Selftest extends Component {
       satoshiPerByte = Math.round(satoshiPerByte.toString(10));
 
       if (satoshiPerByte !== 46) {
-        errorMessage += 'created tx satoshiPerByte doesnt match; ';
-        isOk = false;
+        throw new Error('created tx satoshiPerByte doesnt match');
       }
 
       //
+
       const data2encrypt = 'really long data string';
       let crypted = encryption.encrypt(data2encrypt, 'password');
       let decrypted = encryption.decrypt(crypted, 'password');
 
-      if (decrypted !== data2encrypt && crypted && decrypted) {
+      if (decrypted !== data2encrypt) {
         throw new Error('encryption lib is not ok');
       }
 
@@ -229,8 +216,7 @@ export default class Selftest extends Component {
       }).address;
 
       if (address !== '3GcKN7q7gZuZ8eHygAhHrvPa5zZbG5Q1rK') {
-        errorMessage += 'bip49 is not ok; ';
-        isOk = false;
+        throw new Error('bip49 is not ok');
       }
 
       //
@@ -257,15 +243,6 @@ export default class Selftest extends Component {
 
         //
 
-        let hd3 = new HDSegwitP2SHWallet();
-        hd3._xpub = 'ypub6Wb82D7F38b48uzRVyTwydMCPcos4njzygPRCJ4x1enm6EA5YUthtWgJUPYiFTs7Sk53q8rJ9d1SJ2fBNqsyhjUTDR7gyF1SXbBnaa9xcQj';
-        await hd3.fetchBalance();
-        if (hd3.getBalance() !== 26000) throw new Error('Could not fetch HD balance');
-        await hd3.fetchTransactions();
-        if (hd3.getTransactions().length !== 1) throw new Error('Could not fetch HD transactions');
-
-        //
-
         let hd4 = new HDSegwitBech32Wallet();
         hd4._xpub = 'zpub6r7jhKKm7BAVx3b3nSnuadY1WnshZYkhK8gKFoRLwK9rF3Mzv28BrGcCGA3ugGtawi1WLb2vyjQAX9ZTDGU5gNk2bLdTc3iEXr6tzR1ipNP';
         await hd4.fetchBalance();
@@ -273,7 +250,7 @@ export default class Selftest extends Component {
         await hd4.fetchTransactions();
         if (hd4.getTransactions().length !== 4) throw new Error('Could not fetch HD Bech32 transactions');
       } else {
-        console.warn('skipping RN-specific test');
+        // skipping RN-specific test
       }
 
       //
@@ -313,7 +290,7 @@ export default class Selftest extends Component {
                 return (
                   <View style={{ alignItems: 'center' }}>
                     <BlueText h4 numberOfLines={0}>
-                      error: {this.state.errorMessage}
+                      {this.state.errorMessage}
                     </BlueText>
                   </View>
                 );
