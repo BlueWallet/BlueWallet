c4b827c719ac23c7257595f276d50abe70ecbcf3
mateuszpmroz <mateuszpmroz@gmail.com>
2020-04-06 08:24:00 +0200

    add palette colors

--- src/styles/index.tsx
+++ src/styles/index.tsx
@@ -1,3 +1,4 @@
 export { headerStyle } from './header';
 export { typography } from './typography';
 export { fonts } from './fonts';
+export { palette } from './palette';
---
+++ src/styles/palette.tsx
@@ -0,0 +1,18 @@
+export const palette = {
+  primary: '#f70949',
+  secondary: '#ebc15c',
+  textSecondary: 'rgb(238, 196, 100)',
+  textBlack: '#000',
+  textRed: 'rgb(230, 93, 93)',
+  textWhiteMuted: 'rgba(255, 255, 255, 0.54)',
+  textGrey: 'rgb(148, 149, 149)',
+  gradientPrimaryFirst: 'rgb(247, 9, 73)',
+  gradientPrimarySecond: 'rgb(31, 1, 31)',
+  gradientSecondaryFirst: 'rgb(255, 214, 99)',
+  gradientSecondarySecond: 'rgb(200, 156, 101)',
+  background: '#FFFFFF',
+  border: 'rgb(204, 204, 204)',
+  error: 'rgb(244, 94, 89)',
+  shadow: 'rgba(0, 0, 0, 0.12)',
+  transparent: 'transparent',
+};
