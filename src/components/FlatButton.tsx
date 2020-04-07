cbd5f5e5c73bc343e6d86d6efaea19ba030f9660
mateuszpmroz <mateuszpmroz@gmail.com>
2020-04-07 21:10:44 +0200

    remove props const

--- src/components/FlatButton.tsx
+++ src/components/FlatButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, TextStyle } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { Button, ButtonProps } from 'react-native-elements';
 import { palette, typography } from 'styles';
 
@@ -12,21 +12,16 @@ interface Props extends ButtonProps {
   buttonType?: ButtonType;
 }
 
-export const FlatButton = (props: Props) => {
-  const { buttonType } = props;
-
-  return (
-    <Button
-      {...props}
-      type={'clear'}
-      titleStyle={[
-        buttonType && buttonType === ButtonType.Warning ? styles.warningTitle : styles.standardTitle,
-        typography.button,
-        props.titleStyle as TextStyle,
-      ]}
-    />
-  );
-};
+export const FlatButton = (props: Props) => (
+  <Button
+    {...(props as ButtonProps)}
+    type={'clear'}
+    titleStyle={[
+      props.buttonType && props.buttonType === ButtonType.Warning ? styles.warningTitle : styles.standardTitle,
+      typography.button,
+    ]}
+  />
+);
 
 const styles = StyleSheet.create({
   standardTitle: {
