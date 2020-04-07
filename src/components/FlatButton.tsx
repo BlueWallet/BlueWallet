2bc75a7789450a0277edc53edd087f5db0105ef9
mateuszpmroz <mateuszpmroz@gmail.com>
2020-04-07 19:27:31 +0200

    add button type

--- src/components/FlatButton.tsx
+++ src/components/FlatButton.tsx
@@ -3,13 +3,36 @@ import { StyleSheet, TextStyle } from 'react-native';
 import { Button, ButtonProps } from 'react-native-elements';
 import { palette, typography } from 'styles';
 
-export const FlatButton = (props: ButtonProps) => (
-  <Button {...props} type={'clear'} titleStyle={[styles.title, props.titleStyle as TextStyle]} />
-);
+export enum ButtonType {
+  Standard,
+  Warning,
+}
+
+interface Props extends ButtonProps {
+  buttonType?: ButtonType;
+}
+
+export const FlatButton = (props: Props) => {
+  const { buttonType } = props;
+
+  return (
+    <Button
+      {...props}
+      type={'clear'}
+      titleStyle={[
+        buttonType && buttonType === ButtonType.Warning ? styles.warningTitle : styles.standardTitle,
+        typography.button,
+        props.titleStyle as TextStyle,
+      ]}
+    />
+  );
+};
 
 const styles = StyleSheet.create({
-  title: {
+  standardTitle: {
     color: palette.secondary,
-    ...typography.button,
+  },
+  warningTitle: {
+    color: palette.textRed,
   },
 });
