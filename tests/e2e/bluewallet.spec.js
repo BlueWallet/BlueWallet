2f34aaf3cc70f347493e64d9b858d867915be4b4
Overtorment <overtorment@gmail.com>
2020-04-20 13:29:37 +0100

    TST: ref detox

--- tests/e2e/bluewallet.spec.js
+++ tests/e2e/bluewallet.spec.js
@@ -1,6 +1,21 @@
 /* global it, describe, expect, element, by, waitFor, device */
 
 describe('BlueWallet UI Tests', () => {
+  it('selftest passes', async () => {
+    await waitFor(element(by.id('WalletsList')))
+      .toBeVisible()
+      .withTimeout(300 * 1000);
+
+    // go to settings, press SelfTest and wait for OK
+    await element(by.id('SettingsButton')).tap();
+    await element(by.id('AboutButton')).tap();
+    await element(by.id('AboutScrollView')).swipe('up', 'fast', 1); // in case emu screen is small and it doesnt fit
+    await element(by.id('RunSelfTestButton')).tap();
+    await waitFor(element(by.id('SelfTestOk')))
+      .toBeVisible()
+      .withTimeout(300 * 1000);
+  });
+
   it('can encrypt storage, with plausible deniability', async () => {
     await yo('WalletsList');
 
