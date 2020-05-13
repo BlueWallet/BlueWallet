ed536260697fbb8b74684e003591e2867ba4b302
Konrad Jawulski <konrad.jawulski@minebest.com>
2020-05-13 11:05:41 +0200

    transaltion files updated

--- loc/id_ID.js
+++ loc/id_ID.js
@@ -1,248 +1,266 @@
 module.exports = {
   _: {
-    storage_is_encrypted: 'Penyimpanan dienkripsi. Masukkan kata sandi untuk dekripsi:',
+    storage_is_encrypted: 'Penyimpanan Anda dienkripsi. Perlu kata sandi untuk mendekripsinya',
     enter_password: 'Masukkan kata sandi',
-    bad_password: 'kata sandi salah, coba lagi',
+    bad_password: 'Kata sandi buruk, coba lagi',
     never: 'tidak pernah',
     continue: 'Lanjutkan',
-    ok: 'OK',
+    ok: 'OK'
+  },
+  message: {
+    somethingWentWrong: 'Terjadi kesalahan',
+    somethingWentWrongWhileCreatingWallet: 'Terjadi kesalahan saat kami membuat dompet Anda. Silakan kembali ke Dasbor dan coba lagi.',
+    success: 'Berhasil',
+    successfullWalletImport: 'Dompet Anda berhasil diimpor. Anda sekarang dapat kembali ke Dasbor.',
+    successfullWalletDelete: 'Dompet Anda berhasil dihapus. Anda sekarang dapat kembali ke Dasbor.',
+    returnToDashboard: 'Kembali ke Dasbor',
+    creatingWallet: 'Membuat dompet Anda',
+    creatingWalletDescription: 'Mohon kesabaran Anda sementara kami membuat dompet Anda. Ini mungkin membutuhkan waktu.'
   },
   wallets: {
-    select_wallet: 'Pilih dompet',
-    options: 'Opsi',
-    createBitcoinWallet:
-      'Belum ada dompet bitcoin. Untuk mendanai dompet Lightning, dompet Bitcoin harus dibuat atau diimpor. Yakin ingin melanjutkan?',
-    list: {
-      app_name: 'BlueWallet',
+    dashboard: {
       title: 'Dompet',
-      header:
-        'Sebuah dompet mewakili sepasang kunci rahasia dan sebuah alamat' + 'yang bisa dipilih untuk menerima koin.',
-      add: 'Tambah dompet',
-      create_a_wallet: 'Buat dompet',
-      create_a_wallet1: 'Gratis dan bisa buat',
-      create_a_wallet2: 'sebanyak yang kamu mau',
-      latest_transaction: 'transaksi terbaru',
-      empty_txs1: 'Transaksimu akan muncul di sini,',
-      empty_txs2: 'saat ini tidak ada transaksi',
-      empty_txs1_lightning:
-        'Lightning wallet should be used for your daily transactions. Fees are unfairly cheap and speed is blazing fast.',
-      empty_txs2_lightning: '\nTo start using it tap on "manage funds" and topup your balance.',
-      tap_here_to_buy: 'Tap di sini untuk membeli bitcoin',
+      noWallets: 'Tidak ada dompet',
+      noWalletsDesc1: 'Tidak ada dompet untuk ditampilkan.',
+      noWalletsDesc2: 'untuk menambahkan dompet pertama Anda.',
+      send: 'Kirim koin',
+      receive: 'Terima koin',
+      noTransactions: 'Tidak ada transaksi untuk ditampilkan.'
     },
-    reorder: {
-      title: 'Susun Dompet',
+    walletModal: { btcv: 'BTCV', wallets: 'Dompet' },
+    importWallet: {
+      title: 'Impor dompet Anda',
+      header: 'Impor dompet',
+      subtitle: 'Tuliskan di sini mnemonik, kunci privat, WIF, atau apa pun yang Anda punya. GoldWallet akan berusaha sebaik mungkin untuk menebak format yang tepat dan mengimpor dompet Anda.',
+      placeholder: 'Mnemonik, kunci pribadi, WIF',
+      import: 'Impor',
+      scanQrCode: 'atau pindai kode QR',
+      walletInUseValidationError: 'Dompet sudah digunakan. Masukkan dompet yang valid.'
     },
+    exportWallet: { title: 'Frasa mnemonik', header: 'Ekspor dompet' },
+    exportWalletXpub: { header: 'Dompet XPUB' },
+    deleteWallet: {
+      title: 'Hapus dompet Anda',
+      header: 'Hapus dompet',
+      description1: 'Anda yakin ingin menghapus',
+      description2: '? Anda tidak dapat mengurungkannya.',
+      no: 'Tidak',
+      yes: 'Ya'
+    },
+    wallet: { none: 'Tidak ada', latest: 'Transaksi terakhir' },
     add: {
-      title: 'tambah dompet',
-      description:
-        'Kamu bisa membuat dompet atau memindai paper wallet dalam WIF (Wallet Import Format). Bluewallet mendukung dompet Segwit.',
-      scan: 'Pindai',
-      create: 'Buat',
-      label_new_segwit: 'Dompet SegWit baru',
-      label_new_lightning: 'Dompet Lightning baru',
-      wallet_name: 'nama dompet',
-      wallet_type: 'tipe',
-      or: 'atau',
-      import_wallet: 'Impor dompet',
-      imported: 'Diimpor',
-      coming_soon: 'Akan datang',
-      lightning: 'Lightning',
-      bitcoin: 'Bitcoin',
+      title: 'Tambah dompet baru',
+      subtitle: 'Namai dompet Anda',
+      description: 'Masukkan nama untuk dompet baru Anda.',
+      inputLabel: 'Nama',
+      addWalletButton: 'Tambah dompet baru',
+      importWalletButton: 'Impor dompet',
+      advancedOptions: 'Opsi lanjutan',
+      multipleAddresses: 'Beberapa alamat',
+      singleAddress: 'Satu alamat'
+    },
+    addSuccess: {
+      title: 'Tambah dompet baru',
+      subtitle: 'Berhasil',
+      description: 'Dompet Anda telah dibuat. Luangkan waktu sebentar untuk menulis frasa mnemonik ini di selembar kertas. Untuk berjaga-jaga. Anda dapat menggunakannya untuk memulihkan dompet di perangkat lain.',
+      okButton: 'Oke, saya sudah menuliskannya!'
     },
     details: {
-      title: 'Dompet',
-      address: 'Alamat',
-      type: 'Tipe',
-      label: 'Label',
-      destination: 'tujuan',
-      description: 'deskripsi',
-      are_you_sure: 'Yakin?',
-      yes_delete: 'Ya, hapus',
-      no_cancel: 'Tidak, batalkan',
-      delete: 'Hapus',
-      save: 'Simpan',
-      delete_this_wallet: 'Hapus dompet ini',
-      export_backup: 'Ekspor / backup',
-      buy_bitcoin: 'Beli Bitcoin',
-      show_xpub: 'Tampilkan XPUB dompet',
+      latestTransaction: 'Transaksi terakhir',
+      typeLabel: 'Tipe',
+      nameLabel: 'Nama',
+      exportWallet: 'Ekspor dompet',
+      showWalletXPUB: 'Tampilkan Dompet XPUB',
+      deleteWallet: 'Hapus dompet',
+      nameEdit: 'Edit nama'
     },
-    export: {
-      title: 'ekspor dompet',
-    },
-    xpub: {
-      title: 'XPUB dompet',
-      copiedToClipboard: 'Disalin ke clipboard.',
-    },
+    export: { title: 'ekspor dompet' },
     import: {
       title: 'impor',
-      explanation:
-        'Ketik kata mnemonic, private key, WIF, atau apapun yang kamu punya. BlueWallet akan mencoba mengimpor dompet kamu.',
+      explanation: 'Tuliskan di sini mnemonik, kunci privat, WIF, atau apa pun yang Anda punya. GoldWallet akan berusaha sebaik mungkin untuk menebak format yang tepat dan mengimpor dompet Anda',
       imported: 'Diimpor',
-      error: 'Gagal mengimpor. Pastikan data yang diketik benar.',
+      error: 'Gagal mengimpor. Harap pastikan data yang diberikan valid.',
       success: 'Berhasil',
       do_import: 'Impor',
-      scan_qr: 'atau mau pindai QR code?',
+      scan_qr: 'atau pindai kode QR?'
     },
     scanQrWif: {
       go_back: 'Kembali',
       cancel: 'Batal',
-      decoding: 'Membaca...',
+      decoding: 'Mendekode',
       input_password: 'Masukkan kata sandi',
-      password_explain: 'Ini adalah private key terenkripsi BIP38',
-      bad_password: 'kata sandi salah',
-      wallet_already_exists: 'Dompet sudah ada',
-      bad_wif: 'WIF salah',
-      imported_wif: 'WIF diimpor ',
-      with_address: ' dengan alamat ',
-      imported_segwit: 'Dompet SegWit diimpor',
-      imported_legacy: 'Dompet lawas diimpor',
-      imported_watchonly: 'Alamat tinjauan diimpor',
-    },
+      password_explain: 'Ini adalah kunci privat terenkripsi BIP38',
+      bad_password: 'Kata sandi buruk',
+      wallet_already_exists: 'Dompet tersebut sudah ada',
+      bad_wif: 'WIF buruk',
+      imported_wif: 'WIF diimpor',
+      with_address: 'dengan alamat',
+      imported_segwit: 'SegWit diimpor',
+      imported_legacy: 'Legacy diimpor',
+      imported_watchonly: 'Watch-only diimpor'
+    }
   },
   transactions: {
-    list: {
-      tabBarLabel: 'Transaksi',
-      title: 'transaksi',
-      description: 'Daftar transaksi keluar dan masuk dompet',
-      conf: 'konfirmasi',
-    },
+    list: { conf: 'Konfirmasi' },
     details: {
       title: 'Transaksi',
-      from: 'Input',
-      to: 'Output',
+      detailTitle: 'Detail transaksi',
+      transactionHex: 'Hex transaksi',
+      transactionHexDescription: 'Ini adalah hex transaksi, ditandatangani dan siap disiarkan ke jaringan',
+      copyAndBoriadcast: 'Salin dan siarkan nanti',
+      verify: 'Verifikasi di coinb.in',
+      amount: 'Jumlah',
+      fee: 'Biaya',
+      txSize: 'Ukuran TX',
+      satoshiPerByte: 'Satoshi per byte',
+      from: 'Dari',
+      to: 'Ke',
+      bytes: 'byte',
       copy: 'Salin',
-      transaction_details: 'Detail transaksi',
-      show_in_block_explorer: 'Tampilkan di block explorer',
-    },
+      noLabel: 'Tidak ada label',
+      details: 'Detail',
+      transactionId: 'ID Transaksi',
+      confirmations: 'konfirmasi',
+      transactionDetails: 'Detail transaksi',
+      viewInBlockRxplorer: 'Lihat di block explorer',
+      addNote: 'Tambah catatan',
+      note: 'Catatan',
+      inputs: 'Input',
+      ouputs: 'Output',
+      sendCoins: 'Kirim koin'
+    }
   },
   send: {
-    header: 'Kirim',
+    header: 'Kirim koin',
+    success: {
+      title: 'Berhasil',
+      description: 'Hore! Anda berhasil menyelesaikan transaksi.',
+      done: 'Selesai',
+      return: 'Kembali ke Dasbor'
+    },
     details: {
       title: 'buat transaksi',
-      amount_field_is_not_valid: 'Jumlah tidak valid',
-      fee_field_is_not_valid: 'Tarif tidak valid',
-      address_field_is_not_valid: 'Alamat tidak valid',
-      total_exceeds_balance: 'Jumlah yang dikirim melebihi saldo.',
-      create_tx_error: 'Kesalahan dalam membuat transaksi. Cek kembali alamat tujuan.',
+      amount_field_is_not_valid: 'Bidang jumlah tidak valid',
+      fee_field_is_not_valid: 'Bidang biaya tidak valid',
+      address_field_is_not_valid: 'Bidang alamat tidak valid',
+      create_tx_error: 'Ada kesalahan saat membuat transaksi. Harap pastikan alamat valid.',
       address: 'alamat',
-      amount_placeholder: 'jumlah (dalam BTCV)',
-      fee_placeholder: 'Tambahan biaya transaksi (dalam BTCV)',
-      note_placeholder: 'catatan pribadi',
-      cancel: 'Batalkan',
+      amount_placeholder: 'jumlah yang dikirimkan (dalam BTCV)',
+      fee_placeholder: 'plus biaya transaksi (dalam BTCV)',
+      note_placeholder: 'catatan untuk diri sendiri',
+      cancel: 'Batal',
       scan: 'Pindai',
       send: 'Kirim',
-      create: 'Buat',
+      next: 'Berikutnya',
+      to: 'ke',
+      feeUnit: 'Sat/B',
+      fee: 'Biaya:',
+      create: 'Buat Faktur',
       remaining_balance: 'Sisa saldo',
+      total_exceeds_balance: 'Jumlah pengiriman melebihi sisa saldo.'
     },
-    confirm: {
-      header: 'Konfirmasi',
-      sendNow: 'Kirim sekarang',
-    },
-    success: {
-      done: 'Selesai',
-    },
+    confirm: { sendNow: 'Kirim sekarang' },
     create: {
-      details: 'Detail',
-      title: 'buat transaksi',
-      error: 'Tidak bisa membuat transaksi. Cek alamat atau jumlah transfer.',
-      go_back: 'Kembali',
-      this_is_hex: 'Ini adalah hex transaksi, siap untuk disiarkan ke jaringan.',
-      to: 'Ke',
       amount: 'Jumlah',
-      fee: 'Tarif',
-      tx_size: 'Ukuran TX',
-      satoshi_per_byte: 'Satoshi per byte',
-      memo: 'Memo',
-      broadcast: 'Siarkan',
-      not_enough_fee: 'Tarif tidak cukup. Naikkan tarif',
-    },
+      fee: 'Biaya',
+      setTransactionFee: 'Atur biaya transaksi',
+      headerText: 'Jika ada sejumlah besar transaksi tertunda di jaringan (>1500), biaya lebih tinggi akan membuat transaksi Anda diproses lebih cepat. Nilai biasanya adalah 1-500 sat/b'
+    }
   },
   receive: {
-    header: 'Terima',
+    header: 'Terima koin',
     details: {
-      title: 'Bagikan alamat ini ke pengirim',
-      share: 'bagikan',
-      copiedToClipboard: 'Disalin ke clipboard.',
-      label: 'Deskripsi',
-      create: 'Buat',
-      setAmount: 'Terima sejumlah',
-    },
-    scan_lnurl: 'Scan to receive',
-  },
-  buyBitcoin: {
-    header: 'Beli bitcoin',
-    tap_your_address: 'Untuk menyalin, tap alamat:',
-    copied: 'Disalin ke Clipboard!',
+      amount: 'Jumlah',
+      share: 'Bagikan',
+      receiveWithAmount: 'Terima dengan jumlah'
+    }
   },
   settings: {
-    header: 'setting',
-    plausible_deniability: 'Plausible deniability...',
-    storage_not_encrypted: 'Penyimpanan: tidak terenkripsi',
-    storage_encrypted: 'Penyimpanan: terenkripsi',
-    password: 'kata sandi',
-    password_explain: 'Buat kata sandi untuk dekripsi penyimpanan',
-    retype_password: 'Ulangi kata sandi',
-    passwords_do_not_match: 'Kata sandi tidak cocok',
-    encrypt_storage: 'Enkripsi penyimpanan',
-    lightning_settings: 'Pengaturan Lightning',
-    lightning_settings_explain:
-      'Pasang LndHub untuk menghubungkan ke node LND kamu' +
-      ' dan masukkan URL di sini. Biarkan kosong untuk menghubungkan ke LndHub standar (lndhub.io)',
-    electrum_settings: 'Electrum Settings',
-    electrum_settings_explain: 'Set to blank to use default',
-    save: 'simpan',
-    about: 'Tentang',
     language: 'Bahasa',
-    currency: 'Mata Uang',
-    advanced_options: 'Advanced Options',
-    enable_advanced_mode: 'Enable advanced mode',
+    general: 'Umum',
+    security: 'Keamanan',
+    about: 'Tentang',
+    electrumServer: 'Server Electrum',
+    advancedOptions: 'Opsi lanjutan',
+    changePin: 'Ubah PIN',
+    fingerprintLogin: 'Login sidik jari',
+    aboutUs: 'Tentang kami',
+    header: 'Pengaturan'
   },
-  plausibledeniability: {
-    title: 'Plausible Deniability',
-    help:
-      'Under certain circumstances, you might be forced to disclose a ' +
-      'password. To keep your coins safe, BlueWallet can create another ' +
-      'encrypted storage, with a different password. Under pressure, ' +
-      'you can disclose this password to a 3rd party. If entered in ' +
-      "BlueWallet, it will unlock new 'fake' storage. This will seem " +
-      'legit to a 3rd party, but will secretly keep your main storage ' +
-      'with coins safe.',
-    help2:
-      'New storage will be fully functional, and you can store some ' +
-      'minimum amounts there so it looks more believable.',
-    create_fake_storage: 'Create fake encrypted storage',
-    go_back: 'Go Back',
-    create_password: 'Create a password',
-    create_password_explanation: 'Password for fake storage should not match password for your main storage',
-    password_should_not_match: 'Password for fake storage should not match password for your main storage',
-    retype_password: 'Retype password',
-    passwords_do_not_match: 'Passwords do not match, try again',
-    success: 'Success',
+  aboutUs: {
+    header: 'Tentang kami',
+    releaseNotes: 'Catatan rilis',
+    runSelfTest: 'Jalankan tes mandiri',
+    buildWithAwesome: 'Bangun dengan hebat:',
+    rateGoldWallet: 'Beri peringkat GoldWallet',
+    goToOurGithub: 'Masuk ke Github kami',
+    alwaysBackupYourKeys: 'Selalu cadangkan kunci Anda',
+    title: 'GoldWallet adalah dompet Bitcoin Vault gratis dan sumber terbuka. Dilisensi MIT.'
   },
-  lnd: {
-    title: 'atur dana',
-    choose_source_wallet: 'Pilih dompet sumber',
-    refill_lnd_balance: 'Isi ulang saldo Lightning',
-    refill: 'Isi ulang',
-    withdraw: 'Tarik',
-    placeholder: 'Invoice',
-    expired: 'Kadaluarsa',
-    sameWalletAsInvoiceError:
-      'Kamu tidak bisa membayar invoice dengan dompet yang sama yang dipakai untuk membuat invoice.',
+  electrumServer: {
+    header: 'Server Electrum',
+    save: 'Simpan',
+    useDefault: 'Gunakan default',
+    host: 'host',
+    port: 'port',
+    successfullSave: 'Perubahan Anda berhasil disimpan. Mungkin dibutuhkan pemulaian ulang agar perubahan berlaku.',
+    connectionError: 'Tidak dapat terhubung ke server Electrum yang tersedia'
   },
-  pleasebackup: {
-    title: 'Your wallet is created...',
-    text:
-      "Please take a moment to write down this mnemonic phrase on a piece of paper. It's your backup you can use to restore the wallet on other device.",
-    ok: 'OK, I wrote this down!',
+  selectLanguage: {
+    header: 'Bahasa',
+    restartInfo: 'Saat memilih bahasa baru, mungkin dibutuhkan pemulaian ulang GoldWallet agar perubahan berlaku',
+    confirmation: 'Konfirmasi',
+    confirm: 'Konfirmasi',
+    alertDescription: 'Pilih bahasa dan mulai ulang aplikasi?',
+    cancel: 'Batal'
   },
-  lndViewInvoice: {
-    wasnt_paid_and_expired: 'This invoice was not paid for and has expired',
-    has_been_paid: 'This invoice has been paid for',
-    please_pay: 'Please pay',
-    sats: 'sats',
-    for: 'For:',
-    additional_info: 'Additional Information',
-    open_direct_channel: 'Open direct channel with this node:',
+  contactList: {
+    cancel: 'Batal',
+    search: 'Cari',
+    bottomNavigationLabel: 'Buku alamat',
+    screenTitle: 'Buku alamat',
+    noContacts: 'Tidak ada kontak',
+    noContactsDesc1: 'Tidak ada kontak untuk ditampilkan. \nKlik',
+    noContactsDesc2: 'untuk menambahkan kontak pertama Anda.',
+    noResults: 'Tidak ada hasil untuk'
   },
-};
+  contactCreate: {
+    screenTitle: 'Tambah kontak baru',
+    subtitle: 'Kontak baru',
+    description: 'Masukkan nama dan alamat\nuntuk kontak baru Anda.',
+    nameLabel: 'Nama',
+    addressLabel: 'Alamat',
+    buttonLabel: 'Tambah kontak baru',
+    successTitle: 'Berhasil',
+    successDescription: 'Hore! Anda telah berhasil\nmenambahkan kontak.',
+    successButton: 'Kembali ke Buku alamat'
+  },
+  contactDetails: {
+    nameLabel: 'Nama',
+    addressLabel: 'Alamat',
+    editName: 'Edit nama',
+    editAddress: 'Edit alamat',
+    sendCoinsButton: 'Kirim koin',
+    showQRCodeButton: 'Tampilkan kode QR',
+    deleteButton: 'Hapus kontak',
+    share: 'Bagikan'
+  },
+  contactDelete: {
+    title: 'Hapus kontak Anda',
+    header: 'Hapus kontak',
+    description1: 'Anda yakin ingin menghapus',
+    description2: '?\nAnda tidak dapat mengurungkannya.',
+    no: 'Tidak',
+    yes: 'Ya',
+    success: 'Berhasil',
+    successDescription: 'Kontak Anda berhasil dihapus.\n' +
+      'Anda sekarang dapat kembali ke Buku alamat.',
+    successButton: 'Kembali ke Buku alamat'
+  },
+  scanQrCode: {
+    permissionTitle: 'Izin untuk menggunakan kamera',
+    permissionMessage: 'Kami membutuhkan izin Anda untuk menggunakan kamera',
+    ok: 'Ok',
+    cancel: 'Batal'
+  }
+}
