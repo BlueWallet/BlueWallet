94fb26f20b2d909dd2feb950ed4788cc1502de15
Ivan Vershigora <ivan.vershigora@gmail.com>
2020-05-25 18:39:34 +0300

    REF: migrate to RN4

--- MainBottomTabs.js
+++ MainBottomTabs.js
@@ -1,4 +1,5 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
 import Settings from './screen/settings/settings';
 import About from './screen/settings/about';
