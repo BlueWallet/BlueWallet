825695f9168b4ce5fb962dd1d9ae664f0aad025d
Michal Kovac <miso@github.ksp.sk>
2020-06-01 11:30:07 +0200

    ADD: Slovak translation

--- screen/settings/language.js
+++ screen/settings/language.js
@@ -44,6 +44,7 @@ export default class Language extends Component {
         { label: 'Português (BR)', value: 'pt_br' },
         { label: 'Português (PT)', value: 'pt_pt' },
         { label: 'Русский', value: 'ru' },
+        { label: 'Slovensky (SK)', value: 'sk_sk' },
         { label: 'Svenska (SE)', value: 'sv_se' },
         { label: 'Thai (TH)', value: 'th_th' },
         { label: 'Vietnamese (VN)', value: 'vi_vn' },
