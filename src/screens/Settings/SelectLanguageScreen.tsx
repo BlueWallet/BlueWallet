5711468f927b3a04e30cda062f9230ae79751e35
Kamil Lewandowski <kamil.lewandowski@stxnext.pl>
2020-06-08 22:18:24 +0200

    Missing translations added

--- src/screens/Settings/SelectLanguageScreen.tsx
+++ src/screens/Settings/SelectLanguageScreen.tsx
@@ -43,10 +43,10 @@ export const SelectLanguageScreen = () => {
     { label: '中文 (ZH)', value: 'zh_cn' },
     { label: 'Español (ES)', value: 'es' },
     { label: 'Indonesian (ID)', value: 'id_id' },
-    { label: '日本語 (JP)', value: 'ja' },
+    { label: '日本語 (JP)', value: 'jp_jp' },
     { label: '한국어 (KO)', value: 'ko_kr' },
     { label: 'Português (PT)', value: 'pt_pt' },
-    { label: 'Tiếng (Việt)', value: 'vi_vn' },
+    { label: 'Tiếng Việt', value: 'vi_vn' },
     { label: 'Türkçe (TR)', value: 'tr_tr' },
   ];
 
