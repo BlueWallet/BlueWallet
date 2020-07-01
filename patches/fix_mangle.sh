161c991df43c07cd89461c7b383f8824901352fd
Overtorment <overtorment@gmail.com>
2020-07-01 11:03:59 +0100

    REF: remove prepare build patches

--- patches/fix_mangle.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-grep -rl "mangle: { toplevel: true }" ./node_modules/  | xargs sed -i '' -e "s/mangle: { toplevel: true }/mangle: false/g" || true
-grep -rl "mangle: {toplevel: true}" ./node_modules/  | xargs sed -i '' -e "s/mangle: {toplevel: true}/mangle: false/g" || true
-grep -rl "BASE_MAP.fill(255)" ./node_modules/  | xargs sed -i '' -e "s/BASE_MAP.fill(255)/for (let c = 0 ; c< 256; c++) BASE_MAP[c] = 255;/g" || true
-echo fix_mangle.sh done
