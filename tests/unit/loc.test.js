936c70c3a5b5237494e6d1eda78498acc7e8565d
Ivan Vershigora <ivan.vershigora@gmail.com>
2020-07-20 12:37:26 +0300

    REF: i18n remove "has all keys in all locales" test

--- tests/unit/loc.test.js
+++ tests/unit/loc.test.js
@@ -82,41 +82,4 @@ describe('Localization', () => {
     },
     240000,
   );
-
-  it.skip('has all keys in all locales', async () => {
-    const en = require('../../loc/en');
-    let issues = 0;
-    for (const key1 of Object.keys(en)) {
-      for (const key2 of Object.keys(en[key1])) {
-        // iterating all keys and subkeys in EN locale, which is main
-        const files = fs.readdirSync('./loc/');
-
-        for (const lang of files) {
-          if (lang === 'en.js' || lang === 'index.js' || lang === 'languages.js') continue; // iteratin all locales except EN
-
-          const locale = require('../../loc/' + lang);
-
-          if (typeof locale[key1] === 'undefined') {
-            console.error('Missing: ' + lang + '.' + key1);
-            issues++;
-          } else if (typeof locale[key1][key2] === 'undefined') {
-            console.error('Missing: ' + lang + '.' + key1 + '.' + key2);
-            issues++;
-          }
-
-          // level 1 & 2 done, doing level 3 (if it exists):
-
-          if (typeof en[key1][key2] !== 'string') {
-            for (const key3 of Object.keys(en[key1][key2])) {
-              if (typeof locale[key1][key2][key3] === 'undefined') {
-                console.error('Missing: ' + lang + '.' + key1 + '.' + key2 + '.' + key3);
-                issues++;
-              }
-            }
-          }
-        }
-      }
-    }
-    assert.ok(issues === 0, 'Some localizations are missing keys. Total issues: ' + issues);
-  });
 });
