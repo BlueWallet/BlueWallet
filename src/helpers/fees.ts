f7589de2927cd664a6af48a7b6346f5ddab664e3
KedziaPawel <67273035+KedziaPawel@users.noreply.github.com>
2020-08-18 09:44:52 +0200

    Fix async storage warning (#233)
    * Fix async storage warning

--- src/helpers/fees.ts
+++ src/helpers/fees.ts
@@ -1,4 +1,4 @@
-import { AsyncStorage } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import NetworkTransactionFees, { NetworkTransactionFee } from '../../models/networkTransactionFees';
 
