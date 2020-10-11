eab4004dd067bae58a398a8c89e367db53aaad4b
marcosrdz <marcospr@pm.me>
2020-10-11 03:13:47 -0400

    ADD: Allow import of .backup files

--- blue_modules/fs.js
+++ blue_modules/fs.js
@@ -84,7 +84,7 @@ const showFilePickerAndReadFile = async function () {
     const res = await DocumentPicker.pick({
       type:
         Platform.OS === 'ios'
-          ? ['io.bluewallet.psbt', 'io.bluewallet.psbt.txn', DocumentPicker.types.plainText, 'public.json']
+          ? ['io.bluewallet.psbt', 'io.bluewallet.psbt.txn', 'io.bluewallet.backup', DocumentPicker.types.plainText, 'public.json']
           : [DocumentPicker.types.allFiles],
     });
 
