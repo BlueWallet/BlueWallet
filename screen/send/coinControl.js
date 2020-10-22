6dcd98fc65f14930a477b67e7f2523fae1e0d03f
Ivan Vershigora <ivan.vershigora@gmail.com>
2020-10-22 16:30:58 +0300

    ADD: output modal

--- screen/send/coinControl.js
+++ screen/send/coinControl.js
@@ -1,10 +1,11 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, FlatList } from 'react-native';
-import { useRoute, useTheme } from '@react-navigation/native';
+import Modal from 'react-native-modal';
 import { ListItem, Avatar, Badge } from 'react-native-elements';
-import { BlueNavigationStyle, SafeBlueArea } from '../../BlueComponents';
+import { StyleSheet, FlatList, KeyboardAvoidingView, View, Text, Platform, Keyboard, TouchableWithoutFeedback } from 'react-native';
+import { useRoute, useTheme } from '@react-navigation/native';
 
+import { BlueNavigationStyle, SafeBlueArea, BlueSpacing40, BlueButton, BlueListItem } from '../../BlueComponents';
 import loc, { formatBalanceWithoutSuffix } from '../../loc';
 import { BitcoinUnit } from '../../models/bitcoinUnits';
 const BlueApp = require('../../BlueApp');
@@ -15,14 +16,14 @@ const oStyles = StyleSheet.create({
   memo: { fontSize: 13, marginTop: 3 },
 });
 
-const Output = ({ item: { txid, value } }) => {
+const Output = ({ item: { txid, value }, onPress }) => {
   const { colors } = useTheme();
   const txMemo = BlueApp.tx_metadata[txid]?.memo ?? `${txid.substring(0, 6)}...${txid.substr(txid.length - 6)}`;
   const color = `#${txid.substring(0, 6)}`;
   const amount = formatBalanceWithoutSuffix(value, BitcoinUnit.BTC, true);
 
   return (
-    <ListItem bottomDivider>
+    <ListItem bottomDivider onPress={onPress}>
       <Avatar rounded overlayContainerStyle={[oStyles.avatar, { backgroundColor: color }]} />
       <ListItem.Content>
         <ListItem.Title style={oStyles.amount}>{amount}</ListItem.Title>
@@ -30,7 +31,7 @@ const Output = ({ item: { txid, value } }) => {
           {txMemo}
         </ListItem.Subtitle>
       </ListItem.Content>
-      <Badge value="99+" status="error" />
+      <Badge value="freeze" status="error" />
     </ListItem>
   );
 };
@@ -38,26 +39,75 @@ const Output = ({ item: { txid, value } }) => {
 Output.propTypes = {
   item: PropTypes.shape({
     txid: PropTypes.string.isRequired,
-    value: PropTypes.string.isRequired,
+    value: PropTypes.number.isRequired,
   }),
+  onPress: PropTypes.func,
 };
 
 const CoinControl = () => {
+  const { colors } = useTheme();
   const route = useRoute();
   const { walletId } = route.params;
   const wallet = useMemo(() => BlueApp.getWallets().find(w => w.getID() === walletId), [walletId]);
   const utxo = useMemo(() => wallet.getUtxo(), [wallet]);
+  const [output, setOutput] = useState();
+  const [freeze, setFreeze] = useState(false);
 
-  const renderItem = ({ item }) => <Output item={item} />;
+  const handleChoose = item => setOutput(item);
+  const renderItem = ({ item }) => <Output item={item} onPress={() => handleChoose(item)} />;
+  const onFreeze = () => {
+    console.info('onFreeze');
+    setFreeze(i => !i);
+  };
 
   console.info('utxo', utxo);
 
   return (
     <SafeBlueArea>
+      <Modal
+        isVisible={Boolean(output)}
+        style={styles.bottomModal}
+        onBackdropPress={() => {
+          Keyboard.dismiss();
+          setOutput(false);
+        }}
+      >
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'position' : null}>
+          <View style={[styles.modalContent, { backgroundColor: colors.elevated }]}>
+            {output && (
+              <>
+                <Output item={output} />
+                <BlueListItem title="Freeze" Component={TouchableWithoutFeedback} switch={{ value: freeze, onValueChange: onFreeze }} />
+                <Text>{loc.multisig.type_your_mnemonics}</Text>
+                <BlueSpacing40 />
+
+                <BlueButton title="Use coin" onPress={() => {}} />
+              </>
+            )}
+          </View>
+        </KeyboardAvoidingView>
+      </Modal>
+
       <FlatList data={utxo} renderItem={renderItem} keyExtractor={item => item.txid} />
     </SafeBlueArea>
   );
 };
+
+const styles = StyleSheet.create({
+  bottomModal: {
+    justifyContent: 'flex-end',
+    margin: 0,
+  },
+  modalContent: {
+    padding: 22,
+    justifyContent: 'center',
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
+    borderColor: 'rgba(0, 0, 0, 0.1)',
+    minHeight: 290,
+    height: 290,
+  },
+});
 
 CoinControl.navigationOptions = () => ({
   ...BlueNavigationStyle(null, false),
