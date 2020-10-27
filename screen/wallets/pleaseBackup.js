02206a490fe3c6bbf1d98067aac2f83d65cb5868
marcosrdz <marcospr@pm.me>
2020-10-27 14:26:26 -0400

    Update pleaseBackup.js

--- screen/wallets/pleaseBackup.js
+++ screen/wallets/pleaseBackup.js
@@ -13,54 +13,22 @@ const PleaseBackup = () => {
   const wallet = wallets.find(w => w.getID() === walletID);
   const navigation = useNavigation();
   const { colors } = useTheme();
-  const styles = StyleSheet.create({
+  const stylesHook = StyleSheet.create({
     flex: {
-      flex: 1,
       backgroundColor: colors.elevated,
     },
-    loading: {
-      flex: 1,
-      paddingTop: 20,
-    },
     word: {
-      width: 'auto',
-      marginRight: 8,
-      marginBottom: 8,
       backgroundColor: colors.inputBackgroundColor,
-      paddingTop: 6,
-      paddingBottom: 6,
-      paddingLeft: 8,
-      paddingRight: 8,
-      borderRadius: 4,
     },
     wortText: {
       color: colors.labelText,
-      fontWeight: 'bold',
     },
-    scrollViewContent: {
-      justifyContent: 'space-between',
-    },
-    please: {
-      alignItems: 'center',
-      paddingHorizontal: 16,
-    },
+
     successText: {
-      textAlign: 'center',
-      fontWeight: 'bold',
       color: colors.foregroundColor,
     },
     pleaseText: {
-      paddingBottom: 10,
-      paddingRight: 0,
-      paddingLeft: 0,
       color: colors.foregroundColor,
-    },
-    secret: {
-      flex: 1,
-      flexDirection: 'row',
-      justifyContent: 'center',
-      flexWrap: 'wrap',
-      marginTop: 14,
     },
   });
 
@@ -82,11 +50,15 @@ const PleaseBackup = () => {
 
   const renderSecret = () => {
     const component = [];
-    for (const [index, secret] of wallet.getSecret().split(' ').entries()) {
-      component.push(
-        <View style={styles.word} key={`${secret}${index}`}>
-          <Text style={styles.wortText}>{`${index + 1}. ${secret}`}</Text>
-        </View>,
+    for (const [index, secret] of wallet.getSecret().split(/\s/).entries()) {
+      component.push(<>
+        <View style={[styles.word, stylesHook.word]} key={`${secret}${index}`}>
+          <Text style={[styles.wortText, stylesHook.wortText]}>{`${index + 1}. ${secret}`}</Text>
+        </View>
+        <View key={`${secret}${index}`}>
+          <Text>{secret}</Text>
+        </View>
+        </>
       );
     }
     return component;
@@ -97,13 +69,14 @@ const PleaseBackup = () => {
       <ActivityIndicator />
     </View>
   ) : (
-    <SafeBlueArea style={styles.flex}>
+    <SafeBlueArea style={[styles.flex, stylesHook.flex]}>
       <StatusBar barStyle="default" />
       <ScrollView contentContainerStyle={styles.scrollViewContent} testID="PleaseBackupScrollView">
         <View style={styles.please}>
           <BlueText>{wallet.getSecret()}</BlueText>
-          <BlueText style={styles.successText}>{loc.pleasebackup.success}</BlueText>
-          <BlueText style={styles.pleaseText}>{loc.pleasebackup.text}</BlueText>
+          <BlueText>{JSON.stringify(wallet.getSecret().split(/\s/))}</BlueText>
+          <BlueText style={[styles.successText, stylesHook.successText]}>{loc.pleasebackup.success}</BlueText>
+          <BlueText style={[styles.pleaseText, stylesHook.pleaseText]}>{loc.pleasebackup.text}</BlueText>
 
           <View style={styles.secret}>{renderSecret()}</View>
 
@@ -122,6 +95,50 @@ PleaseBackup.navigationOptions = ({ navigation }) => ({
   headerRight: null,
   gestureEnabled: false,
   swipeEnabled: false,
+});
+const styles = StyleSheet.create({
+  flex: {
+    flex: 1,
+  },
+  loading: {
+    flex: 1,
+    paddingTop: 20,
+  },
+  word: {
+    marginRight: 8,
+    marginBottom: 8,
+    paddingTop: 6,
+    paddingBottom: 6,
+    paddingLeft: 8,
+    paddingRight: 8,
+    borderRadius: 4,
+  },
+  wortText: {
+    fontWeight: 'bold',
+  },
+  scrollViewContent: {
+    justifyContent: 'space-between',
+  },
+  please: {
+    alignItems: 'center',
+    paddingHorizontal: 16,
+  },
+  successText: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  pleaseText: {
+    paddingBottom: 10,
+    paddingRight: 0,
+    paddingLeft: 0,
+  },
+  secret: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    marginTop: 14,
+  },
 });
 
 export default PleaseBackup;
