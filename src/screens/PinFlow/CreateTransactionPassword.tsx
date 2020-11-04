7e82119981a40c08cf1669f1a482d1dbb7af3690
Mateusz Mróz <33812970+mateuszpmroz@users.noreply.github.com>
2020-11-04 17:06:06 +0100

    fix visibility icon on transaction confirm (#448)

--- src/screens/PinFlow/CreateTransactionPassword.tsx
+++ src/screens/PinFlow/CreateTransactionPassword.tsx
@@ -116,7 +116,7 @@ const styles = StyleSheet.create({
     paddingTop: 20,
     width: '100%',
   },
-  visibilityIcon: { position: 'absolute', right: 0, bottom: 24, zIndex: 3 },
+  visibilityIcon: { position: 'absolute', right: 0, top: 48, zIndex: 3 },
   icon: {
     width: 24,
     height: 24,
--- src/screens/UnlockTransaction.tsx
+++ src/screens/UnlockTransaction.tsx
@@ -105,8 +105,7 @@ const styles = StyleSheet.create({
   inputContainer: {
     width: '100%',
   },
-  visibilityIcon: { position: 'absolute', right: 0, bottom: 24, zIndex: 3 },
-
+  visibilityIcon: { position: 'absolute', right: 0, top: 28, zIndex: 3 },
   description: {
     ...typography.caption,
     color: palette.textGrey,
