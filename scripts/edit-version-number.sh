bb06e1a433cfc0952db698a3ea8e9edab84a5538
marcosrdz <marcospr@pm.me>
2020-11-08 16:47:04 -0500

    OPS: Version bump

--- scripts/edit-version-number.sh
+++ scripts/edit-version-number.sh
@@ -3,6 +3,9 @@ vim ios/BlueWalletWatch/Info.plist
 vim "ios/BlueWalletWatch Extension/Info.plist"
 vim "ios/TodayExtension/Info.plist"
 vim ios/BlueWallet.xcodeproj/project.pbxproj
+vim ios/WalletInformationWidget/Widgets/MarketWidget/Info.plist 
+vim ios/WalletInformationWidget/Widgets/WalletInformationAndMarketWidget/Info.plist 
+vim ios/WalletInformationWidget/Info.plist 
 vim android/app/build.gradle
 vim package.json
 vim package-lock.json
