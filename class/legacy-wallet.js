1d6a041d85addb242189bc4573151aa2dab5884c
KedziaPawel <67273035+KedziaPawel@users.noreply.github.com>
2020-11-18 13:32:23 +0100

    Improve sentry logging (#500)

--- class/legacy-wallet.js
+++ class/legacy-wallet.js
@@ -218,6 +218,13 @@ export class LegacyWallet extends AbstractWallet {
       if (!transaction) {
         return false;
       }
+
+      if (tx.tx_type === undefined) {
+        logger.warning('legacy-wallet', `outdated tx: ${JSON.stringify(tx)}`);
+
+        logger.info('legacy-wallet', `fetched transaction from electrum x: ${JSON.stringify(transaction)}`);
+      }
+
       return transaction.tx_type === tx.tx_type;
     });
 
@@ -245,6 +252,10 @@ export class LegacyWallet extends AbstractWallet {
       }
       tx.height = txs.find(t => t.tx_hash === tx.txid).height;
       tx.tx_type = findLast(txs, t => t.tx_hash === tx.txid).tx_type;
+
+      if (tx.tx_type === undefined) {
+        logger.warning('legacy-wallet', `couldn't find tx_type for new tx: ${JSON.stringify(tx)}`);
+      }
 
       tx.value = new BigNumber(value).multipliedBy(100000000).toNumber();
       if (tx.time) {
