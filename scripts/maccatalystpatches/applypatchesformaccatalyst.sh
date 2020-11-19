ac1dad1037daf39ca341212a49bfa75436e71ea6
marcosrdz <marcospr@pm.me>
2020-11-19 00:24:06 -0500

    WIP

--- scripts/maccatalystpatches/applypatchesformaccatalyst.sh
+++ scripts/maccatalystpatches/applypatchesformaccatalyst.sh
@@ -1,7 +1,7 @@
 echo "Applying patch for AppDelegate.m"
-patch ios/BlueWallet/AppDelegate.m  ./scripts/maccatalystpatches/appdelegate.patch
+patch ios/BlueWallet/AppDelegate.m  ./scripts/maccatalystpatches/appdelegate.patch --no-backup-if-mismatch
 echo "Applying patch for package.json"
-patch package.json ./scripts/maccatalystpatches/packagejson.patch
+patch package.json ./scripts/maccatalystpatches/packagejson.patch --no-backup-if-mismatch
 echo "Removing node_modules"
 rm -fr node_modules
 echo "Removing ios/Pods"
@@ -12,18 +12,18 @@ echo "Re-installing node_modules"
 npm i
 echo "Applying patch for react-native-camera"
 cd node_modules/react-native-camera/ios/RCT
-patch RCTCameraManager.m ../../../../scripts/maccatalystpatches/RCTCameraManager.patch
+patch RCTCameraManager.m ../../../../scripts/maccatalystpatches/RCTCameraManager.patch --no-backup-if-mismatch
 cd ../RN/
 patch RNCamera.m ../../../../scripts/maccatalystpatches/RNCamera.patch
 echo "Applying patch for react-native-image-picker"
 cd ../../../../
 cd node_modules/react-native-image-picker/ios
-patch ImagePickerManager.m ../../../scripts/maccatalystpatches/ImagePickerManager.patch 
+patch ImagePickerManager.m ../../../scripts/maccatalystpatches/ImagePickerManager.patch --no-backup-if-mismatch
 echo "Applying patch for Podfile"
 cd ../../../
-patch ios/Podfile ./scripts/maccatalystpatches/podfile.patch
+patch ios/Podfile ./scripts/maccatalystpatches/podfile.patch --no-backup-if-mismatch
 echo "Applying patch for realm podspec"
-patch node_modules/realm/RealmJS.podspec ./scripts/maccatalystpatches/realm.patch
+patch node_modules/realm/RealmJS.podspec ./scripts/maccatalystpatches/realm.patch --no-backup-if-mismatch
 cd ios
 pod update
 echo ""
