6bdd88c610fcca16d139c25d3504f5aa2e3cff80
Kamil Lewandowski <kamil2912@gmail.com>
2020-11-20 13:01:31 +0100

    hide biometric switch on not supporting device (#507)

--- src/screens/Settings/SettingsScreen.tsx
+++ src/screens/Settings/SettingsScreen.tsx
@@ -67,17 +67,16 @@ export const SettingsScreen = (props: Props) => {
         iconHeight={20}
         onPress={goToChangePin}
       />
-      <ListItem
-        disabled={!biometryTypeAvailable}
-        title={
-          biometryTypeAvailable ? i18n.settings[BiometricService.biometryType!] : i18n.settings.notSupportedFingerPrint
-        }
-        source={icons.fingerprintIcon}
-        switchValue={isBiometricsEnabled}
-        onSwitchValueChange={onFingerprintLoginChange}
-        iconWidth={17}
-        iconHeight={19}
-      />
+      {biometryTypeAvailable && (
+        <ListItem
+          title={i18n.settings[BiometricService.biometryType!]}
+          source={icons.fingerprintIcon}
+          switchValue={isBiometricsEnabled}
+          onSwitchValueChange={onFingerprintLoginChange}
+          iconWidth={17}
+          iconHeight={19}
+        />
+      )}
     </>
   );
 
