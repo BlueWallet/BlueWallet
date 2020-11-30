e80d9d42ebe039298e372106935ccf3aa2204a8b
Sebastian <72856131+PanRadaCloudbest@users.noreply.github.com>
2020-11-30 09:06:34 +0100

    Fix imports and aliases in project

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,16 @@
 module.exports = {
   presets: ['@babel/preset-typescript', 'module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          app: './src',
+        },
+      },
+    ],
+  ],
   env: {
     test: {
       plugins: ['react-native-config-node/transform'],
