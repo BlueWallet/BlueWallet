5829290501e073a8f145e5e2de83371c95e3ff45
marcosrdz <marcospr@pm.me>
2020-12-04 23:18:31 -0500

    FIX:  Modal on multisig seed #2176

--- screen/wallets/addMultisigStep2.js
+++ screen/wallets/addMultisigStep2.js
@@ -59,7 +59,6 @@ const WalletsAddMultisigStep2 = () => {
   const { m, n, format } = useRoute().params;
 
   const [cosigners, setCosigners] = useState([]); // array of cosigners user provided. if format [cosigner, fp, path]
-  const [isOnCreateButtonEnabled, setIsOnCreateButtonEnabled] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [isMnemonicsModalVisible, setIsMnemonicsModalVisible] = useState(false);
   const [isProvideMnemonicsModalVisible, setIsProvideMnemonicsModalVisible] = useState(false);
@@ -172,11 +171,11 @@ const WalletsAddMultisigStep2 = () => {
     w.generate().then(() => {
       const cosignersCopy = [...cosigners];
       cosignersCopy.push([w.getSecret(), false, false]);
-      LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+      if (Platform.OS !== 'android') LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
       setCosigners(cosignersCopy);
       setVaultKeyData({ keyIndex: cosignersCopy.length, seed: w.getSecret(), xpub: w.getXpub(), isLoading: false });
       setIsMnemonicsModalVisible(true);
-      if (cosignersCopy.length === n) setIsOnCreateButtonEnabled(true);
+
       // filling cache
       setTimeout(() => {
         // filling cache
@@ -265,9 +264,8 @@ const WalletsAddMultisigStep2 = () => {
 
     const cosignersCopy = [...cosigners];
     cosignersCopy.push([xpub, fp, path]);
-    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+    if (Platform.OS !== 'android') LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
     setCosigners(cosignersCopy);
-    if (cosignersCopy.length === n) setIsOnCreateButtonEnabled(true);
   };
 
   const useMnemonicPhrase = () => {
@@ -285,9 +283,9 @@ const WalletsAddMultisigStep2 = () => {
 
     const cosignersCopy = [...cosigners];
     cosignersCopy.push([hd.getSecret(), false, false]);
-    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+    if (Platform.OS !== 'android') LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
     setCosigners(cosignersCopy);
-    if (cosignersCopy.length === n) setIsOnCreateButtonEnabled(true);
+
     setIsProvideMnemonicsModalVisible(false);
     setIsLoading(false);
     setImportText('');
@@ -372,9 +370,8 @@ const WalletsAddMultisigStep2 = () => {
 
       const cosignersCopy = [...cosigners];
       cosignersCopy.push([cosigner.getXpub(), cosigner.getFp(), cosigner.getPath()]);
-      LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+      if (Platform.OS !== 'android') LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
       setCosigners(cosignersCopy);
-      if (cosignersCopy.length === n) setIsOnCreateButtonEnabled(true);
     }
   };
 
@@ -633,7 +630,7 @@ const WalletsAddMultisigStep2 = () => {
     <BlueLoading />
   ) : (
     <View style={styles.buttonBottom}>
-      <BlueButton title={loc.multisig.create} onPress={onCreate} disabled={!isOnCreateButtonEnabled} />
+      <BlueButton title={loc.multisig.create} onPress={onCreate} disabled={cosigners.length !== n} />
     </View>
   );
 
