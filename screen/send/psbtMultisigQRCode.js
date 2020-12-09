d53db68f6c3d419131813e3f5739124fe795811f
marcosrdz <marcospr@pm.me>
2020-12-09 16:34:48 -0500

    FIX: Was unable to use PSBT Multisig on macOS

--- screen/send/psbtMultisigQRCode.js
+++ screen/send/psbtMultisigQRCode.js
@@ -7,9 +7,13 @@ import { SquareButton } from '../../components/SquareButton';
 import { getSystemName } from 'react-native-device-info';
 import loc from '../../loc';
 import ImagePicker from 'react-native-image-picker';
-import ScanQRCode from './ScanQRCode';
 import { useNavigation, useRoute, useTheme } from '@react-navigation/native';
+import RNFS from 'react-native-fs';
+import DocumentPicker from 'react-native-document-picker';
+import { presentCameraNotAuthorizedAlert } from '../../class/camera';
+import ActionSheet from '../ActionSheet';
 const bitcoin = require('bitcoinjs-lib');
+
 const fs = require('../../blue_modules/fs');
 const LocalQRCode = require('@remobile/react-native-qrcode-local-image');
 const isDesktop = getSystemName() === 'Mac OS X';
@@ -41,35 +45,97 @@ const PsbtMultisigQRCode = () => {
     } else if (ret.data.indexOf('+') === -1 && ret.data.indexOf('=') === -1 && ret.data.indexOf('=') === -1) {
       // this looks like NOT base64, so maybe its transaction's hex
       // we dont support it in this flow
+      alert(loc.wallets.import_error);
     } else {
       // psbt base64?
       navigate('PsbtMultisig', { receivedPSBTBase64: ret.data });
     }
   };
 
+  const choosePhoto = () => {
+    ImagePicker.launchImageLibrary(
+      {
+        title: null,
+        mediaType: 'photo',
+        takePhotoButtonTitle: null,
+      },
+      response => {
+        if (response.uri) {
+          const uri = Platform.OS === 'ios' ? response.uri.toString().replace('file://', '') : response.path.toString();
+          LocalQRCode.decode(uri, (error, result) => {
+            if (!error) {
+              onBarScanned(result);
+            } else {
+              alert(loc.send.qr_error_no_qrcode);
+            }
+          });
+        }
+      },
+    );
+  };
+
+  const takePhoto = () => {
+    ImagePicker.launchCamera(
+      {
+        title: null,
+        mediaType: 'photo',
+        takePhotoButtonTitle: null,
+      },
+      response => {
+        if (response.uri) {
+          const uri = Platform.OS === 'ios' ? response.uri.toString().replace('file://', '') : response.path.toString();
+          LocalQRCode.decode(uri, (error, result) => {
+            if (!error) {
+              onBarScanned(result);
+            } else {
+              alert(loc.send.qr_error_no_qrcode);
+            }
+          });
+        } else if (response.error) {
+          presentCameraNotAuthorizedAlert(response.error);
+        }
+      },
+    );
+  };
+
+  const handleImportFileButtonPressed = async () => {
+    try {
+      const res = await DocumentPicker.pick({
+        type: [DocumentPicker.types.allFiles],
+      });
+
+      const file = await RNFS.readFile(res.uri);
+      if (file) {
+        onBarScanned(file);
+      } else {
+        throw new Error();
+      }
+    } catch (err) {
+      if (!DocumentPicker.isCancel(err)) {
+        alert(loc.wallets.import_error);
+      }
+    }
+  };
+
+  const showActionSheet = async () => {
+    if (Platform.OS === 'ios') {
+      const options = [loc._.cancel, 'Take Photo', loc.wallets.list_long_choose, loc.wallets.import_file];
+
+      ActionSheet.showActionSheetWithOptions({ options, cancelButtonIndex: 0 }, buttonIndex => {
+        if (buttonIndex === 1) {
+          takePhoto();
+        } else if (buttonIndex === 2) {
+          choosePhoto();
+        } else if (buttonIndex === 3) {
+          handleImportFileButtonPressed();
+        }
+      });
+    }
+  };
+
   const openScanner = () => {
     if (isDesktop) {
-      ImagePicker.launchCamera(
-        {
-          title: null,
-          mediaType: 'photo',
-          takePhotoButtonTitle: null,
-        },
-        response => {
-          if (response.uri) {
-            const uri = Platform.OS === 'ios' ? response.uri.toString().replace('file://', '') : response.path.toString();
-            LocalQRCode.decode(uri, (error, result) => {
-              if (!error) {
-                onBarScanned(result);
-              } else {
-                alert(loc.send.qr_error_no_qrcode);
-              }
-            });
-          } else if (response.error) {
-            ScanQRCode.presentCameraNotAuthorizedAlert(response.error);
-          }
-        },
-      );
+      showActionSheet();
     } else {
       navigate('ScanQRCodeRoot', {
         screen: 'ScanQRCode',
