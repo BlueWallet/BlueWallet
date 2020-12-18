5d8f08b7a85ae2bd3bc985b6270c5164bdd76991
marcosrdz <marcospr@pm.me>
2020-12-18 17:01:11 -0500

    ADD: Privacy settings for iOS 14 widgets

--- screen/settings/SettingsPrivacy.js
+++ screen/settings/SettingsPrivacy.js
@@ -1,18 +1,22 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { ScrollView, TouchableWithoutFeedback, StyleSheet, Linking } from 'react-native';
-import { BlueText, BlueSpacing20, BlueListItem, BlueNavigationStyle, BlueCard } from '../../BlueComponents';
+import { ScrollView, TouchableWithoutFeedback, StyleSheet, Linking, Platform } from 'react-native';
+import { BlueText, BlueSpacing20, BlueListItem, BlueNavigationStyle, BlueCard, BlueHeaderDefaultSub } from '../../BlueComponents';
 import { useTheme } from '@react-navigation/native';
 import loc from '../../loc';
 import BlueClipboard from '../../blue_modules/clipboard';
 import DeviceQuickActions from '../../class/quick-actions';
 import { BlueStorageContext } from '../../blue_modules/storage-context';
 
+import WidgetCommunication from '../../blue_modules/WidgetCommunication';
+
 const SettingsPrivacy = () => {
   const { colors } = useTheme();
   const { isStorageEncrypted } = useContext(BlueStorageContext);
-  const sections = Object.freeze({ ALL: 0, CLIPBOARDREAD: 1, QUICKACTION: 2 });
+  const sections = Object.freeze({ ALL: 0, CLIPBOARDREAD: 1, QUICKACTION: 2, WIDGETS: 3 });
   const [isLoading, setIsLoading] = useState(sections.ALL);
   const [isReadClipboardAllowed, setIsReadClipboardAllowed] = useState(false);
+
+  const [isDisplayWidgetBalanceAllowed, setIsDisplayWidgetBalanceAllowed] = useState(false);
   const [isQuickActionsEnabled, setIsQuickActionsEnabled] = useState(false);
   const [storageIsEncrypted, setStorageIsEncrypted] = useState(true);
 
@@ -22,6 +26,7 @@ const SettingsPrivacy = () => {
         setIsReadClipboardAllowed(await BlueClipboard.isReadClipboardAllowed());
         setStorageIsEncrypted(await isStorageEncrypted());
         setIsQuickActionsEnabled(await DeviceQuickActions.getEnabled());
+        setIsDisplayWidgetBalanceAllowed(await WidgetCommunication.isBalanceDisplayAllowed());
       } catch (e) {
         console.log(e);
       }
@@ -52,6 +57,17 @@ const SettingsPrivacy = () => {
     setIsLoading(false);
   };
 
+  const onWidgetsTotalBalanceValueChange = async value => {
+    setIsLoading(sections.WIDGETS);
+    try {
+      await WidgetCommunication.setBalanceDisplayAllowed(value);
+      setIsDisplayWidgetBalanceAllowed(value);
+    } catch (e) {
+      console.log(e);
+    }
+    setIsLoading(false);
+  };
+
   const stylesWithThemeHook = StyleSheet.create({
     root: {
       backgroundColor: colors.background,
@@ -64,6 +80,7 @@ const SettingsPrivacy = () => {
 
   return (
     <ScrollView style={[styles.root, stylesWithThemeHook.root]}>
+      <BlueHeaderDefaultSub leftText={loc.settings.general} rightComponent={null} />
       <BlueListItem
         hideChevron
         title={loc.settings.privacy_read_clipboard}
@@ -84,6 +101,24 @@ const SettingsPrivacy = () => {
           />
           <BlueCard>
             <BlueText>{loc.settings.privacy_quickactions_explanation}</BlueText>
+          </BlueCard>
+        </>
+      )}
+      {Platform.OS === 'ios' && !storageIsEncrypted && (
+        <>
+          <BlueHeaderDefaultSub leftText={loc.settings.widgets} rightComponent={null} />
+          <BlueListItem
+            hideChevron
+            title={loc.settings.total_balance}
+            Component={TouchableWithoutFeedback}
+            switch={{
+              onValueChange: onWidgetsTotalBalanceValueChange,
+              value: isDisplayWidgetBalanceAllowed,
+              disabled: isLoading === sections.ALL,
+            }}
+          />
+          <BlueCard>
+            <BlueText>{loc.settings.total_balance_explanation}</BlueText>
           </BlueCard>
         </>
       )}
