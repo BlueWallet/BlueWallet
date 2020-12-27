9404b6b2b728b55b3e32d2578686f0b14ab50fd8
marcosrdz <marcospr@pm.me>
2020-12-27 18:52:20 -0500

    OPS: Update Privacy Blur package on iOS to remove RN Warnings

--- Privacy.js
+++ Privacy.js
@@ -1,13 +1,12 @@
 import Obscure from 'react-native-obscure';
 import { Platform } from 'react-native';
-const PrivacySnapshot = require('react-native-privacy-snapshot');
-
+import { enabled } from 'react-native-privacy-snapshot';
 export default class Privacy {
   static enableBlur() {
-    Platform.OS === 'android' ? Obscure.activateObscure() : PrivacySnapshot.enabled(true);
+    Platform.OS === 'android' ? Obscure.activateObscure() : enabled(true);
   }
 
   static disableBlur() {
-    Platform.OS === 'android' ? Obscure.deactivateObscure() : PrivacySnapshot.enabled(false);
+    Platform.OS === 'android' ? Obscure.deactivateObscure() : enabled(false);
   }
 }
