6c47651d63d66efb1bf8963e0092ab04e16855da
Overtorment <overtorment@gmail.com>
2021-02-12 23:34:28 +0000

    TST: mute warnings

--- class/wallets/abstract-wallet.js
+++ class/wallets/abstract-wallet.js
@@ -170,7 +170,6 @@ export class AbstractWallet {
         try {
           parsedSecret = JSON.parse(this.secret);
         } catch (e) {
-          console.log(e);
           parsedSecret = JSON.parse(newSecret);
         }
       } else {
