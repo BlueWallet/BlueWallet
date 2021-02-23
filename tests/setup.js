96b5beecdd0c6911601a7a3e85d0227ee8ae890d
Marcos Rodriguez Vélez <marcospr@pm.me>
2021-02-22 22:15:47 -0500

    Update setup.js

--- tests/setup.js
+++ tests/setup.js
@@ -1,4 +1,7 @@
 /* global jest */
+import mockRNCNetInfo from '@react-native-community/netinfo/jest/netinfo-mock.js';
+
+jest.mock('@react-native-community/netinfo', () => mockRNCNetInfo);
 
 jest.mock('react-native-watch-connectivity', () => {
   return {
