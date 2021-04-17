4deb9e3f7dae6bdde1b00cd1d63069b1213c3c36
marcosrdz <marcospr@pm.me>
2021-04-17 16:08:31 -0400

    ADD: Tooltip for address list

--- components/addresses/AddressItem.js
+++ components/addresses/AddressItem.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { StyleSheet, Text } from 'react-native';
 import { useTheme } from '@react-navigation/native';
 import { ListItem } from 'react-native-elements';
 import PropTypes from 'prop-types';
 import { AddressTypeBadge } from './AddressTypeBadge';
-import { formatBalance } from '../../loc';
+import loc, { formatBalance } from '../../loc';
+import TooltipMenu from '../TooltipMenu';
+import Clipboard from '@react-native-clipboard/clipboard';
+import Share from 'react-native-share';
 
 const AddressItem = ({ item, balanceUnit, onPress }) => {
   const { colors } = useTheme();
+  const tooltip = useRef();
+  const listItem = useRef();
 
   const styles = StyleSheet.create({
     container: {
@@ -32,19 +37,56 @@ const AddressItem = ({ item, balanceUnit, onPress }) => {
     },
   });
 
+  const showToolTipMenu = () => {
+    tooltip.current.showMenu();
+  };
+
   const balance = formatBalance(item.balance, balanceUnit, true);
+
+  const handleCopyPress = () => {
+    Clipboard.setString(item.address);
+  };
+
+  const handleSharePress = () => {
+    Share.open({ message: item.address }).catch(error => console.log(error));
+  };
 
   const render = () => {
     return (
-      <ListItem key={`${item.key}`} button onPress={onPress} containerStyle={styles.container}>
-        <ListItem.Content style={styles.list}>
-          <ListItem.Title style={styles.list} numberOfLines={1} ellipsizeMode="middle">
-            <Text style={styles.index}>{item.index}</Text> <Text style={styles.address}>{item.address}</Text>
-          </ListItem.Title>
-          <ListItem.Subtitle style={[styles.list, styles.balance]}>{balance}</ListItem.Subtitle>
-        </ListItem.Content>
-        <AddressTypeBadge isInternal={item.isInternal} />
-      </ListItem>
+      <>
+        <TooltipMenu
+          ref={tooltip}
+          anchorRef={listItem}
+          actions={[
+            {
+              id: 'copyToClipboard',
+              text: loc.transactions.details_copy,
+              onPress: handleCopyPress,
+            },
+            {
+              id: 'share',
+              text: loc.receive.details_share,
+              onPress: handleSharePress,
+            },
+          ]}
+        />
+        <ListItem
+          ref={listItem}
+          key={`${item.key}`}
+          button
+          onPress={onPress}
+          containerStyle={styles.container}
+          onLongPress={showToolTipMenu}
+        >
+          <ListItem.Content style={styles.list}>
+            <ListItem.Title style={styles.list} numberOfLines={1} ellipsizeMode="middle">
+              <Text style={styles.index}>{item.index}</Text> <Text style={styles.address}>{item.address}</Text>
+            </ListItem.Title>
+            <ListItem.Subtitle style={[styles.list, styles.balance]}>{balance}</ListItem.Subtitle>
+          </ListItem.Content>
+          <AddressTypeBadge isInternal={item.isInternal} />
+        </ListItem>
+      </>
     );
   };
 
