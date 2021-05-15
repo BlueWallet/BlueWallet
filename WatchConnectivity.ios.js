d93d36713bfce67274a1c1ee8e9b402551d39c1a
marcosrdz <marcosrodriguez@me.com>
2021-05-14 23:24:04 -0400

    FIX: Applied xpub to wrong object

--- WatchConnectivity.ios.js
+++ WatchConnectivity.ios.js
@@ -69,11 +69,17 @@ function WatchConnectivity() {
     if (message.request === 'createInvoice') {
       handleLightningInvoiceCreateRequest(message.walletIndex, message.amount, message.description)
         .then(createInvoiceRequest => reply({ invoicePaymentRequest: createInvoiceRequest }))
-        .catch(e => console.log(e));
+        .catch(e => {
+          console.log(e);
+          reply({});
+        });
     } else if (message.message === 'sendApplicationContext') {
       sendWalletsToWatch();
+      reply({});
     } else if (message.message === 'fetchTransactions') {
-      fetchWalletTransactions().then(() => saveToDisk());
+      fetchWalletTransactions()
+        .then(() => saveToDisk())
+        .finally(() => reply({}));
     } else if (message.message === 'hideBalance') {
       const walletIndex = message.walletIndex;
       const wallet = wallets[walletIndex];
@@ -207,7 +213,7 @@ function WatchConnectivity() {
         hideBalance: wallet.hideBalance,
       };
       if (wallet.chain === Chain.ONCHAIN && wallet.type !== MultisigHDWallet.type) {
-        walletInformation.push({ xpub: wallet.getXpub() ? wallet.getXpub() : wallet.getSecret() });
+        walletInformation.xpub = wallet.getXpub() ? wallet.getXpub() : wallet.getSecret();
       }
       walletsToProcess.push(walletInformation);
     }
