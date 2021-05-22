98231b91b93381d8b5bb039ac10fc88cbe7c3124
marcosrdz <marcosrodriguez@me.com>
2021-05-22 00:44:08 -0400

    ADD: Abbreviated Price display

--- ios/WalletInformationWidget/Widgets/Shared/WidgetDataStore.swift
+++ ios/WalletInformationWidget/Widgets/Shared/WidgetDataStore.swift
@@ -8,6 +8,23 @@
 
 import Foundation
 
+extension Numeric {
+    
+    var abbreviated: String {
+      let bytecountFormatter = ByteCountFormatter()
+      bytecountFormatter.zeroPadsFractionDigits = true
+      bytecountFormatter.countStyle = .decimal
+      bytecountFormatter.allowsNonnumericFormatting = true
+      let bytesString = bytecountFormatter.string(fromByteCount: (self as! NSNumber).int64Value)
+      
+        let numericString = bytesString
+            .replacingOccurrences(of: "bytes", with: "")
+            .replacingOccurrences(of: "B", with: "") // removes B (bytes) in 'KB'/'MB'/'GB'
+            .replacingOccurrences(of: "G", with: "B") // replace G (Giga) to just B (billions)
+        return numericString.replacingOccurrences(of: " ", with: "")
+    }
+}
+
 struct WidgetDataStore: Codable {
   let rate: String
   let lastUpdate: String
@@ -23,19 +40,21 @@ struct WidgetDataStore: Codable {
     }
     return rate
   }
+  var formattedRateForSmallComplication: String? {
+    return rateDouble.abbreviated
+  }
+  
   var formattedRateForComplication: String? {
     let numberFormatter = NumberFormatter()
     numberFormatter.locale = Locale(identifier: WidgetAPI.getUserPreferredCurrencyLocale())
     numberFormatter.numberStyle = .currency
-    numberFormatter.usesSignificantDigits = true
-    numberFormatter.maximumFractionDigits = 0
-    numberFormatter.minimumFractionDigits = 0
     numberFormatter.currencySymbol = ""
     if let rateString = numberFormatter.string(from: NSNumber(value: rateDouble)) {
       return rateString
     }
     return rate
   }
+  
   var date: Date? {
     let isoDateFormatter = ISO8601DateFormatter()
     let dateFormatter = DateFormatter()
