6ab6e8444f3f291871737bd1100a875f268580e7
Overtorment <overtorment@gmail.com>
2021-06-02 17:33:37 +0100

    TST: add mock for rn keystore

--- tests/setup.js
+++ tests/setup.js
@@ -141,4 +141,14 @@ jest.mock('react-native-share', () => {
   };
 });
 
+const keychainMock = {
+  SECURITY_LEVEL_ANY: 'MOCK_SECURITY_LEVEL_ANY',
+  SECURITY_LEVEL_SECURE_SOFTWARE: 'MOCK_SECURITY_LEVEL_SECURE_SOFTWARE',
+  SECURITY_LEVEL_SECURE_HARDWARE: 'MOCK_SECURITY_LEVEL_SECURE_HARDWARE',
+  setGenericPassword: jest.fn().mockResolvedValue(),
+  getGenericPassword: jest.fn().mockResolvedValue(),
+  resetGenericPassword: jest.fn().mockResolvedValue(),
+};
+jest.mock('react-native-keychain', () => keychainMock);
+
 global.alert = () => {};
