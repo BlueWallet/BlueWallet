630afd1b5436c8bdce56a92d04c82950c68c6515
Marcos Rodriguez Vélez <marcosrodriguez@me.com>
2021-07-02 14:25:50 -0400

    Update updateMinSdkVersion.sh

--- scripts/updateMinSdkVersion.sh
+++ scripts/updateMinSdkVersion.sh
@@ -1,6 +1,6 @@
 # React Native 64.0 requires all Android packages to have a minSdkVersion of at least 21
-sed -i 's/minSdkVersion 16/minSdkVersion 21/' node_modules/react-native-secure-key-store/android/build.gradle
-sed -i 's/minSdkVersion 16/minSdkVersion 21/' node_modules/@remobile/react-native-qrcode-local-image/android/build.gradle
-sed -i 's/minSdkVersion 16/minSdkVersion 21/' node_modules/react-native-tor/android/build.gradle
-sed -i 's/minSdkVersion 16/minSdkVersion 21/' node_modules/react-native-gesture-handler/android/lib/build.gradle
+sed -i '' "s/minSdkVersion 16/minSdkVersion 21/" node_modules/react-native-secure-key-store/android/build.gradle
+sed -i '' "s/minSdkVersion 16/minSdkVersion 21/" node_modules/@remobile/react-native-qrcode-local-image/android/build.gradle
+sed -i '' "s/minSdkVersion 16/minSdkVersion 21/" node_modules/react-native-tor/android/build.gradle
+sed -i '' "s/minSdkVersion 16/minSdkVersion 21/" node_modules/react-native-gesture-handler/android/lib/build.gradle
 
