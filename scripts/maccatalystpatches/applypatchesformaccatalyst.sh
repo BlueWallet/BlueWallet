ece7475f0d4537db62764677210e07e0118a3875
Marcos Rodriguez Vélez <marcosrodriguez@me.com>
2021-07-09 00:04:50 -0400

    Update applypatchesformaccatalyst.sh

--- scripts/maccatalystpatches/applypatchesformaccatalyst.sh
+++ scripts/maccatalystpatches/applypatchesformaccatalyst.sh
@@ -3,4 +3,4 @@ sed -i '' '/react-native-tor/d' ./package.json
 echo "Re-installing node_modules"
 npm i
 echo ""
-echo "react-native-tor is not currently compatible with Mac Catalyst. You will need to remove all references from torrific.js. After this, you should now be able to compile BlueWallet using Mac Catalyst on XCode. If you are running macOS Catalina, you will need to remove the iOS 14 Widgets from the project targets."
+echo "react-native-tor is not currently compatible with Mac Catalyst. You will need to remove all references from torrific.js. After this, you should be able to compile BlueWallet using Mac Catalyst on XCode. If you are running macOS Catalina, you will need to remove the iOS 14 Widgets from the project targets."
