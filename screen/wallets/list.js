e09d5069eb1b3b08826df3909600f89f8e19d1c8
Marcos Rodriguez Vélez <marcosrodriguez@me.com>
2021-07-17 15:43:55 -0400

    Update list.js

--- screen/wallets/list.js
+++ screen/wallets/list.js
@@ -171,13 +171,15 @@ const WalletsList = () => {
 
     const contentOffset = e.nativeEvent.contentOffset;
     const index = Math.ceil(contentOffset.x / width);
-    console.log('onSnapToItem', index);
+    console.log('onSnapToItem', wallets.length === index ? 'NewWallet/Importing card' : index);
 
     if (currentWalletIndex.current !== index) {
       if (wallets[index] && (wallets[index].timeToRefreshBalance() || wallets[index].timeToRefreshTransaction())) {
         console.log(wallets[index].getLabel(), 'thinks its time to refresh either balance or transactions. refetching both');
         refreshAllWalletTransactions(index, false).finally(() => setIsLoading(false));
       }
+    } else {
+      console.log('onSnapToItem did not change. Most likely momentum stopped at the same index it started.');
     }
     currentWalletIndex.current = index;
   };
