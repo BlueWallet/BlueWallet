3afbf5df6d19b67ff2d40109c21a0bba31377e73
Gabriele Genta <gabriele.genta@gmail.com>
2021-07-26 15:40:37 +0200

    REF: convert LegacyWallet to typescript

---
+++ blue_modules/BlueElectrum.d.ts
@@ -0,0 +1,71 @@
+type Utxo = {
+  height: number;
+  value: number;
+  address: string;
+  txId: string;
+  vout: number;
+  wif?: string;
+};
+
+type Transaction = {
+  txid: string;
+  hash: string;
+  version: number;
+  size: number;
+  vsize: number;
+  weight: number;
+  locktime: number;
+  vin: {
+    txid: string;
+    vout: number;
+    scriptSig: { asm: string; hex: string };
+    txinwitness: string[];
+    sequence: number;
+  }[];
+  vout: {
+    value: number;
+    n: number;
+    scriptPubKey: {
+      asm: string;
+      hex: string;
+      reqSigs: number;
+      type: string;
+      addresses: string[];
+    };
+  }[];
+  blockhash: string;
+  confirmations: number;
+  time: number;
+  blocktime: number;
+};
+
+export function getBalanceByAddress(address: string): Promise<{ confirmed: number; unconfirmed: number }>;
+
+export function multiGetUtxoByAddress(addresses: string[]): Promise<Record<string, Utxo[]>>;
+
+// TODO: this function returns different results based on the value of `verbose`, consider splitting it into two
+export function multiGetTransactionByTxid(
+  txIds: string[],
+  batchsize: number = 45,
+  verbose: true = true,
+): Promise<Record<string, Transaction>>;
+export function multiGetTransactionByTxid(txIds: string[], batchsize: number, verbose: false): Promise<Record<string, string>>;
+
+export function getTransactionsByAddress(address: string): Transaction[];
+
+export function estimateCurrentBlockheight(): number;
+
+export function multiGetHistoryByAddress(
+  addresses: string[],
+): Promise<
+  Record<
+    string,
+    {
+      tx_hash: string; // eslint-disable-line camelcase
+      height: number;
+      address: string;
+    }[]
+  >
+>;
+
+export function broadcastV2(txhex: string): Promise<string>;
