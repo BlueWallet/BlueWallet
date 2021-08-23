000f75ee2914010bd93edc6062c15f89e40c4260
Marcos Rodriguez Vélez <marcosrodriguez@me.com>
2021-08-23 15:39:52 -0400

    REF: Use ContextButton for advanced options

--- components/TooltipMenu.ios.js
+++ components/TooltipMenu.ios.js
@@ -2,30 +2,42 @@ import React from 'react';
 import { ContextMenuView, ContextMenuButton } from 'react-native-ios-context-menu';
 import PropTypes from 'prop-types';
 
-const ToolTipMenu = (props, ref) => {
-  const menuItems = props.actions.map(action => ({
-    actionKey: action.id,
-    actionTitle: action.text,
-    actionOnPress: action.onPress,
-    icon: action.icon,
-    menuOptions: action.menuOptions,
-    menuTitle: action.menuTitle,
-  }));
+const ToolTipMenu = props => {
+  const menuItems = props.actions.map(action => {
+    const item = {
+      actionKey: action.id,
+      actionTitle: action.text,
+      actionOnPress: action.onPress,
+      icon: action.icon,
+      menuOptions: action.menuOptions,
+      menuTitle: action.menuTitle,
+    };
+    if (action.menuStateOn) {
+      item.menuState = action.menuStateOn ? 'on' : 'off';
+    }
+    if (action.disabled) {
+      item.menuAttributes = ['disabled'];
+    }
+    return item;
+  });
   const menuTitle = props.title ?? '';
   const submenu = props.submenu;
-  const isButton  = props.isButton ? true : false;
+  const isButton = !!props.isButton;
   const isMenuPrimaryAction = props.isMenuPrimaryAction ? props.isMenuPrimaryAction : false;
-  return isButton ? 
-  <ContextMenuButton 
-  onPressMenuItem={({ nativeEvent }) => {
-    props.onPress(nativeEvent.actionKey);
-  }}
-  isMenuPrimaryAction={isMenuPrimaryAction}
-  menuConfig={{
-    menuTitle,
-    menuItems: menuItems.concat(submenu),
-  }}
-  >{props.children}</ContextMenuButton>: (
+  return isButton ? (
+    <ContextMenuButton
+      onPressMenuItem={({ nativeEvent }) => {
+        props.onPress(nativeEvent.actionKey);
+      }}
+      isMenuPrimaryAction={isMenuPrimaryAction}
+      menuConfig={{
+        menuTitle,
+        menuItems: menuItems.concat(submenu),
+      }}
+    >
+      {props.children}
+    </ContextMenuButton>
+  ) : (
     <ContextMenuView
       onPressMenuItem={({ nativeEvent }) => {
         props.onPress(nativeEvent.actionKey);
@@ -47,4 +59,6 @@ ToolTipMenu.propTypes = {
   submenu: PropTypes.object,
   children: PropTypes.node.isRequired,
   onPress: PropTypes.func.isRequired,
+  isMenuPrimaryAction: PropTypes.bool,
+  isButton: PropTypes.bool,
 };
