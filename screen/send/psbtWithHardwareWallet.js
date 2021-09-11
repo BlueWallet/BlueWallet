d88bb5b7b6e99cdd5d6290c6e82a39f62d139d31
Marcos Rodriguez Vélez <marcosrodriguez@me.com>
2021-09-11 13:46:01 -0400

    FIX: Pause QRCode during Export

--- screen/send/psbtWithHardwareWallet.js
+++ screen/send/psbtWithHardwareWallet.js
@@ -41,6 +41,7 @@ const PsbtWithHardwareWallet = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [txHex, setTxHex] = useState(route.params.txhex);
   const openScannerButton = useRef();
+  const dynamicQRCode = useRef();
 
   const stylesHook = StyleSheet.create({
     root: {
@@ -185,7 +186,10 @@ const PsbtWithHardwareWallet = () => {
 
   const exportPSBT = () => {
     const fileName = `${Date.now()}.psbt`;
-    fs.writeFileAndExport(fileName, typeof psbt === 'string' ? psbt : psbt.toBase64());
+    dynamicQRCode.current?.stopAutoMove();
+    fs.writeFileAndExport(fileName, typeof psbt === 'string' ? psbt : psbt.toBase64()).finally(() => {
+      dynamicQRCode.current?.startAutoMove();
+    });
   };
 
   const openSignedTransaction = async () => {
@@ -237,7 +241,7 @@ const PsbtWithHardwareWallet = () => {
             <Text testID="PSBTHex" style={styles.hidden}>
               {psbt.toHex()}
             </Text>
-            <DynamicQRCode value={psbt.toHex()} />
+            <DynamicQRCode value={psbt.toHex()} ref={dynamicQRCode} />
             <BlueSpacing20 />
             <SecondButton
               testID="PsbtTxScanButton"
--- screen/wallets/exportMultisigCoordinationSetup.js
+++ screen/wallets/exportMultisigCoordinationSetup.js
@@ -17,6 +17,7 @@ const ExportMultisigCoordinationSetup = () => {
   const { wallets } = useContext(BlueStorageContext);
   const wallet = wallets.find(w => w.getID() === walletId);
   const qrCodeContents = useRef();
+  const dynamicQRCode = useRef();
   const [isLoading, setIsLoading] = useState(true);
   const [isShareButtonTapped, setIsShareButtonTapped] = useState(false);
   const { goBack } = useNavigation();
@@ -37,9 +38,11 @@ const ExportMultisigCoordinationSetup = () => {
 
   const exportTxtFile = async () => {
     setIsShareButtonTapped(true);
+    dynamicQRCode.current?.stopAutoMove();
     setTimeout(() => {
       fs.writeFileAndExport(wallet.getLabel() + '.txt', wallet.getXpub()).finally(() => {
         setIsShareButtonTapped(false);
+        dynamicQRCode.current?.startAutoMove();
       });
     }, 10);
   };
@@ -79,7 +82,7 @@ const ExportMultisigCoordinationSetup = () => {
           <BlueText style={[styles.type, stylesHook.type]}>{wallet.getLabel()}</BlueText>
         </View>
         <BlueSpacing20 />
-        <DynamicQRCode value={qrCodeContents.current} />
+        <DynamicQRCode value={qrCodeContents.current} ref={dynamicQRCode} />
         <BlueSpacing20 />
         {isShareButtonTapped ? (
           <ActivityIndicator />
