4ac665d22b9ede622030429e6f8a344a121b2e69
Ivan Vershigora <ivan.vershigora@gmail.com>
2022-04-24 21:21:37 +0200

    TST: jest27, detox to dev deps, remove jasmine

--- tests/e2e/init.js
+++ tests/e2e/init.js
@@ -1,29 +1,9 @@
 const detox = require('detox');
 
-// Set the default timeout
-// jest.setTimeout(1200000); // 20 min
-
 beforeAll(async () => {
-  // await detox.init(config);
   await device.launchApp();
 });
 
-// beforeEach(async () => {
-//   const lockFile = '/tmp/travislock.' + hashIt(expect.getState().currentTestName);
-//   if (process.env.TRAVIS) {
-//     if (require('fs').existsSync(lockFile))
-//       // speeds up test pass
-//       return;
-//   }
-//   await adapter.beforeEach();
-// });
-
 afterAll(async () => {
-  // await adapter.afterAll();
   await detox.cleanup();
 });
-
-// function hashIt(s) {
-//   const createHash = require('create-hash');
-//   return createHash('sha256').update(s).digest().toString('hex');
-// }
