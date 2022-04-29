4d5bc644f5985afb0dc240f9766dab9198150365
Overtorment <overtorment@gmail.com>
2022-04-29 13:56:43 +0100

    OPS: build & sign release APK on GA

---
+++ scripts/build-release-apk.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+# assumes 2 env variables: KEYSTORE_FILE_HEX & KEYSTORE_PASSWORD
+#
+# PS. to turn file to hex and back:
+#     $ xxd -plain test.txt > test.hex
+#     $ xxd -plain -revert test.hex test2.txt
+
+
+echo $KEYSTORE_FILE_HEX > bluewallet-release-key.keystore.hex
+xxd -plain -revert bluewallet-release-key.keystore.hex > ./android/bluewallet-release-key.keystore
+rm bluewallet-release-key.keystore.hex
+
+cd android
+TIMESTAMP=$(date +%s)
+sed -i "s/versionCode 1/versionCode $TIMESTAMP/g" app/build.gradle
+./gradlew assembleRelease
+apksigner sign --ks ./bluewallet-release-key.keystore   --ks-pass=pass:$KEYSTORE_PASSWORD ./app/build/outputs/apk/release/app-release-unsigned.apk
+mv ./app/build/outputs/apk/release/app-release-unsigned.apk ./app/build/outputs/apk/release/app-release.apk
+rm bluewallet-release-key.keystore
+cd ..
+
