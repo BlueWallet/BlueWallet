b772fc6ac7f33760256c4c2c2949e7433a7c0eb3
Overtorment <overtorment@gmail.com>
2022-04-30 13:27:01 +0100

    OPS: minor scripts fix

--- scripts/current-branch.sh
+++ scripts/current-branch.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
+if [ -n "$GITHUB_HEAD_REF" ]; then
+  echo \"$GITHUB_HEAD_REF\"
+  exit
+fi
+
+
 BRANCH1=`git log -n 1 --pretty=%d HEAD | awk '{print $2}' | sed 's/origin\///' | sed 's/)//'`
 if [ "$BRANCH1" = '->' ]
 then
