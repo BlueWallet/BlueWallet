8a68a51fb90b4d832862c936dae07161024774c5
Overtorment <overtorment@gmail.com>
2022-04-30 13:23:09 +0100

    WIP

--- scripts/current-branch.sh
+++ scripts/current-branch.sh
@@ -1,19 +1,15 @@
 #!/bin/sh
-echo a
-git log -n 1 --pretty=%d HEAD | awk '{print $2}' | sed 's/origin\///' | sed 's/)//'
-echo b
-git rev-parse --abbrev-ref HEAD
-echo c
-git branch --show-current
-echo d
-git branch | grep \*
-echo e
-git symbolic-ref --short HEAD
-echo d
-git rev-parse --abbrev-ref HEAD
-echo e
-echo $GITHUB_REF_NAME
-echo f
-echo $GITHUB_HEAD_REF
-echo g
-echo $GITHUB_REF
+
+if [ -n "$GITHUB_HEAD_REF" ]; then
+  echo \"GITHUB_HEAD_REF\"
+  exit
+fi
+
+
+BRANCH1=`git log -n 1 --pretty=%d HEAD | awk '{print $2}' | sed 's/origin\///' | sed 's/)//'`
+if [ "$BRANCH1" = '->' ]
+then
+  BRANCH1=`git rev-parse --abbrev-ref HEAD`
+fi
+
+echo \"$BRANCH1\"
