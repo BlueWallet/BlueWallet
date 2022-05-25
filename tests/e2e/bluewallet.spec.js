678117b7426b37fa36fd8f8c30520ab355b2d98c
Overtorment <overtorment@gmail.com>
2022-05-25 20:22:04 +0100

    WIP

--- tests/e2e/bluewallet.spec.js
+++ tests/e2e/bluewallet.spec.js
@@ -13,7 +13,7 @@ const bitcoin = require('bitcoinjs-lib');
 const assert = require('assert');
 
 describe('BlueWallet UI Tests - no wallets', () => {
-  it.only('selftest passes', async () => {
+  it('selftest passes', async () => {
     const lockFile = '/tmp/travislock.' + hashIt('t1');
     if (process.env.TRAVIS) {
       if (require('fs').existsSync(lockFile)) return console.warn('skipping', JSON.stringify('t1'), 'as it previously passed on Travis');
@@ -36,7 +36,7 @@ describe('BlueWallet UI Tests - no wallets', () => {
     process.env.TRAVIS && require('fs').writeFileSync(lockFile, '1');
   });
 
-  it('all settings screens work', async () => {
+  it.only('all settings screens work', async () => {
     const lockFile = '/tmp/travislock.' + hashIt('t2');
     if (process.env.TRAVIS) {
       if (require('fs').existsSync(lockFile)) return console.warn('skipping', JSON.stringify('t2'), 'as it previously passed on Travis');
