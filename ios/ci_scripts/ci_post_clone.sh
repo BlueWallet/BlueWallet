5a6f78534d1c803451a8e9d6dfa2049d2b3aef11
Marcos Rodriguez Vélez <marcospr@pm.me>
2022-06-26 16:18:56 -0400

    Update ci_post_clone.sh

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -8,9 +8,9 @@ brew install node@16
 brew link node@16
 
 # Install dependencies you manage with CocoaPods.
+cd /Volumes/workspace/repository/ios/
 npm install
 cd ios
 pod install
-cd ..
 # the sed command from RN cant find the file... so we have to run it ourselves
 sed -i -e  $'s/ && (__IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0)//' /Volumes/workspace/repository/ios/Pods/RCT-Folly/folly/portability/Time.h
