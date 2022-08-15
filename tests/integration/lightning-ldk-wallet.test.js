2c56d4fdca2edb372f9f723004d985cc471bb36a
Overtorment <overtorment@gmail.com>
2022-08-15 14:44:57 +0100

    TST: script to recover LDK bytes for autopsy

---
+++ tests/integration/lightning-ldk-wallet.test.js
@@ -0,0 +1,19 @@
+import { LightningLdkWallet } from '../../class';
+import SyncedAsyncStorage from '../../class/synced-async-storage';
+const assert = require('assert');
+
+describe('', () => {
+  it.skip('can import & dump LDK bytes from network storage  ', async () => {
+    const ldk = new LightningLdkWallet();
+    ldk.setSecret('');
+    assert.ok(ldk.valid());
+
+    const syncedStorage = new SyncedAsyncStorage(ldk.getEntropyHex());
+    const keys = await syncedStorage.getAllKeysRemote();
+    for (const k of keys) {
+      let val = await syncedStorage.getItemRemote(k);
+      val = syncedStorage.decrypt(val);
+      console.log(`${k}\n---------------------------------------\n${val}`);
+    }
+  });
+});
