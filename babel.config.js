c54e28578b66d8a5909ba43024cf3874701bae6b
overtorment <overtorment@gmail.com>
2023-01-01 21:20:06 +0000

    REF: switch to react-native-quick-crypto

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'], // required by react-native-reanimated v2 https://docs.swmansion.com/react-native-reanimated/docs/installation/
+  plugins: [
+    'react-native-reanimated/plugin', // required by react-native-reanimated v2 https://docs.swmansion.com/react-native-reanimated/docs/installation/
+    [
+      'module-resolver',
+      {
+        alias: {
+          crypto: 'react-native-quick-crypto',
+          stream: 'stream-browserify',
+          buffer: '@craftzdog/react-native-buffer',
+        },
+      },
+    ],
+  ],
 };
