eec3c88edbaf4cda820998db61c0d4405b26075a
overtorment <overtorment@gmail.com>
2023-01-26 15:50:17 +0000

    FIX: allow deletion of borked wallets (closes #5228)

--- screen/wallets/details.js
+++ screen/wallets/details.js
@@ -227,7 +227,11 @@ const WalletDetails = () => {
 
   const navigateToOverviewAndDeleteWallet = () => {
     setIsLoading(true);
-    Notifications.unsubscribe(wallet.getAllExternalAddresses(), [], []);
+    let externalAddresses = [];
+    try {
+      externalAddresses = wallet.getAllExternalAddresses();
+    } catch (_) {}
+    Notifications.unsubscribe(externalAddresses, [], []);
     popToTop();
     deleteWallet(wallet);
     saveToDisk(true);
