feb22e98da69263372ac9e876ac5025991d3b46d
Marcos Rodriguez Velez <marcospr@pm.me>
2023-02-09 16:01:36 -0400

    WIP

--- android/app/src/main/java/io/bluewallet/bluewallet/MainApplication.java
+++ android/app/src/main/java/io/bluewallet/bluewallet/MainApplication.java
@@ -7,6 +7,8 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import com.facebook.react.modules.i18nmanager.I18nUtil;
@@ -16,7 +18,7 @@ import com.bugsnag.android.Bugsnag;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
-      new ReactNativeHost(this) {
+      new DefaultReactNativeHost(this) {
         @Override
         public boolean getUseDeveloperSupport() {
           return BuildConfig.DEBUG;
@@ -35,6 +37,16 @@ public class MainApplication extends Application implements ReactApplication {
         protected String getJSMainModuleName() {
           return "index";
         }
+
+           @Override
+	        protected boolean isNewArchEnabled() {
+	          return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+	        }
+	
+	        @Override
+	        protected Boolean isHermesEnabled() {
+	          return BuildConfig.IS_HERMES_ENABLED;
+	        }
       };
 
   @Override
@@ -49,7 +61,11 @@ public class MainApplication extends Application implements ReactApplication {
     I18nUtil sharedI18nUtilInstance = I18nUtil.getInstance();
     sharedI18nUtilInstance.allowRTL(getApplicationContext(), true);
     SoLoader.init(this, /* native exopackage */ false);
-    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+      if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+	      // If you opted-in for the New Architecture, we load the native entry point for this app.
+	      DefaultNewArchitectureEntryPoint.load();
+	    }	
+	    ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 
   /**
