cdff97814f151e2601862701ab0892d14517c9f7
abhishandy <abhishek.sandy@gmail.com>
2023-02-09 20:57:29 -0500

    fix: use copy-pastable text component

--- screen/wallets/paymentCode.tsx
+++ screen/wallets/paymentCode.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { NativeStackScreenProps } from 'react-native-screens/lib/typescript/native-stack';
+import { BlueCopyTextToClipboard } from '../../BlueComponents';
 import QRCodeComponent from '../../components/QRCodeComponent';
 
 type PaymentCodeStackParamList = {
@@ -16,7 +17,7 @@ export default function PaymentCode({ route }: NativeStackScreenProps<PaymentCod
       {paymentCode && (
         <>
           <QRCodeComponent value={paymentCode} />
-          <Text style={styles.paymentCodeText}>{paymentCode}</Text>
+          <BlueCopyTextToClipboard text={paymentCode} />
         </>
       )}
     </View>
@@ -28,10 +29,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  paymentCodeText: {
-    fontSize: 16,
-    marginTop: 20,
-    marginHorizontal: 40,
   },
 });
