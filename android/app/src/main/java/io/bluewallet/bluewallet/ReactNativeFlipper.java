35e9c618a12eca04daa0ac2de6241c66c21ca320
overtorment <overtorment@gmail.com>
2023-02-24 19:33:08 +0000

    WIP: android build

---
+++ android/app/src/main/java/io/bluewallet/bluewallet/ReactNativeFlipper.java
@@ -0,0 +1,8 @@
+package io.bluewallet.bluewallet;
+import android.content.Context;
+import com.facebook.react.ReactInstanceManager;
+public class ReactNativeFlipper {
+	  public static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
+		      // Do nothing as we don't want to initialize Flipper on Release.
+	  }
+}
