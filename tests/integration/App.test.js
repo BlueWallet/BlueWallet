f9e4c402902d36abac76aa3ddda2d29c0488bba0
overtorment <overtorment@gmail.com>
2023-02-24 10:20:22 +0000

    TST: fixed stuck tests

--- tests/integration/App.test.js
+++ tests/integration/App.test.js
@@ -4,21 +4,11 @@ import TestRenderer from 'react-test-renderer';
 import Settings from '../../screen/settings/settings';
 import Selftest from '../../screen/selftest';
 import { BlueHeaderDefaultSub } from '../../BlueComponents';
-import * as BlueElectrum from '../../blue_modules/BlueElectrum';
 
-jest.mock('react-native-qrcode-svg', () => 'Video');
-
-beforeAll(async () => {
-  // awaiting for Electrum to be connected. For RN Electrum would naturally connect
-  // while app starts up, but for tests we need to wait for it
-  await BlueElectrum.connectMain();
-  jest.useFakeTimers();
-});
-
-afterAll(() => {
-  jest.useRealTimers();
-  // after all tests we close socket so the test suite can actually terminate
-  BlueElectrum.forceDisconnect();
+jest.mock('../../blue_modules/BlueElectrum', () => {
+  return {
+    connectMain: jest.fn(),
+  };
 });
 
 it('BlueHeaderDefaultSub works', () => {
