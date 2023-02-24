62d22c1e48c5e4b67ce4bc86743fa98c8e12f3f4
Marcos Rodriguez Velez <marcospr@pm.me>
2023-02-24 13:40:40 -0400

    WIP

--- android/app/src/main/java/io/bluewallet/bluewallet/MainActivity.java
+++ android/app/src/main/java/io/bluewallet/bluewallet/MainActivity.java
@@ -9,7 +9,7 @@ import androidx.annotation.Nullable;
 import com.facebook.react.ReactActivity;
 
 import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 public class MainActivity extends ReactActivity {
