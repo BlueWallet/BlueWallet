85b3333f43a307294b09ef38bd471e04e16fde2b
Chloe Jung <chloe@onesandzeros.nz>
2023-03-23 10:19:48 +1300

    Update the default ground control uri

--- blue_modules/constants.js
+++ blue_modules/constants.js
@@ -2,4 +2,4 @@
  * Let's keep config vars, constants and definitions here
  */
 
-export const groundControlUri = 'https://groundcontrol-bluewallet.herokuapp.com/';
+export const groundControlUri = 'https://gc.boltcardwallet.com';
