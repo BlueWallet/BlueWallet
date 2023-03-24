f63bf7ea610b3a4132d3cc4412ba0fbc02478381
Marcos Rodriguez Velez <marcospr@pm.me>
2023-03-23 22:17:52 -0400

    REF: Handoff component from js to tsx

---
+++ components/handoff.tsx
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react';
+import Handoff from 'react-native-handoff';
+import { BlueStorageContext } from '../blue_modules/storage-context';
+
+interface HandoffComponentProps {
+  url?: string;
+}
+
+interface HandoffComponentWithActivityTypes extends React.FC<HandoffComponentProps> {
+  activityTypes: {
+    ReceiveOnchain: string;
+    Xpub: string;
+    ViewInBlockExplorer: string;
+  };
+}
+
+const HandoffComponent: HandoffComponentWithActivityTypes = props => {
+  const { isHandOffUseEnabled } = useContext(BlueStorageContext);
+
+  if (isHandOffUseEnabled) {
+    return <Handoff {...props} />;
+  }
+  return null;
+};
+
+const activityTypes = {
+  ReceiveOnchain: 'io.bluewallet.bluewallet.receiveonchain',
+  Xpub: 'io.bluewallet.bluewallet.xpub',
+  ViewInBlockExplorer: 'io.bluewallet.bluewallet.blockexplorer',
+};
+
+HandoffComponent.activityTypes = activityTypes;
+
+export default HandoffComponent;
