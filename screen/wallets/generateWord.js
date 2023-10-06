acc5823f2881777f34894e5458523d3dbe738f50
3bitcoins <144179047+3bitcoins@users.noreply.github.com>
2023-10-05 18:12:25 -0600

    Lint

--- screen/wallets/generateWord.js
+++ screen/wallets/generateWord.js
@@ -1,5 +1,5 @@
-import React, { useState, useRef } from 'react';
-import { useNavigation, useRoute, useTheme } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { useTheme } from '@react-navigation/native';
 import { StyleSheet, View, KeyboardAvoidingView, Platform, TextInput, Keyboard } from 'react-native';
 
 import loc from '../../loc';
