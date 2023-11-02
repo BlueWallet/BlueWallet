1af6df41862ae489a6b377df49b433039cc413bc
Marcos Rodriguez Velez <marcospr@pm.me>
2023-11-01 20:40:34 -0400

    FIX: Call dismiss on ActionSheet to avoid duplicate firing

--- screen/ActionSheet.ios.js
+++ screen/ActionSheet.ios.js
@@ -2,6 +2,7 @@ import { ActionSheetIOS, InteractionManager } from 'react-native';
 
 export default class ActionSheet {
   static showActionSheetWithOptions(options, completion) {
+    ActionSheetIOS.dismissActionSheet();
     InteractionManager.runAfterInteractions(() => {
       ActionSheetIOS.showActionSheetWithOptions(options, completion);
     });
