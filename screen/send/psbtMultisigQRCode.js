dfd5f37224982b64d77a08066c3caacc1d699e0f
Marcos Rodriguez Velez <marcospr@pm.me>
2023-12-13 02:47:53 -0400

    Lint fix

--- screen/send/psbtMultisigQRCode.js
+++ screen/send/psbtMultisigQRCode.js
@@ -58,7 +58,7 @@ const PsbtMultisigQRCode = () => {
       alert(loc.wallets.import_error);
     } else {
       // psbt base64?
-      navigate('PsbtMultisig', { receivedPSBTBase64: ret.data }, { merge: true});
+      navigate('PsbtMultisig', { receivedPSBTBase64: ret.data }, { merge: true });
     }
   };
 
