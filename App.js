ff6d182dc27287d6147fe49592aeba52640eae6b
Marcos Rodriguez Velez <marcospr@pm.me>
2024-01-04 20:31:29 -0400

    FIX: Ignore log triggered by haptic feedback on sim

--- App.js
+++ App.js
@@ -38,7 +38,7 @@ const currency = require('./blue_modules/currency');
 const eventEmitter = Platform.OS === 'ios' ? new NativeEventEmitter(NativeModules.EventEmitter) : undefined;
 const { EventEmitter } = NativeModules;
 
-LogBox.ignoreLogs(['Require cycle:']);
+LogBox.ignoreLogs(['Require cycle:', 'Battery state `unknown` and monitoring disabled, this is normal for simulators and tvOS.']);
 
 const ClipboardContentType = Object.freeze({
   BITCOIN: 'BITCOIN',
