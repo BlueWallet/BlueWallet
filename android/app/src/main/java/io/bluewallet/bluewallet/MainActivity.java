0d88be935fc1d788d4cff95fca8418f3a2d9c2a1
Marcos Rodriguez Vélez <marcospr@pm.me>
2024-01-07 21:43:07 -0400

    Update MainActivity.java

--- android/app/src/main/java/io/bluewallet/bluewallet/MainActivity.java
+++ android/app/src/main/java/io/bluewallet/bluewallet/MainActivity.java
@@ -36,13 +36,12 @@ public class MainActivity extends ReactActivity {
 	   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
 	   * (aka React 18) with two boolean flags.
 	   */
-	  @Override
-	  protected ReactActivityDelegate createReactActivityDelegate() {
-	    return new DefaultReactActivityDelegate(
-	        this,
-	        getMainComponentName(),
-	        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-	        DefaultNewArchitectureEntryPoint.getFabricEnabled(), // fabricEnabled
-	        );
-	  }
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new DefaultReactActivityDelegate(
+        this,
+        getMainComponentName(),
+        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+        DefaultNewArchitectureEntryPoint.getFabricEnabled());
+  }
 }
