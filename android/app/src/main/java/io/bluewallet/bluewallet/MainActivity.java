06dc2252f53f2d94f093c3bb0044e9d89345b312
Marcos Rodriguez Velez <marcospr@pm.me>
2024-01-07 22:08:25 -0400

    Update MainActivity.java

--- android/app/src/main/java/io/bluewallet/bluewallet/MainActivity.java
+++ android/app/src/main/java/io/bluewallet/bluewallet/MainActivity.java
@@ -2,12 +2,7 @@ package io.bluewallet.bluewallet;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-
-import androidx.annotation.Nullable;
-
 import com.facebook.react.ReactActivity;
-
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
@@ -31,17 +26,17 @@ public class MainActivity extends ReactActivity {
         }
     }
 
-     /**
-	   * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
-	   * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
-	   * (aka React 18) with two boolean flags.
-	   */
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new DefaultReactActivityDelegate(
-        this,
-        getMainComponentName(),
-        // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-        DefaultNewArchitectureEntryPoint.getFabricEnabled());
-  }
+    /**
+     * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
+     * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+     * (aka React 18) with two boolean flags.
+     */
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new DefaultReactActivityDelegate(
+                this,
+                getMainComponentName(),
+                // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+                DefaultNewArchitectureEntryPoint.getFabricEnabled());
+    }
 }
