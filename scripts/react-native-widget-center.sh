1ec8ea00e4a9674148d899685821ba16e125d6e6
Marcos Rodriguez Velez <marcospr@pm.me>
2024-01-14 18:01:16 -0400

    wip

--- scripts/react-native-widget-center.sh
+++
@@ -1,5 +0,0 @@
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' 's/classifier/archiveClassifier/g' node_modules/react-native-widget-center/android/build.gradle
-else
-    sed -i 's/classifier/archiveClassifier/g' node_modules/react-native-widget-center/android/build.gradle
-fi
