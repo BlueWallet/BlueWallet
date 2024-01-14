b29ba5985cdfad841156ad7a0d7aae3da4a94f75
Marcos Rodriguez Velez <marcospr@pm.me>
2024-01-14 13:32:06 -0400

    Update react-native-widget-center.sh

--- scripts/react-native-widget-center.sh
+++ scripts/react-native-widget-center.sh
@@ -1,1 +1,5 @@
-sed -i '' 's/classifier/archiveClassifier/g' node_modules/react-native-widget-center/android/build.gradle
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' 's/classifier/archiveClassifier/g' node_modules/react-native-widget-center/android/build.gradle
+else
+    sed -i 's/classifier/archiveClassifier/g' node_modules/react-native-widget-center/android/build.gradle
+fi
