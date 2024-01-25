cfabf827fc2d4d22a57a881f7cc78ae1ff391618
Marcos Rodriguez Velez <marcospr@pm.me>
2024-01-25 00:39:01 -0400

    REF: watchOS app code optimization

--- ios/BlueWalletWatch Extension/ExtensionDelegate.swift
+++ ios/BlueWalletWatch Extension/ExtensionDelegate.swift
@@ -3,7 +3,7 @@
 //  BlueWalletWatch Extension
 //
 //  Created by Marcos Rodriguez on 3/6/19.
-//  Copyright © 2019 Facebook. All rights reserved.
+
 //
 
 import WatchKit
--- ios/Bridge.swift
+++ ios/Bridge.swift
@@ -3,7 +3,6 @@
 //  BlueWallet
 //
 //  Created by Marcos Rodriguez on 9/19/19.
-//  Copyright © 2019 Facebook. All rights reserved.
 //
 
 import Foundation
