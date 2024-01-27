f2df2164775b75e83f87a44d6b024dd15b22b2b7
Marcos Rodriguez Velez <marcospr@pm.me>
2024-01-27 09:44:11 -0400

    REF: Currency settings screen to TSX

--- models/fiatUnit.ts
+++ models/fiatUnit.ts
@@ -174,6 +174,13 @@ type FiatUnit = {
 };
 export const FiatUnit = untypedFiatUnit as FiatUnit;
 
+export type FiatUnitType = {
+  endPointKey: string;
+  symbol: string;
+  locale: string;
+  source: keyof typeof FiatUnitSource;
+};
+
 export async function getFiatRate(ticker: string): Promise<number> {
   return await RateExtractors[FiatUnit[ticker].source](ticker);
 }
