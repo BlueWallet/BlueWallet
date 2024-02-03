c5134f302391400e52cff1b516ba8cd4746faa4b
Marcos Rodriguez Velez <marcospr@pm.me>
2024-02-03 12:02:03 -0400

    WIP

--- android/app/src/main/java/io/bluewallet/bluewallet/MainApplication.kt
+++ android/app/src/main/java/io/bluewallet/bluewallet/MainApplication.kt
@@ -9,7 +9,6 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
