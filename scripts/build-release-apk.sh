5e619d2d42705f8d190b33bf3f8efd120a88b4b2
Marcos Rodriguez Velez <marcospr@pm.me>
2024-02-04 09:05:18 -0400

    FIX: use versionName

--- scripts/build-release-apk.sh
+++ scripts/build-release-apk.sh
@@ -13,9 +13,16 @@ rm bluewallet-release-key.keystore.hex
 cd android
 # Use the BUILD_NUMBER environment variable set in the GitHub Actions workflow
 sed -i'.original' "s/versionCode 1/versionCode $BUILD_NUMBER/g" app/build.gradle
+
+# Extract versionName from build.gradle
+VERSION_NAME=$(grep versionName app/build.gradle | awk '{print $2}' | tr -d '"')
+
 ./gradlew assembleRelease
-# Rename the APK file to include the version and build number with parentheses
-mv ./app/build/outputs/apk/release/app-release-unsigned.apk "./app/build/outputs/apk/release/BlueWallet-6.5.0($BUILD_NUMBER).apk"
+
+# Rename the APK file to include the dynamic version and build number with parentheses
+mv ./app/build/outputs/apk/release/app-release-unsigned.apk "./app/build/outputs/apk/release/BlueWallet-${VERSION_NAME}($BUILD_NUMBER).apk"
+
 echo wheres waldo?
 find $ANDROID_HOME | grep apksigner | grep -v jar
-$ANDROID_HOME/build-tools/34.0.0/apksigner sign --ks ./bluewallet-release-key.keystore --ks-pass=pass:$KEYSTORE_PASSWORD "./app/build/outputs/apk/release/BlueWallet-6.5.0($BUILD_NUMBER).apk"
+
+$ANDROID_HOME/build-tools/34.0.0/apksigner sign --ks ./bluewallet-release-key.keystore --ks-pass=pass:$KEYSTORE_PASSWORD "./app/build/outputs/apk/release/BlueWallet-${VERSION_NAME}($BUILD_NUMBER).apk"
