fc0fc968ea0ebf2b41f758a176a202e19e658c91
Marcos Rodriguez Velez <marcospr@pm.me>
2024-02-17 10:36:51 -0400

    wip

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,18 +1,35 @@
-#!/bin/sh -
+#!/bin/bash -e
 
+# Ensures the script exits immediately if a command exits with a non-zero status.
+
+echo "===== Starting post-clone script for Xcode Cloud ====="
+
+# Update Homebrew to ensure the latest versions of Node.js and CocoaPods are available
+echo "===== Updating Homebrew ====="
+brew update
+
+# Install Node.js
 echo "===== Installing Node.js ====="
 brew install node
 
-echo "===== Installling CocoaPods ====="
+# Install CocoaPods
+echo "===== Installing CocoaPods ====="
 brew install cocoapods
 
-# Move to the root directory of the project
-cd ../..
+# Navigate to the root directory of the React Native project
+# This assumes the script is executed from the root directory, adjust if necessary
+echo "Navigating to the project root directory"
+cd "$(dirname "$0")"
 
-# Install dependencies
-echo "===== Running npm install ====="
+# Install JavaScript dependencies using npm
+echo "===== Installing JavaScript dependencies ====="
 npm install
 
-echo "===== Running pod install ====="
+# Navigate to the iOS directory and install CocoaPod dependencies
+echo "===== Installing CocoaPod dependencies ====="
 cd ios
 pod install
+# Return to the project root directory after installation
+cd ..
+
+echo "===== Post-clone script completed successfully ====="
