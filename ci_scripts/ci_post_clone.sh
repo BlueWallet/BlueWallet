218b8c19fe43bd03a09e34a2c1ec6a4c0a4a6cdd
Marcos Rodriguez Velez <marcospr@pm.me>
2024-02-17 14:38:11 -0400

    Update ci_post_clone.sh

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,6 +6,8 @@ brew install node@16
 brew install cocoapods
 
 echo "===== Installing Project Dependencies ====="
+npm config set maxsockets 3
+npm ci
 npm install -y
 
 echo "===== Cleaning and Installing iOS Dependencies ====="
