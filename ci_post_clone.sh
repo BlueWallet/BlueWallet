666a976c062ec383c8a6fbfc8a920dc545839886
Marcos Rodriguez Velez <marcospr@pm.me>
2024-02-17 10:20:13 -0400

    Update ci_post_clone.sh

--- ci_post_clone.sh
+++ ci_post_clone.sh
@@ -1,14 +1,18 @@
-#!/bin/zsh
+#!/bin/sh -
+
+echo "===== Installing Node.js ====="
+brew install node
 
 echo "===== Installling CocoaPods ====="
-export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
 brew install cocoapods
-echo "===== Installing Node.js ====="
-brew install node@21
+
+# Move to the root directory of the project
+cd ../..
 
 # Install dependencies
 echo "===== Running npm install ====="
 npm install
+
 echo "===== Running pod install ====="
 cd ios
 pod install
