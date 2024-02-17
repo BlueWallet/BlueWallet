e4e50226625fc58b77a37898d2a10500f6669f49
Marcos Rodriguez Velez <marcospr@pm.me>
2024-02-17 13:44:44 -0400

    Create ci_post_clone.sh

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+echo "===== Installling CocoaPods ====="
+export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
+brew install cocoapods
+echo "===== Installing Node.js ====="
+brew install node
+
+# Install dependencies
+echo "===== Running yarn install ====="
+npm install
+echo "===== Running pod install ====="
+cd ios
+pod install
+cd ..
