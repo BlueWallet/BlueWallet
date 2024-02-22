fff04479a94c28e6bacf38ac723a79d627a2acdd
Marcos Rodriguez Velez <marcospr@pm.me>
2024-02-21 21:38:31 -0400

    wip

--- hooks/usePrivacy.tsx
+++ hooks/usePrivacy.tsx
@@ -1,7 +1,42 @@
-export const usePrivacy = () => {
-  const enableBlur = () => {};
+import { useContext, useEffect } from 'react';
+// @ts-ignore: Package is not in the type definition
+import { enableSecureView, disableSecureView, forbidAndroidShare, allowAndroidShare } from 'react-native-prevent-screenshot-ios-android';
+import { BlueStorageContext } from '../blue_modules/storage-context';
+import { Platform } from 'react-native';
 
-  const disableBlur = () => {};
+export const usePrivacy = () => {
+  const { isPrivacyBlurEnabled } = useContext(BlueStorageContext);
+
+  const enableBlur = () => {
+    if (!isPrivacyBlurEnabled) return;
+    if (Platform.OS === 'android') {
+      forbidAndroidShare();
+    } else if (Platform.OS === 'ios') {
+      enableSecureView();
+    }
+  };
+
+  const disableBlur = () => {
+    if (Platform.OS === 'android') {
+      allowAndroidShare();
+    } else if (Platform.OS === 'ios') {
+      disableSecureView();
+    }
+  };
+
+  useEffect(() => {
+    // Automatically activate or deactivate on mount and when isPrivacyBlurEnabled changes
+    if (isPrivacyBlurEnabled) {
+      enableBlur();
+    } else {
+      disableBlur();
+    }
+
+    return () => {
+      disableBlur();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return { enableBlur, disableBlur };
 };
