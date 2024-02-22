5a08b4ef63ede73122cc32672366e3229c27cd09
Marcos Rodriguez Velez <marcospr@pm.me>
2024-02-21 22:08:20 -0400

    Revert "wip"
    This reverts commit fff04479a94c28e6bacf38ac723a79d627a2acdd.

--- hooks/usePrivacy.tsx
+++ hooks/usePrivacy.tsx
@@ -1,42 +1,7 @@
-import { useContext, useEffect } from 'react';
-// @ts-ignore: Package is not in the type definition
-import { enableSecureView, disableSecureView, forbidAndroidShare, allowAndroidShare } from 'react-native-prevent-screenshot-ios-android';
-import { BlueStorageContext } from '../blue_modules/storage-context';
-import { Platform } from 'react-native';
-
 export const usePrivacy = () => {
-  const { isPrivacyBlurEnabled } = useContext(BlueStorageContext);
+  const enableBlur = () => {};
 
-  const enableBlur = () => {
-    if (!isPrivacyBlurEnabled) return;
-    if (Platform.OS === 'android') {
-      forbidAndroidShare();
-    } else if (Platform.OS === 'ios') {
-      enableSecureView();
-    }
-  };
-
-  const disableBlur = () => {
-    if (Platform.OS === 'android') {
-      allowAndroidShare();
-    } else if (Platform.OS === 'ios') {
-      disableSecureView();
-    }
-  };
-
-  useEffect(() => {
-    // Automatically activate or deactivate on mount and when isPrivacyBlurEnabled changes
-    if (isPrivacyBlurEnabled) {
-      enableBlur();
-    } else {
-      disableBlur();
-    }
-
-    return () => {
-      disableBlur();
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const disableBlur = () => {};
 
   return { enableBlur, disableBlur };
 };
