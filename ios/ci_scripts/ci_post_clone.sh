e11a85429ff405b7cfae360ea578aa2abbcdbd31
Marcos Rodriguez Velez <marcospr@pm.me>
2024-02-24 16:01:45 -0400

    Update ci_post_clone.sh

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -8,12 +8,13 @@ brew install node@21
 echo "===== Installing yarn (Xcode Cloud doenst like NPM ) ====="
 brew install yarn
 
+# LDK is not currently available for Catalyst, so we need to patch the wallet
+echo "===== Running LDK is not currently available for Catalyst, so we need to patch the wallet ====="
+cp scripts/maccatalystpatches/lightning-ldk-wallet.ts class/wallets/lightning-ldk-wallet.ts
+
 # Install dependencies
 echo "===== Running yarn install ====="
 yarn install
 echo "===== Running pod install ====="
 cd ios
 pod install
-# LDK is not currently available for Catalyst, so we need to patch the wallet
-cd ..
-cp scripts/maccatalystpatches/lightning-ldk-wallet.ts class/wallets/lightning-ldk-wallet.ts
