1640249e17d1349f343f3980dac20e7acd5981f6
overtorment <overtorment@gmail.com>
2024-03-20 15:44:43 +0000

    REF: convert TransactionPendingIconBig to ts

--- components/TransactionPendingIconBig.tsx
+++ components/TransactionPendingIconBig.tsx
@@ -1,33 +1,39 @@
-/* eslint react/prop-types: "off", react-native/no-inline-styles: "off" */
-
 import { StyleSheet, View } from 'react-native';
-import { Icon } from 'react-native-elements';
 import React from 'react';
+import { Icon } from 'react-native-elements';
 import { useTheme } from './themes';
 
-export const TransactionPendingIconBig = props => {
+export const TransactionPendingIconBig: React.FC = () => {
   const { colors } = useTheme();
 
-  const stylesBlueIconHooks = StyleSheet.create({
+  const hookStyles = StyleSheet.create({
     ball: {
       backgroundColor: colors.buttonBackgroundColor,
     },
-    ball2: {
-      width: 150,
-      height: 150,
-      borderRadius: 75,
-    },
-    boxIncoming: {
-      position: 'relative',
-    },
   });
+
   return (
-    <View {...props}>
-      <View style={stylesBlueIconHooks.boxIncoming}>
-        <View style={[stylesBlueIconHooks.ball2, stylesBlueIconHooks.ball]}>
-          <Icon {...props} name="more-horiz" type="material" size={100} color={colors.foregroundColor} iconStyle={{ left: 0, top: 25 }} />
+    <View>
+      <View style={styles.boxIncoming}>
+        <View style={[styles.ball, hookStyles.ball]}>
+          <Icon name="more-horiz" type="material" size={100} color={colors.foregroundColor} iconStyle={styles.iconStyle} />
         </View>
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  boxIncoming: {
+    position: 'relative',
+  },
+  ball: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+  },
+  iconStyle: {
+    left: 0,
+    top: 25,
+  },
+});
