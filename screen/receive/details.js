234321ef37628f9fe600d7c2b5efc6120ab483f3
Marcos Rodriguez Velez <marcospr@pm.me>
2024-03-20 19:16:12 -0400

    FIX: Wallet export reminder was not visible on macOS

--- screen/receive/details.js
+++ screen/receive/details.js
@@ -61,6 +61,7 @@ const ReceiveDetails = () => {
   const [initialUnconfirmed, setInitialUnconfirmed] = useState(0);
   const [displayBalance, setDisplayBalance] = useState('');
   const fetchAddressInterval = useRef();
+  const receiveAddressButton = useRef();
   const stylesHook = StyleSheet.create({
     modalContent: {
       backgroundColor: colors.modal,
@@ -270,7 +271,7 @@ const ReceiveDetails = () => {
           )}
 
           <QRCodeComponent value={bip21encoded} />
-          <BlueCopyTextToClipboard text={isCustom ? bip21encoded : address} />
+          <BlueCopyTextToClipboard text={isCustom ? bip21encoded : address} ref={receiveAddressButton} />
         </View>
         <View style={styles.share}>
           <BlueCard>
@@ -344,7 +345,6 @@ const ReceiveDetails = () => {
             BlueAlertWalletExportReminder({
               onSuccess: obtainWalletAddress,
               onFailure: () => {
-                goBack();
                 navigate('WalletExportRoot', {
                   screen: 'WalletExport',
                   params: {
@@ -352,6 +352,7 @@ const ReceiveDetails = () => {
                   },
                 });
               },
+              anchor: receiveAddressButton.current,
             });
           } else {
             obtainWalletAddress();
