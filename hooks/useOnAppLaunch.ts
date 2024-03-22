9627667c1eb69911b1a484efb2991b1e888e87b0
Marcos Rodriguez Velez <marcospr@pm.me>
2024-03-22 19:17:38 -0400

    Update useOnAppLaunch.ts

--- hooks/useOnAppLaunch.ts
+++ hooks/useOnAppLaunch.ts
@@ -1,25 +1,27 @@
-import { useCallback } from 'react';
+import { useCallback, useContext } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { AbstractWallet } from '../class';
-const BlueApp = require('../BlueApp');
+import { BlueStorageContext } from '../blue_modules/storage-context';
 
 const useOnAppLaunch = () => {
   const STORAGE_KEY = 'ONAPP_LAUNCH_SELECTED_DEFAULT_WALLET_KEY';
+  const { wallets } = useContext(BlueStorageContext);
 
   const getSelectedDefaultWallet = useCallback(async (): Promise<AbstractWallet | false> => {
     let selectedWallet: AbstractWallet | false = false;
     try {
       const selectedWalletID = JSON.parse((await AsyncStorage.getItem(STORAGE_KEY)) || 'null');
       if (selectedWalletID) {
-        selectedWallet = BlueApp.getWallets().find((wallet: AbstractWallet) => wallet.getID() === selectedWalletID);
+        selectedWallet = wallets.find((wallet: AbstractWallet) => wallet.getID() === selectedWalletID);
         if (!selectedWallet) {
-          await AsyncStorage.setItem(STORAGE_KEY, '');
+          await AsyncStorage.removeItem(STORAGE_KEY);
         }
       }
     } catch (_e) {
       return false;
     }
     return selectedWallet;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [STORAGE_KEY]); // No external dependencies
 
   const setSelectedDefaultWallet = useCallback(
@@ -43,13 +45,14 @@ const useOnAppLaunch = () => {
       if (!value) {
         const selectedDefaultWallet = await getSelectedDefaultWallet();
         if (!selectedDefaultWallet) {
-          const firstWallet = BlueApp.getWallets()[0];
+          const firstWallet = wallets[0];
           await setSelectedDefaultWallet(firstWallet.getID());
         }
       } else {
         await AsyncStorage.setItem(STORAGE_KEY, '');
       }
     },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [STORAGE_KEY, getSelectedDefaultWallet, setSelectedDefaultWallet],
   ); // Include dependencies here
 
