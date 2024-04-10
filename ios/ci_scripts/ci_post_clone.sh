313382a5c4e04a9b901222f2f61f1fb51f9a9a55
Marcos Rodriguez Velez <marcospr@pm.me>
2024-04-10 18:46:25 -0400

    Update ci_post_clone.sh

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -9,7 +9,17 @@ echo "===== Installing Node.js ====="
 brew install node@18
 echo "Node.js installation complete."
 
-# Note: Removed yarn installation as per request to use npm instead.
+# Configure environment to use node@18
+echo "Configuring environment to use node@18..."
+echo 'export PATH="/usr/local/opt/node@18/bin:$PATH"' >> ~/.zshrc
+export PATH="/usr/local/opt/node@18/bin:$PATH"
+
+echo 'export LDFLAGS="-L/usr/local/opt/node@18/lib"' >> ~/.zshrc
+export LDFLAGS="-L/usr/local/opt/node@18/lib"
+
+echo 'export CPPFLAGS="-I/usr/local/opt/node@18/include"' >> ~/.zshrc
+export CPPFLAGS="-I/usr/local/opt/node@18/include"
+echo "Configuration complete."
 
 # Install dependencies using npm
 echo "===== Running npm install ====="
@@ -20,6 +30,6 @@ echo "===== Running pod install ====="
 cd ios
 pod install | tee pod-install-log.txt
 echo "pod install complete. Full log output in pod-install-log.txt"
-
+cd ..
 
 echo "===== Installation and Setup Complete ====="
