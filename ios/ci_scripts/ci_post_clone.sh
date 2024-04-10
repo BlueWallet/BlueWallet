a9bc9fbde795986ab52eba54234ef1597fb3ca1e
Marcos Rodriguez Velez <marcospr@pm.me>
2024-04-10 18:09:43 -0400

    OPS: Version bump

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,16 +1,25 @@
 #!/bin/zsh
 
-echo "===== Installling CocoaPods ====="
+echo "===== Installing CocoaPods ====="
 export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
 brew install cocoapods
+echo "CocoaPods installation complete."
+
 echo "===== Installing Node.js ====="
 brew install node@18
-echo "===== Installing yarn (Xcode Cloud doenst like NPM ) ====="
-brew install yarn
+echo "Node.js installation complete."
 
-# Install dependencies
-echo "===== Running yarn install ====="
-npm install -y
+# Note: Removed yarn installation as per request to use npm instead.
+
+# Install dependencies using npm
+echo "===== Running npm install ====="
+npm install -y | tee npm-install-log.txt
+echo "npm install complete. Full log output in npm-install-log.txt"
+
 echo "===== Running pod install ====="
 cd ios
-pod install
+pod install | tee pod-install-log.txt
+echo "pod install complete. Full log output in pod-install-log.txt"
+cd ..
+
+echo "===== Installation and Setup Complete ====="
