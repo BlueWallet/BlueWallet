430e4e34ad0715e58e3cd29f13315d06a9e70b91
Marcos Rodriguez Velez <marcospr@pm.me>
2024-05-04 20:24:36 -0400

    REF: Load WalletLists faster with useEffet

--- screen/wallets/WalletsList.tsx
+++ screen/wallets/WalletsList.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useLayoutEffect, useReducer, useRef } from 'react';
+import React, { useCallback, useEffect, useReducer, useRef } from 'react';
 import {
   View,
   TouchableOpacity,
@@ -166,7 +166,7 @@ const WalletsList: React.FC = () => {
     }
   };
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     setOptions({
       navigationBarColor: colors.navigationBarColor,
       headerShown: !isDesktop,
@@ -176,26 +176,14 @@ const WalletsList: React.FC = () => {
       // eslint-disable-next-line react/no-unstable-nested-components
       headerRight: () =>
         I18nManager.isRTL ? null : (
-          <TouchableOpacity
-            accessibilityRole="button"
-            accessibilityLabel={loc._.more}
-            testID="SettingsButton"
-            style={styles.headerTouch}
-            onPress={navigateToSettings}
-          >
+          <TouchableOpacity accessibilityRole="button" accessibilityLabel={loc._.more} testID="SettingsButton" onPress={navigateToSettings}>
             <Icon size={22} name="more-horiz" type="material" color={colors.foregroundColor} />
           </TouchableOpacity>
         ),
       // eslint-disable-next-line react/no-unstable-nested-components
       headerLeft: () =>
         I18nManager.isRTL ? (
-          <TouchableOpacity
-            accessibilityRole="button"
-            accessibilityLabel={loc._.more}
-            testID="SettingsButton"
-            style={styles.headerTouch}
-            onPress={navigateToSettings}
-          >
+          <TouchableOpacity accessibilityRole="button" accessibilityLabel={loc._.more} testID="SettingsButton" onPress={navigateToSettings}>
             <Icon size={22} name="more-horiz" type="material" color={colors.foregroundColor} />
           </TouchableOpacity>
         ) : null,
@@ -471,10 +459,6 @@ const styles = StyleSheet.create({
   },
   walletsListWrapper: {
     flex: 1,
-  },
-  headerTouch: {
-    height: 48,
-    paddingVertical: 10,
   },
   listHeaderBack: {
     flexDirection: 'row',
