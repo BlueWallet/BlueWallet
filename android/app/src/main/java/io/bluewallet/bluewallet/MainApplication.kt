a85ec788d983a870e3c93bb16a8feed5dc5b2660
Marcos Rodriguez Velez <marcospr@pm.me>
2024-05-17 23:41:50 -0400

    Fixes

--- android/app/src/main/java/io/bluewallet/bluewallet/MainApplication.kt
+++ android/app/src/main/java/io/bluewallet/bluewallet/MainApplication.kt
@@ -2,57 +2,49 @@ package io.bluewallet.bluewallet
 
 import android.app.Application
 import android.content.Context
-import android.content.SharedPreferences
 import com.bugsnag.android.Bugsnag
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactInstanceManager
+import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
+import com.facebook.react.ReactPackage
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 import com.facebook.react.modules.i18nmanager.I18nUtil
 
 class MainApplication : Application(), ReactApplication {
 
-    private val mReactNativeHost = object : DefaultReactNativeHost(this) {
-        override fun getUseDeveloperSupport(): Boolean {
-            return BuildConfig.DEBUG
+    override val reactNativeHost: ReactNativeHost =
+        object : DefaultReactNativeHost(this) {
+            override fun getPackages(): List<ReactPackage> =
+                PackageList(this).packages.apply {
+                    // Packages that cannot be autolinked yet can be added manually here, for example:
+                    // add(MyReactNativePackage())
+                }
+
+            override fun getJSMainModuleName(): String = "index"
+
+            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
+            override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+            override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
         }
 
-        override fun getPackages(): List<ReactPackage> {
-            val packages = PackageList(this).packages
-            // Packages that cannot be autolinked yet can be added manually here, for example:
-            // packages.add(MyReactNativePackage())
-            return packages
-        }
+    override val reactHost: ReactHost
+        get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
 
-        override fun getJSMainModuleName(): String {
-            return "index"
-        }
-
-        override fun isNewArchEnabled(): Boolean {
-            return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        }
-
-        override fun isHermesEnabled(): Boolean {
-            return BuildConfig.IS_HERMES_ENABLED
-        }
-    }
-
-    override fun getReactNativeHost(): ReactNativeHost {
-        return mReactNativeHost
-    }
 
     override fun onCreate() {
         super.onCreate()
         val sharedI18nUtilInstance = I18nUtil.getInstance()
         sharedI18nUtilInstance.allowRTL(applicationContext, true)
         SoLoader.init(this, /* native exopackage */ false)
-        
+
         if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
             // If you opted-in for the New Architecture, we load the native entry point for this app.
-            DefaultNewArchitectureEntryPoint.load()
+            load()
         }
 
         val sharedPref = applicationContext.getSharedPreferences("group.io.bluewallet.bluewallet", Context.MODE_PRIVATE)
