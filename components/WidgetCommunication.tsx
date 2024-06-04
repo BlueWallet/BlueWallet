246913418137695a7879ac1d24266a50e11eee4a
Marcos Rodriguez Velez <marcospr@pm.me>
2024-06-04 00:23:53 -0400

    FIX: If latest tx was unconfirmed, widget should show "Never" for latest

--- components/WidgetCommunication.tsx
+++ components/WidgetCommunication.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 
-import { TWallet } from '../class/wallets/types';
-
 export const isBalanceDisplayAllowed = async (): Promise<boolean> => {
   return true;
 };
 
 export const setBalanceDisplayAllowed = async (value: boolean): Promise<void> => {};
-
-export const syncWidgetBalanceWithWallets = async (_wallets: TWallet[], _walletsInitialized: boolean): Promise<void> => {};
 
 const WidgetCommunication: React.FC = () => {
   return null; // This component does not render anything.
