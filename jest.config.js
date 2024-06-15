5e96b76f42d9fe0e683f481688dc55e753f091e6
Marcos Rodriguez Velez <marcospr@pm.me>
2024-06-15 17:57:51 -0400

    wip

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
   moduleFileExtensions: ['js', 'json', 'ts', 'tsx'],
-  transformIgnorePatterns: ['node_modules/(?!((jest-)?react-native(-.*)?|@react-native(-community)?)|silent-payments/)'],
+  transformIgnorePatterns: ['node_modules/(?!((jest-)?react-native(-.*)?|@react-native(-community)?)|@rneui|silent-payments/)'],
   setupFiles: ['./tests/setup.js'],
   watchPathIgnorePatterns: ['<rootDir>/node_modules'],
   setupFilesAfterEnv: ['./tests/setupAfterEnv.js'],
