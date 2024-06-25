f012743e861b0df5a1f9272dc9561962119096cc
Marcos Rodriguez Velez <marcospr@pm.me>
2024-06-25 13:29:22 -0400

    wip

--- ios/BlueWalletWatch Extension/Objects/TransactionTableRowView.swift
+++ ios/BlueWalletWatch Extension/Objects/TransactionTableRowView.swift
@@ -9,11 +9,17 @@ struct TransactionTableRowView: View {
                 Text(transaction.amount)
                 Text(transaction.memo)
                 Text(transaction.time)
-                .foregroundColor(transaction.type == .Pending ? .orange : .primary)
+                    .foregroundColor(transaction.type == .Pending ? .orange : .primary)
             }
             Spacer()
-          Image(systemName: icon(for: transaction.type))
+            Image(systemName: icon(for: transaction.type))
+                .foregroundColor(foregroundColor(for: transaction.type))
+                .background(backgroundColor(for: transaction.type))
+                .clipShape(Circle())
         }
+        .padding()
+        .background(backgroundColor(for: transaction.type))
+        .cornerRadius(8)
     }
 
     private func icon(for type: WalletTransactionType) -> String {
@@ -26,10 +32,32 @@ struct TransactionTableRowView: View {
             return "arrow.up.right.circle"
         }
     }
+
+    private func foregroundColor(for type: WalletTransactionType) -> Color {
+        switch type {
+        case .Pending:
+            return .orange
+        case .Received:
+            return Color(hex: "#37c0a1")
+        case .Sent:
+            return Color(hex: "#d0021b")
+        }
+    }
+
+    private func backgroundColor(for type: WalletTransactionType) -> Color {
+        switch type {
+        case .Pending:
+            return Color.orange.opacity(0.2)
+        case .Received:
+            return Color(hex: "#d2f8d6")
+        case .Sent:
+            return Color(hex: "#f8d2d2")
+        }
+    }
 }
 
 struct TransactionTableRowView_Previews: PreviewProvider {
     static var previews: some View {
-      TransactionTableRowView(transaction: SampleData.createSampleWallet().transactions.first!)
+        TransactionTableRowView(transaction: SampleData.createSampleWallet().transactions.first!)
     }
 }
--- ios/Shared/WalletGradient.swift
+++ ios/Shared/WalletGradient.swift
@@ -30,18 +30,3 @@ struct WalletGradient {
         }
     }
 }
-
-extension Color {
-    init(hex: String) {
-        let scanner = Scanner(string: hex)
-        scanner.currentIndex = hex.index(hex.startIndex, offsetBy: 1)
-        var rgbValue: UInt64 = 0
-        scanner.scanHexInt64(&rgbValue)
-
-        let red = Double((rgbValue & 0xff0000) >> 16) / 255.0
-        let green = Double((rgbValue & 0x00ff00) >> 8) / 255.0
-        let blue = Double(rgbValue & 0x0000ff) / 255.0
-
-        self.init(red: red, green: green, blue: blue)
-    }
-}
