61ee3dfabaf1c25e62e6b7386d5dff3d287c4b3a
Marcos Rodriguez Velez <marcospr@pm.me>
2024-07-04 15:38:50 -0400

    FIX: Accessibility for Transaction rows

--- components/types.ts
+++ components/types.ts
@@ -30,6 +30,7 @@ export interface ToolTipMenuProps {
   style?: ViewStyle | ViewStyle[];
   accessibilityLabel?: string;
   accessibilityHint?: string;
+  accessibilityState?: object;
   buttonStyle?: ViewStyle | ViewStyle[];
   onMenuWillShow?: () => void;
   onMenuWillHide?: () => void;
