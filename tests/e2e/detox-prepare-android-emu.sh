5043ba3d7a796aae2a0818f03e2ff09e32974696
Marcos Rodriguez Velez <marcospr@pm.me>
2024-07-06 19:45:13 -0400

    wip

--- tests/e2e/detox-prepare-android-emu.sh
+++ tests/e2e/detox-prepare-android-emu.sh
@@ -1,4 +1,4 @@
-$ANDROID_HOME/tools/bin/sdkmanager "system-images;android-34;default;x86_64"
+$ANDROID_HOME/tools/bin/sdkmanager "system-images;android-29;default;x86_64"
 echo no | $ANDROID_HOME/tools/bin/sdkmanager --licenses
-echo no | $ANDROID_HOME/tools/bin/avdmanager create avd -n Pixel_API_34_AOSP --force --package "system-images;android-34;default;x86_64"
-printf "\nhw.lcd.height=1334\nhw.lcd.width=750\nhw.lcd.density=320\nskin.name=750x1334" >> ~/.android/avd/Pixel_API_34_AOSP.avd/config.ini
+echo no | $ANDROID_HOME/tools/bin/avdmanager create avd -n Pixel_API_29_AOSP --force --package "system-images;android-29;default;x86_64"
+printf "\nhw.lcd.height=1329\nhw.lcd.width=750\nhw.lcd.density=320\nskin.name=750x1329" >> ~/.android/avd/Pixel_API_29_AOSP.avd/config.ini
