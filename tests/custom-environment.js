bed020a097fbe0746e01f78c6259692b8834dcec
overtorment <overtorment@gmail.com>
2024-07-24 21:18:28 +0100

    TST: add retries for integration tests on CI

---
+++ tests/custom-environment.js
@@ -0,0 +1,17 @@
+import NodeEnvironment from 'jest-environment-node';
+class CustomEnvironment extends NodeEnvironment {
+  async handleTestEvent(event) {
+    if (event.name === 'test_start') {
+      if (!process.env.RETRY) return;
+
+      const fullName = (event.test.parent.name === 'ROOT_DESCRIBE_BLOCK' ? '' : event.test.parent.name + ' ') + event.test.name;
+      const hash = require('crypto').createHash('md5').update(fullName).digest('hex');
+      if (require('fs').existsSync(`/tmp/${hash}`)) {
+        event.test.mode = 'skip';
+        console.log('skipping as it previously passed on CI:', fullName);
+      }
+    }
+  }
+}
+
+module.exports = CustomEnvironment;
