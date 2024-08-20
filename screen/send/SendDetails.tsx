01477e1b6d13a13598dfea909778efd7e5af5059
Marcos Rodriguez Velez <marcospr@pm.me>
2024-08-20 15:35:51 -0400

    FIX: SelectFeeModal cost formatting

--- screen/send/SendDetails.tsx
+++ screen/send/SendDetails.tsx
@@ -1380,6 +1380,7 @@ const SendDetails = () => {
           feeRate={feeRate}
           setCustomFee={setCustomFee}
           setFeePrecalc={setFeePrecalc}
+          feeUnit={feeUnit || BitcoinUnit.BTC}
         />
       </View>
       <BlueDismissKeyboardInputAccessory />
