62f63d03b15df6e1b343762e9fc1b3a631804836
Marcos Rodriguez Vélez <marcospr@pm.me>
2024-08-27 02:28:26 -0400

    Update build-release-apk.sh

--- scripts/build-release-apk.sh
+++ scripts/build-release-apk.sh
@@ -4,22 +4,25 @@
 # expects an optional parameter to specify the build type: "release" or "reproducible"
 # if no parameter is provided, defaults to "release"
 
-BUILD_TYPE=${1:-release}
+BUILD_TYPE="${1:-release}"
 
 if [ "$BUILD_TYPE" == "release" ]; then
     # Convert the release keystore from hex
-    echo $KEYSTORE_FILE_HEX > bluewallet-release-key.keystore.hex
+    echo "$KEYSTORE_FILE_HEX" > bluewallet-release-key.keystore.hex
     xxd -plain -revert bluewallet-release-key.keystore.hex > ./android/bluewallet-release-key.keystore
     rm bluewallet-release-key.keystore.hex
 
-    APK_PATH="./android/app/build/outputs/apk/release/BlueWallet-${VERSION_NAME}($BUILD_NUMBER).apk"
+    APK_PATH="./android/app/build/outputs/apk/release/BlueWallet-${VERSION_NAME}(${BUILD_NUMBER}).apk"
     BUILD_COMMAND="./gradlew assembleRelease"
     SIGN_COMMAND="$APKSIGNER_PATH sign --ks ./bluewallet-release-key.keystore --ks-pass=pass:$KEYSTORE_PASSWORD \"$APK_PATH\""
 
 elif [ "$BUILD_TYPE" == "reproducible" ]; then
-    APK_PATH="./android/app/build/outputs/apk/reproducible/BlueWallet-Reproducible-${VERSION_NAME}($BUILD_NUMBER).apk"
+    APK_OUTPUT_DIR="./app/build/outputs/apk/reproducible"
+    APK_FILENAME="app-$BUILD_TYPE-unsigned.apk"
+    APK_PATH="$APK_OUTPUT_DIR/$APK_FILENAME"
+    FINAL_APK_PATH="./android/app/build/outputs/apk/reproducible/BlueWallet-Reproducible-${VERSION_NAME}(${BUILD_NUMBER}).apk"
     BUILD_COMMAND="./gradlew assembleReproducible"
-    SIGN_COMMAND="$APKSIGNER_PATH sign --ks ./reproducible.keystore --ks-pass=pass:BWReproducibleBuild --key-pass=pass:reproducible \"$APK_PATH\""
+    SIGN_COMMAND="$APKSIGNER_PATH sign --ks ./reproducible.keystore --ks-pass=pass:BWReproducibleBuild --key-pass=pass:reproducible \"$FINAL_APK_PATH\""
 
 else
     echo "Invalid build type specified. Use 'release' or 'reproducible'."
@@ -29,26 +32,32 @@ fi
 cd android
 
 # Use the BUILD_NUMBER environment variable set in the GitHub Actions workflow
-sed -i'.original' "s/versionCode 1/versionCode $BUILD_NUMBER/g" app/build.gradle
+sed -i'.original' "s/versionCode 1/versionCode ${BUILD_NUMBER}/g" app/build.gradle
 
 # Extract versionName from build.gradle
 VERSION_NAME=$(grep versionName app/build.gradle | awk '{print $2}' | tr -d '"')
 
 # Find apksigner tool
 echo "Locating apksigner..."
-APKSIGNER_PATH=$(find $ANDROID_HOME -type f -name apksigner | grep -v jar | head -n 1)
+APKSIGNER_PATH=$(find "$ANDROID_HOME" -type f -name apksigner | grep -v jar | head -n 1)
 echo "Using apksigner at: $APKSIGNER_PATH"
 
 # Build the APK
 echo "Building $BUILD_TYPE APK..."
 $BUILD_COMMAND
 
-# Rename the APK file to include the dynamic version and build number with parentheses
-mv ./app/build/outputs/apk/$BUILD_TYPE/app-$BUILD_TYPE-unsigned.apk "$APK_PATH"
+# Verify that the APK was created before renaming
+if [ -f "$APK_PATH" ]; then
+    mv "$APK_PATH" "$FINAL_APK_PATH"
+else
+    echo "Error: APK not found at $APK_PATH"
+    ls -Rla "$APK_OUTPUT_DIR"
+    exit 1
+fi
 
 # Sign the APK
 echo "Signing $BUILD_TYPE APK..."
-$APKSIGNER_PATH sign --ks ./reproducible.keystore --ks-pass=pass:$KEYSTORE_PASSWORD "$APK_PATH"
+"$APKSIGNER_PATH" sign --ks "$FINAL_APK_PATH" --ks-pass=pass:"$KEYSTORE_PASSWORD" "$FINAL_APK_PATH"
 
 echo "APK signing complete."
-echo "$BUILD_TYPE APK: $APK_PATH"
+echo "$BUILD_TYPE APK: $FINAL_APK_PATH"
