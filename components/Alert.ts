4f938360eae4d420b447200aeae8c3134882f802
Marcos Rodriguez Velez <marcospr@pm.me>
2024-10-03 22:26:44 -0400

    wip

--- components/Alert.ts
+++ components/Alert.ts
@@ -6,13 +6,24 @@ export enum AlertType {
   Toast,
 }
 
+interface AlertButton {
+  text: string;
+  onPress?: () => void;
+  style?: 'default' | 'cancel' | 'destructive';
+}
+
+interface AlertOptions {
+  cancelable?: boolean;
+}
+
 const presentAlert = (() => {
-  // Private variable to store the last alert's parameters
   let lastAlertParams: {
     title?: string;
     message: string;
     type?: AlertType;
     hapticFeedback?: HapticFeedbackTypes;
+    buttons?: AlertButton[];
+    options?: AlertOptions;
   } | null = null;
 
   return ({
@@ -20,49 +31,47 @@ const presentAlert = (() => {
     message,
     type = AlertType.Alert,
     hapticFeedback,
+    buttons = [],
+    options = { cancelable: false },
   }: {
     title?: string;
     message: string;
     type?: AlertType;
     hapticFeedback?: HapticFeedbackTypes;
+    buttons?: AlertButton[];
+    options?: AlertOptions;
   }) => {
-    // Check if the current parameters match the last ones
     if (
       lastAlertParams &&
       lastAlertParams.title === title &&
       lastAlertParams.message === message &&
       lastAlertParams.type === type &&
-      lastAlertParams.hapticFeedback === hapticFeedback
+      lastAlertParams.hapticFeedback === hapticFeedback &&
+      JSON.stringify(lastAlertParams.buttons) === JSON.stringify(buttons) &&
+      JSON.stringify(lastAlertParams.options) === JSON.stringify(options)
     ) {
-      // Skip showing the alert if the content is the same as the last one
-      return;
+      return; // Skip showing the alert if the content is the same as the last one
     }
 
-    // Update the last alert parameters
-    lastAlertParams = { title, message, type, hapticFeedback };
+    lastAlertParams = { title, message, type, hapticFeedback, buttons, options };
 
-    // Trigger haptic feedback if provided
     if (hapticFeedback) {
       triggerHapticFeedback(hapticFeedback);
     }
 
-    // Force Alert for non-Android platforms
     if (Platform.OS !== 'android') {
       type = AlertType.Alert;
     }
 
-    // Show the appropriate alert based on the platform and type
     switch (type) {
       case AlertType.Toast:
         ToastAndroid.show(message, ToastAndroid.LONG);
         break;
       default:
         if (Platform.OS === 'android') {
-          // Android-specific behavior: If no title, use an empty string as the title
-          RNAlert.alert(title || '', message);
+          RNAlert.alert(title || '', message, buttons, options);
         } else {
-          // For other platforms, use default logic
-          RNAlert.alert(title ?? message, title && message ? message : undefined);
+          RNAlert.alert(title ?? message, title && message ? message : undefined, buttons, options);
         }
         break;
     }
