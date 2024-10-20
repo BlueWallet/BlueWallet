24d9cc925410f3c647e396a663f110cb13f44f6f
Marcos Rodriguez Velez <marcospr@pm.me>
2024-10-20 14:16:50 -0400

    Update GenerateQRCodeIntent.swift

--- ios/AppIntents/GenerateQRCodeIntent.swift
+++ ios/AppIntents/GenerateQRCodeIntent.swift
@@ -1,45 +1,50 @@
+//
+//  GenerateQRCodeIntent.swift
+//  BlueWallet
+//
+//  Created by Marcos Rodriguez on 10/20/24.
+//  Copyright © 2024 BlueWallet. All rights reserved.
+//
+
 import UIKit
 import CoreImage.CIFilterBuiltins
 import AppIntents
-import SwiftUI
+import UniformTypeIdentifiers
 
-@available(iOS 16.4, *)
+@available(iOS 16.0, *)
 struct GenerateQRCodeIntent: AppIntent {
     static var title: LocalizedStringResource = "Generate QR Code"
-    
+
     static var description = IntentDescription(
-        "Generates a QR code for a Bitcoin address from your wallet and exports it as an image."
+        "Generates a QR code for a Bitcoin address from your wallet and returns it as an image."
     )
-    
-    static var parameterSummary: some ParameterSummary {
-        Summary("Generate a QR code for a Bitcoin address from your selected wallet.")
-    }
 
     @Parameter(title: "Show Label", default: true)
     var showLabel: Bool
-    
+
     @Parameter(title: "Show Address", default: true)
     var showAddress: Bool
+
+    static var parameterSummary: some ParameterSummary {
+        Summary("Generate a QR code for your selected wallet.")
+    }
 
     enum GenerateQRCodeError: Error, LocalizedError {
         case noWalletSelected
         case invalidWalletData
         case qrCodeGenerationFailed
-        case imageSavingFailed
-        
+
         var errorDescription: String? {
             switch self {
             case .noWalletSelected, .invalidWalletData:
                 return "It seems something went wrong. Please choose a wallet to use in your BlueWallet settings > General > Shortcuts."
             case .qrCodeGenerationFailed:
                 return "Failed to generate the QR code."
-            case .imageSavingFailed:
-                return "Failed to save the QR code image."
             }
         }
     }
 
-    func perform() async throws -> some ReturnsValue<IntentFile> {
+    func perform() async throws -> some IntentResult & ReturnsValue<IntentFile> {
         guard let qrCodeData = KeychainService.shared.fetchQRCodeData() else {
             throw GenerateQRCodeError.noWalletSelected
         }
@@ -48,7 +53,7 @@ struct GenerateQRCodeIntent: AppIntent {
             throw GenerateQRCodeError.invalidWalletData
         }
 
-        guard let image = renderBitcoinAddressQRCodeSnippetAsImageCoreGraphics(
+        guard let image = renderBitcoinAddressQRCodeSnippetAsImage(
             qrCode: qrCodeData.address,
             label: qrCodeData.label,
             showAddress: showAddress,
@@ -57,29 +62,19 @@ struct GenerateQRCodeIntent: AppIntent {
             throw GenerateQRCodeError.qrCodeGenerationFailed
         }
 
-        guard let imageData = image.pngData(), let fileURL = saveImageToFile(imageData) else {
-            throw GenerateQRCodeError.imageSavingFailed
+        guard let imageData = image.pngData() else {
+            throw GenerateQRCodeError.qrCodeGenerationFailed
         }
 
-        let intentFile = IntentFile(fileURL: fileURL, filename: "bitcoin_qr_code.png", type: .png)
-        
+        let uniqueFilename = "bitcoin_qr_code_\(UUID().uuidString).png"
+
+        let intentFile = IntentFile(data: imageData, filename: uniqueFilename, type: .png)
+
         return .result(value: intentFile)
-    }
-
-    private func saveImageToFile(_ imageData: Data) -> URL? {
-        let tempDirectory = FileManager.default.temporaryDirectory
-        let fileURL = tempDirectory.appendingPathComponent("bitcoin_qr_code.png")
-
-        do {
-            try imageData.write(to: fileURL)
-            return fileURL
-        } catch {
-            return nil
-        }
     }
 }
 
-func renderBitcoinAddressQRCodeSnippetAsImageCoreGraphics(
+func renderBitcoinAddressQRCodeSnippetAsImage(
     qrCode: String,
     label: String,
     showAddress: Bool,
@@ -87,15 +82,18 @@ func renderBitcoinAddressQRCodeSnippetAsImageCoreGraphics(
     size: CGSize = CGSize(width: 400, height: 400)
 ) -> UIImage? {
     guard let qrImage = generateQRCode(from: qrCode) else { return nil }
-    
+
     let renderer = UIGraphicsImageRenderer(size: size)
-    
-    return renderer.image { context in
+
+    let image = renderer.image { context in
         context.cgContext.setFillColor(UIColor.white.cgColor)
         context.cgContext.fill(CGRect(origin: .zero, size: size))
 
         let qrCodeSize = CGSize(width: 200, height: 200)
-        let qrCodeOrigin = CGPoint(x: (size.width - qrCodeSize.width) / 2, y: (size.height - qrCodeSize.height) / 2)
+        let qrCodeOrigin = CGPoint(
+            x: (size.width - qrCodeSize.width) / 2,
+            y: (size.height - qrCodeSize.height) / 2
+        )
         qrImage.draw(in: CGRect(origin: qrCodeOrigin, size: qrCodeSize))
 
         if let appIcon = UIImage(named: "SplashIcon") {
@@ -109,12 +107,12 @@ func renderBitcoinAddressQRCodeSnippetAsImageCoreGraphics(
                 x: qrCodeOrigin.x + (qrCodeSize.width - containerSize.width) / 2,
                 y: qrCodeOrigin.y + (qrCodeSize.height - containerSize.height) / 2
             )
-            
+
             UIColor.white.setFill()
             context.cgContext.fill(CGRect(origin: containerOrigin, size: containerSize))
             UIColor.black.setStroke()
             context.cgContext.stroke(CGRect(origin: containerOrigin, size: containerSize))
-            
+
             appIcon.draw(in: CGRect(origin: iconOrigin, size: iconSize))
         }
 
@@ -125,17 +123,32 @@ func renderBitcoinAddressQRCodeSnippetAsImageCoreGraphics(
                 .foregroundColor: UIColor.black
             ]
             let textSize = labelText.size(withAttributes: attributes)
-            let labelOrigin = CGPoint(x: (size.width - textSize.width) / 2, y: qrCodeOrigin.y - textSize.height - 10)
+            let labelOrigin = CGPoint(
+                x: (size.width - textSize.width) / 2,
+                y: qrCodeOrigin.y - textSize.height - 10
+            )
             labelText.draw(at: labelOrigin, withAttributes: attributes)
         }
 
         if showAddress {
-            drawStyledAddress(qrCode: qrCode, context: context, size: size, qrCodeOrigin: qrCodeOrigin)
+            drawStyledAddress(
+                qrCode: qrCode,
+                context: context,
+                size: size,
+                qrCodeOrigin: qrCodeOrigin
+            )
         }
     }
+
+    return image
 }
 
-private func drawStyledAddress(qrCode: String, context: UIGraphicsImageRendererContext, size: CGSize, qrCodeOrigin: CGPoint) {
+private func drawStyledAddress(
+    qrCode: String,
+    context: UIGraphicsImageRendererContext,
+    size: CGSize,
+    qrCodeOrigin: CGPoint
+) {
     let fontSize: CGFloat = 14
     let fontBold = UIFont.monospacedSystemFont(ofSize: fontSize, weight: .bold)
     let fontRegular = UIFont.monospacedSystemFont(ofSize: fontSize, weight: .regular)
@@ -160,16 +173,16 @@ private func drawStyledAddress(qrCode: String, context: UIGraphicsImageRendererC
         let secondMiddle = String(middle.dropFirst(splitIndex))
 
         let firstLine = NSMutableAttributedString()
-        firstLine.append(NSAttributedString(string: firstFour, attributes: attributesBold))
+        firstLine.append(NSAttributedString(string: firstFour + " ", attributes: attributesBold))
         firstLine.append(NSAttributedString(string: firstMiddle, attributes: attributesNormal))
 
         let secondLine = NSMutableAttributedString()
         secondLine.append(NSAttributedString(string: secondMiddle, attributes: attributesNormal))
-        secondLine.append(NSAttributedString(string: lastFour, attributes: attributesBold))
+        secondLine.append(NSAttributedString(string: " " + lastFour, attributes: attributesBold))
 
-        let firstLineWidth = firstLine.size().width
-        let secondLineWidth = secondLine.size().width
-        let widthDifference = abs(firstLineWidth - secondLineWidth)
+        let firstLineSize = firstLine.size()
+        let secondLineSize = secondLine.size()
+        let widthDifference = abs(firstLineSize.width - secondLineSize.width)
 
         if widthDifference < minWidthDifference {
             minWidthDifference = widthDifference
@@ -179,12 +192,12 @@ private func drawStyledAddress(qrCode: String, context: UIGraphicsImageRendererC
     }
 
     let firstLineAttributedString = NSMutableAttributedString()
-    firstLineAttributedString.append(NSAttributedString(string: firstFour, attributes: attributesBold))
+    firstLineAttributedString.append(NSAttributedString(string: firstFour + " ", attributes: attributesBold))
     firstLineAttributedString.append(NSAttributedString(string: bestFirstMiddle, attributes: attributesNormal))
 
     let secondLineAttributedString = NSMutableAttributedString()
     secondLineAttributedString.append(NSAttributedString(string: bestSecondMiddle, attributes: attributesNormal))
-    secondLineAttributedString.append(NSAttributedString(string: lastFour, attributes: attributesBold))
+    secondLineAttributedString.append(NSAttributedString(string: " " + lastFour, attributes: attributesBold))
 
     let firstLineSize = firstLineAttributedString.size()
     let secondLineSize = secondLineAttributedString.size()
