612b0fd360090c19476dfcb7f66242bb62964de0
Marcos Rodriguez Velez <marcospr@pm.me>
2024-11-11 21:59:28 -0400

    Update CompanionDelegates.tsx

--- components/CompanionDelegates.tsx
+++ components/CompanionDelegates.tsx
@@ -52,7 +52,7 @@ const CompanionDelegates = () => {
     try {
       const notifications2process = await getStoredNotifications();
       await clearStoredNotifications();
-      await setApplicationIconBadgeNumber(0);
+      setApplicationIconBadgeNumber(0);
 
       const deliveredNotifications = await getDeliveredNotifications();
       setTimeout(async () => {
