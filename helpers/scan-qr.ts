8a28c89a8e0fb74af9ea0f3ddb8463ed67795bc0
Marcos Rodriguez Velez <marcospr@pm.me>
2025-01-02 18:42:55 -0400

    Update scan-qr.ts

--- helpers/scan-qr.ts
+++ helpers/scan-qr.ts
@@ -1,27 +1,5 @@
 import { Platform } from 'react-native';
 import { check, request, PERMISSIONS, RESULTS } from 'react-native-permissions';
-import { navigationRef } from '../NavigationService';
-
-/**
- * Helper function that navigates to ScanQR screen, and returns promise that will resolve with the result of a scan,
- * and then navigates back. If QRCode scan was closed, promise resolves to null.
- *
- * @param currentScreenName {string}
- * @param showFileImportButton {boolean}
- *
- * @param useMerge {boolean} - if true, will merge the new screen with the current screen, otherwise will replace the current screen
- * @return {Promise<string>}
- */
-function scanQrHelper(currentScreenName: string, showFileImportButton = true, useMerge = true): void {
-  requestCameraAuthorization().then(() => {
-    const params = { launchedBy: currentScreenName, showFileImportButton: Boolean(showFileImportButton) };
-    navigationRef.navigate({
-      name: 'ScanQRCode',
-      params,
-      merge: useMerge,
-    });
-  });
-}
 
 const isCameraAuthorizationStatusGranted = async () => {
   const status = await check(Platform.OS === 'android' ? PERMISSIONS.ANDROID.CAMERA : PERMISSIONS.IOS.CAMERA);
@@ -32,4 +10,4 @@ const requestCameraAuthorization = () => {
   return request(Platform.OS === 'android' ? PERMISSIONS.ANDROID.CAMERA : PERMISSIONS.IOS.CAMERA);
 };
 
-export { scanQrHelper, isCameraAuthorizationStatusGranted, requestCameraAuthorization };
+export { isCameraAuthorizationStatusGranted, requestCameraAuthorization };
