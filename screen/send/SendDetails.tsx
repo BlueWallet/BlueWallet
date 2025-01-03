28f893f91fe8259cb9aa6f02d799676b4ba53bd0
Marcos Rodriguez Velez <marcospr@pm.me>
2025-01-03 11:31:15 -0400

    Update SendDetails.tsx

--- screen/send/SendDetails.tsx
+++ screen/send/SendDetails.tsx
@@ -1174,7 +1174,7 @@ const SendDetails = () => {
       return (
         <View style={styles.select}>
           <CoinsSelected
-            number={utxos?.length || 0}
+            number={utxos.length || 0}
             onContainerPress={handleCoinControl}
             onClose={() => {
               LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
