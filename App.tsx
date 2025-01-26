0f174b7ff56f6ea33ba7153385805ee6362a8d7b
Marcos Rodriguez Velez <marcospr@pm.me>
2025-01-26 10:47:36 -0400

    ADD: Nav devtools

--- App.tsx
+++ App.tsx
@@ -9,10 +9,13 @@ import { SettingsProvider } from './components/Context/SettingsProvider';
 import { BlueDarkTheme, BlueDefaultTheme } from './components/themes';
 import MasterView from './navigation/MasterView';
 import { navigationRef } from './NavigationService';
+import { useLogger } from '@react-navigation/devtools';
 import { StorageProvider } from './components/Context/StorageProvider';
 
 const App = () => {
   const colorScheme = useColorScheme();
+
+  useLogger(navigationRef);
 
   return (
     <NavigationContainer ref={navigationRef} theme={colorScheme === 'dark' ? BlueDarkTheme : BlueDefaultTheme}>
