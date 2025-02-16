884774ef54dc60daeda7e8f1aea423a755f0b8b1
Marcos Rodriguez Velez <marcospr@pm.me>
2025-02-15 20:36:05 -0400

    psbt

--- screen/send/psbtMultisigQRCode.js
+++ screen/send/psbtMultisigQRCode.js
@@ -48,17 +48,17 @@ const PsbtMultisigQRCode = () => {
       if (!ret.data) ret = { data: ret };
       if (ret.data.toUpperCase().startsWith('UR')) {
         presentAlert({ message: 'BC-UR not decoded. This should never happen' });
-      } else if (ret.data.indexOf('+') === -1 && ret.data.indexOf('=') === -1 && ret.data.indexOf('=') === -1) {
+      } else if (ret.data.indexOf('+') === -1 && ret.data.indexOf('=') === -1) {
+        presentAlert({ message: loc.wallets.import_error });
         // this looks like NOT base64, so maybe its transaction's hex
         // we dont support it in this flow
-        presentAlert({ message: loc.wallets.import_error });
       } else {
         // psbt base64?
-        const popToAction = StackActions.popTo('PsbtMultisig', { receivedPSBTBase64: ret.data }, { merge: true });
+        const popToAction = StackActions.popTo('PsbtMultisig', { psbtBase64, receivedPSBTBase64: ret.data, ...params }, true);
         navigation.dispatch(popToAction);
       }
     },
-    [navigation],
+    [navigation, psbtBase64, params],
   );
 
   useEffect(() => {
