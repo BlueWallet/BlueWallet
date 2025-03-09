c67eea81556904792b6b12801fb3f080038cc408
Marcos Rodriguez Velez <marcospr@pm.me>
2025-03-09 07:45:19 -0400

    REF: Use bottom tabs

--- hooks/useMenuElements.ts
+++ hooks/useMenuElements.ts
@@ -1,3 +1,5 @@
+import { useCallback } from 'react';
+
 type MenuActionHandler = () => void;
 
 interface MenuElementsHook {
@@ -6,6 +8,22 @@ interface MenuElementsHook {
   isMenuElementsSupported: boolean;
 }
 
-declare const useMenuElements: () => MenuElementsHook;
+// Default implementation for platforms other than iOS
+const useMenuElements = (): MenuElementsHook => {
+  const registerTransactionsHandler = useCallback((_handler: MenuActionHandler, _screenKey?: string): boolean => {
+    // Non-functional stub for non-iOS platforms
+    return false;
+  }, []);
+
+  const unregisterTransactionsHandler = useCallback((_screenKey: string): void => {
+    // No-op for non-supported platforms
+  }, []);
+
+  return {
+    registerTransactionsHandler,
+    unregisterTransactionsHandler,
+    isMenuElementsSupported: false, // Not supported on platforms other than iOS
+  };
+};
 
 export default useMenuElements;
