f1b8c708be714071a75319d3daf1a69688a09a8c
Marcos Rodriguez Velez <marcospr@pm.me>
2025-03-14 23:34:14 -0400

    FIX: Large Screen provider failed on appstate change

--- navigation/DrawerRoot.tsx
+++ navigation/DrawerRoot.tsx
@@ -1,8 +1,6 @@
-// DrawerRoot.tsx
 import { createDrawerNavigator, DrawerNavigationOptions, DrawerContentComponentProps } from '@react-navigation/drawer';
 import React, { useLayoutEffect, useMemo } from 'react';
-import { I18nManager, LayoutAnimation } from 'react-native';
-
+import { I18nManager, LayoutAnimation, Animated, Platform, Easing, View } from 'react-native';
 import { useIsLargeScreen } from '../hooks/useIsLargeScreen';
 import DrawerList from '../screen/wallets/DrawerList';
 import DetailViewStackScreensStack from './DetailViewScreensStack';
@@ -12,30 +10,60 @@ import { DrawerParamList } from './DrawerParamList';
 const Drawer = createDrawerNavigator<DrawerParamList>();
 
 const DrawerContent = (props: DrawerContentComponentProps) => {
+  const { isLargeScreen } = useIsLargeScreen();
+
+  if (!isLargeScreen) {
+    return <View />;
+  }
+
   return <DrawerList {...props} />;
+};
+
+const getAnimationConfig = (isDrawerTransitionConfigured: boolean) => {
+  if (!isDrawerTransitionConfigured) return {};
+
+  return {
+    config: {
+      timing: Animated.timing,
+      useNativeDriver: true,
+      duration: 250,
+      easing: Easing.inOut(Easing.cubic),
+    },
+  };
 };
 
 const DrawerRoot = () => {
   const { isLargeScreen } = useIsLargeScreen();
   const { isDrawerShouldHide } = useSettings();
 
+  const getDrawerWidth = useMemo(() => {
+    return isLargeScreen && !isDrawerShouldHide ? 320 : 0;
+  }, [isLargeScreen, isDrawerShouldHide]);
+
+  useLayoutEffect(() => {
+    const animConfig =
+      Platform.OS === 'ios'
+        ? LayoutAnimation.create(300, LayoutAnimation.Types.easeInEaseOut, LayoutAnimation.Properties.scaleXY)
+        : LayoutAnimation.Presets.easeInEaseOut;
+
+    LayoutAnimation.configureNext(animConfig);
+  }, [isDrawerShouldHide]);
+
   const drawerStyle: DrawerNavigationOptions = useMemo(
     () => ({
       drawerPosition: I18nManager.isRTL ? 'right' : 'left',
       drawerStyle: {
-        width: isLargeScreen && !isDrawerShouldHide ? 320 : '0%',
+        width: getDrawerWidth,
         height: '100%',
       },
       drawerType: isLargeScreen ? 'permanent' : 'front',
-      overlayColor: 'rgba(0,0,0,0.5)',
+      overlayColor: 'rgba(0,0,0,0.4)',
       swipeEnabled: !isDrawerShouldHide,
+      drawerStatusBarAnimation: 'fade',
+      ...getAnimationConfig(true),
     }),
-    [isDrawerShouldHide, isLargeScreen],
+    [getDrawerWidth, isDrawerShouldHide, isLargeScreen],
   );
-
-  useLayoutEffect(() => {
-    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
-  }, [isDrawerShouldHide]);
 
   return (
     <Drawer.Navigator screenOptions={drawerStyle} drawerContent={DrawerContent} initialRouteName="DetailViewStackScreensStack">
