5167df20aeca484bd4a3db544ba824f89487e361
Marcos Rodriguez <marcospr@pm.me>
2025-05-06 22:15:51 -0400

    FIX: SegmntController was crashing on iOS

--- ios/Components/CustomSegmentedControl.m
+++ ios/Components/CustomSegmentedControl.m
@@ -11,11 +11,11 @@
 #import <React/RCTBridgeModule.h>
 #import <React/RCTUIManager.h>
 #import <React/RCTEventDispatcher.h>
-#import "YourProjectName-Swift.h" // Replace 'YourProjectName' with the actual product module name
 
 @interface RCT_EXTERN_MODULE(CustomSegmentedControlManager, RCTViewManager)
 
-RCT_EXTERN_METHOD(setValues:(nonnull NSNumber *)reactTag values:(NSArray<NSString *> *)values)
-RCT_EXTERN_METHOD(setSelectedIndex:(nonnull NSNumber *)reactTag selectedIndex:(nonnull NSNumber *)selectedIndex)
+RCT_EXPORT_VIEW_PROPERTY(values, NSArray)
+RCT_EXPORT_VIEW_PROPERTY(selectedIndex, NSNumber)
+RCT_EXPORT_VIEW_PROPERTY(onChangeEvent, RCTDirectEventBlock)
 
 @end
--- ios/Components/SegmentedControl/CustomSegmentedControl.swift
+++ ios/Components/SegmentedControl/CustomSegmentedControl.swift
@@ -1,30 +1,35 @@
 import UIKit
 import React
 
+@objc(CustomSegmentedControl)
 class CustomSegmentedControl: UISegmentedControl {
     @objc var onChangeEvent: RCTDirectEventBlock?
-    
+
+    @objc var values: [String] = [] {
+        didSet {
+            removeAllSegments()
+            for (index, title) in values.enumerated() {
+                insertSegment(withTitle: title, at: index, animated: false)
+            }
+        }
+    }
+
+    @objc var selectedIndex: NSNumber = 0 {
+        didSet {
+            self.selectedSegmentIndex = selectedIndex.intValue
+        }
+    }
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         addTarget(self, action: #selector(onChange(_:)), for: .valueChanged)
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         addTarget(self, action: #selector(onChange(_:)), for: .valueChanged)
     }
-    
-    @objc func setValues(_ values: [String]) {
-        removeAllSegments()
-        for (index, title) in values.enumerated() {
-            insertSegment(withTitle: title, at: index, animated: false)
-        }
-    }
-    
-    @objc func setSelectedIndex(_ selectedIndex: NSNumber) {
-        self.selectedSegmentIndex = selectedIndex.intValue
-    }
-    
+
     @objc func onChange(_ sender: UISegmentedControl) {
         onChangeEvent?(["selectedIndex": sender.selectedSegmentIndex])
     }
@@ -32,38 +37,10 @@ class CustomSegmentedControl: UISegmentedControl {
 
 @objc(CustomSegmentedControlManager)
 class CustomSegmentedControlManager: RCTViewManager {
-    static var isRegistered = false
-    
     override func view() -> UIView! {
-        // Ensure native module is registered before returning the view.
-        CustomSegmentedControlManager.registerIfNecessary()
         return CustomSegmentedControl(frame: .zero)
     }
-    
-    @objc static func registerIfNecessary() {
-        if !isRegistered {
-            isRegistered = true
-        }
-    }
-    
-    // Changed from static to instance method.
-    override func constantsToExport() -> [AnyHashable: Any]! {
-        return [
-            "bubblingEventTypes": [
-                "onChangeEvent": [
-                    "phasedRegistrationNames": [
-                        "bubbled": "onChangeEvent",
-                        "captured": "onChangeEventCapture"
-                    ]
-                ]
-            ]
-        ]
-    }
 
-    override class func moduleName() -> String! {
-        return "CustomSegmentedControl"
-    }
-    
     override class func requiresMainQueueSetup() -> Bool {
         return true
     }
