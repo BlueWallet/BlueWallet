f07ae214b070ea3fa53d8dff744437f0bb6202aa
danilobatson <69876068+danilobatson@users.noreply.github.com>
2025-05-30 10:50:59 -0700

    REF: refactor BlueSpacing components to remove inline styling

--- components/BlueSpacing.tsx
+++ components/BlueSpacing.tsx
@@ -1,6 +1,5 @@
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { View, ViewProps } from 'react-native';
+import { View, ViewProps, StyleSheet } from 'react-native';
 
 interface BlueSpacingProps extends ViewProps {
   horizontal?: boolean; // Optional prop to determine if spacing is horizontal
@@ -8,62 +7,42 @@ interface BlueSpacingProps extends ViewProps {
 
 export const BlueSpacing10: React.FC<BlueSpacingProps> = props => {
   const { style, ...otherProps } = props;
-  return (
-    <View
-      {...otherProps}
-      style={[
-        {
-          height: 10,
-        },
-        style,
-      ]}
-    />
-  );
+  return <View {...otherProps} style={[styles.spacing10, style]} />;
 };
 
 export const BlueSpacing20: React.FC<BlueSpacingProps> = props => {
   const { horizontal = false, style, ...otherProps } = props;
-  return (
-    <View
-      {...otherProps}
-      style={[
-        {
-          height: horizontal ? 0 : 20,
-          width: horizontal ? 20 : 0,
-          opacity: 0,
-        },
-        style,
-      ]}
-    />
-  );
+  return <View {...otherProps} style={[horizontal ? styles.spacing20Horizontal : styles.spacing20Vertical, style]} />;
 };
 
 export const BlueSpacing40: React.FC<BlueSpacingProps> = props => {
   const { style, ...otherProps } = props;
-  return (
-    <View
-      {...otherProps}
-      style={[
-        {
-          height: 40,
-        },
-        style,
-      ]}
-    />
-  );
+  return <View {...otherProps} style={[styles.spacing40, style]} />;
 };
 
 export const BlueSpacing: React.FC<BlueSpacingProps> = props => {
   const { style, ...otherProps } = props;
-  return (
-    <View
-      {...otherProps}
-      style={[
-        {
-          height: 60,
-        },
-        style,
-      ]}
-    />
-  );
+  return <View {...otherProps} style={[styles.spacing60, style]} />;
 };
+
+const styles = StyleSheet.create({
+  spacing10: {
+    height: 10,
+  },
+  spacing20Vertical: {
+    height: 20,
+    width: 0,
+    opacity: 0,
+  },
+  spacing20Horizontal: {
+    height: 0,
+    width: 20,
+    opacity: 0,
+  },
+  spacing40: {
+    height: 40,
+  },
+  spacing60: {
+    height: 60,
+  },
+});
