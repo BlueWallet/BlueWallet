e67b68d4e5edea710b4c0b9dcfd6f8ab92ac3902
Marcos Rodriguez <marcospr@pm.me>
2025-06-04 00:01:13 -0400

    REF: Android segment control is now native
    iOS was native. Android wasnt. Now they are on par

---
+++ android/app/src/main/java/io/bluewallet/bluewallet/components/segmentedcontrol/CustomSegmentedControlPackage.kt
@@ -0,0 +1,17 @@
+package io.bluewallet.bluewallet.components.segmentedcontrol
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+class CustomSegmentedControlPackage : ReactPackage {
+
+    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+        return emptyList()
+    }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+        return listOf(CustomSegmentedControlManager())
+    }
+}
