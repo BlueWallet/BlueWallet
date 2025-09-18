26d4f40e0f19d041416e4d3417388f4c0258d370
overtorment <overtorment@gmail.com>
2025-09-18 11:57:39 +0100

    REF: remove rn-nodeify in favor or metro bundler configuration

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,15 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    extraNodeModules: {
+      stream: require.resolve('stream-browserify'),
+      crypto: require.resolve('crypto-browserify'),
+      net: require.resolve('react-native-tcp-socket'),
+      tls: require.resolve('react-native-tcp-socket'),
+    },
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
