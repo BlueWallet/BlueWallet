78beb8856905f513b395c9e4a99e8ef37ef85af7
overtorment <overtorment@gmail.com>
2025-11-20 14:46:08 +0000

    FIX: better electrum fee estimation

--- models/networkTransactionFees.ts
+++ models/networkTransactionFees.ts
@@ -29,7 +29,11 @@ export default class NetworkTransactionFees {
         throw new Error('Electrum is disabled. Dont attempt to fetch fees');
       }
       const response = await BlueElectrum.estimateFees();
-      return new NetworkTransactionFee(response.fast + 5, response.medium + 2, response.slow);
+      if (response.fast === response.medium) {
+        // exception, if fees are equal lets bump priority fee + 1 so actual priority tx is above the rest
+        return new NetworkTransactionFee(response.fast + 1, response.medium, response.slow);
+      }
+      return new NetworkTransactionFee(response.fast, response.medium, response.slow);
     } catch (err) {
       console.warn(err);
       return new NetworkTransactionFee(2, 1, 1);
