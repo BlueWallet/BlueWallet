d9150edcb2f20b08642203e92b4d07a83817f2d1
overtorment <overtorment@gmail.com>
2026-02-26 18:12:54 +0000

    REF: revert metro config

--- metro.config.js
+++ metro.config.js
@@ -4,10 +4,6 @@ const path = require('path');
 const resolveAliases = {
   '@arkade-os/sdk/adapters/expo': path.join(__dirname, 'node_modules/@arkade-os/sdk/dist/cjs/adapters/expo.js'),
   'expo/fetch': path.join(__dirname, 'util/expo-fetch.js'),
-  stream: require.resolve('stream-browserify'),
-  crypto: require.resolve('crypto-browserify'),
-  net: require.resolve('react-native-tcp-socket'),
-  tls: require.resolve('react-native-tcp-socket'),
 };
 
 /**
@@ -18,6 +14,12 @@ const resolveAliases = {
  */
 const config = {
   resolver: {
+    extraNodeModules: {
+      stream: require.resolve('stream-browserify'),
+      crypto: require.resolve('crypto-browserify'),
+      net: require.resolve('react-native-tcp-socket'),
+      tls: require.resolve('react-native-tcp-socket'),
+    },
     resolveRequest: (context, moduleName, platform) => {
       if (resolveAliases[moduleName])
         return {
