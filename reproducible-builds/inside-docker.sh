25c02c82f2fb43e0f961be5c15f3a840f386b472
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2026-05-25 22:03:46 +0300

    OPS: Handle password securely

--- reproducible-builds/inside-docker.sh
+++ reproducible-builds/inside-docker.sh
@@ -41,8 +41,8 @@ else
   apksigner sign \
     --ks "$KEYSTORE" \
     --ks-key-alias temp-key \
-    --ks-pass pass:password \
-    --key-pass pass:password \
+    --ks-pass env:password \
+    --key-pass env:password \
     --deterministic-dsa-signing \
     --out "$APK_SIGNED" \
     "$APK_UNSIGNED"
