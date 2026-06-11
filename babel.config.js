a04cad686f485939bf1a679f444f7d5f26d5871b
Ivan Vershigora <ivan.vershigora@gmail.com>
2026-06-11 17:35:49 +0100

    fix: comment with the reference

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  // Pin the @babel/runtime version so Metro resolves a single copy instead of
+  // bundling duplicate helpers, which bloats the bundle.
+  // See https://github.com/babel/babel/issues/18050
   presets: [['module:@react-native/babel-preset', { enableBabelRuntime: '^7.26.0' }]],
   plugins: ['react-native-worklets/plugin'],
 };
