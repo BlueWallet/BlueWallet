012e49b05140445ec22372e850aac6362216659c
ps5link007-hash <ps5link007@gmail.com>
2026-06-12 04:19:36 -0400

    Delete blue_modules directory

--- blue_modules/constants.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Let's keep config vars, constants and definitions here
- */
-
-export const groundControlUri: string = 'https://groundcontrol.bluewallet.io';
-
-/** bitcoin-payment-push-service base URL, no trailing slash. Empty = disabled. */
-export const arkadePaymentPushUri: string = 'https://electrum2.bluewallet.io:444';
--- blue_modules/environment.ts
+++
@@ -1,7 +0,0 @@
-import { getDeviceType, isTablet as checkIsTablet } from 'react-native-device-info';
-
-const isTablet: boolean = checkIsTablet();
-const isDesktop: boolean = getDeviceType() === 'Desktop';
-const isHandset: boolean = getDeviceType() === 'Handset';
-
-export { isDesktop, isHandset, isTablet };
