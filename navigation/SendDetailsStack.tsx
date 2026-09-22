475dadf7f5808f4f15d5ad56cfbf589b3cf5bc64
marcosrdz <marcospr@pm.me>
2026-09-21 20:06:31 -0500

    FIX: Alert on SignVerify had no anchor

--- navigation/SendDetailsStack.tsx
+++ navigation/SendDetailsStack.tsx
@@ -144,7 +144,12 @@ const SendDetailsStack = () => {
       <Stack.Screen
         name="PsbtWithHardwareWallet"
         component={PsbtWithHardwareWalletComponent}
-        options={navigationStyle({ title: loc.send.header, closeButtonPosition: CloseButtonPosition.Right, gestureEnabled: false, fullScreenGestureEnabled: false })(theme)}
+        options={navigationStyle({
+          title: loc.send.header,
+          closeButtonPosition: CloseButtonPosition.Right,
+          gestureEnabled: false,
+          fullScreenGestureEnabled: false,
+        })(theme)}
       />
       <Stack.Screen
         name="CreateTransaction"
